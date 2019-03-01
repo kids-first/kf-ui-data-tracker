7bcee034bb85023bd80d9dd28f22bfb967b4f9f6
Ben Dolly <bromeodolly@gmail.com>
2019-03-01 11:32:19 -0500

    :heavy_plus_sign: adds react-app-rewired
    add react-app-rewired so we can use postcss

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = config => {
+  // require("react-app-rewire-postcss")(config, {
+  //   plugins: loader => [
+  //     require("postcss-import"),
+  //     require("postcss-inherit"),
+  //     require("postcss-nested")
+  //   ]
+  // });
+
+  return config;
+};
