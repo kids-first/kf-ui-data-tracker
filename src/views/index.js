cbd0a8eab5163efbbd8173e7d3fddd1394e3e97a
liberaliscomputing <liberalis.computing@gmail.com>
2019-03-05 15:40:57 -0500

    :recycle: re-order exports

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,3 @@
-export { default as HomeView } from "./HomeView";
-export { default as InvestigatorListView } from "./InvestigatorListView";
-export { default as FileUploadView } from "./FileUploadView";
-export { default as FileListView } from "./FileListView";
 export { default as LoginView } from "./LoginView";
+export { default as InvestigatorsView } from "./InvestigatorsView";
+export { default as FileUploadView } from "./FileUploadView";
