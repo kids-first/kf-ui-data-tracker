abe2118326d5a5d9d87e3369f887475ea1565637
Ben Dolly <bromeodolly@gmail.com>
2019-03-15 13:36:46 -0400

    :fire: removes unused UploadFile component and httplink from client.js

--- src/state/client.js
+++ src/state/client.js
@@ -6,10 +6,6 @@ import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { KF_STUDY_API } from '../common/globals';
 
-const httpLink = createHttpLink({
-  uri: `${KF_STUDY_API}/graphql`,
-});
-
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('egoToken');
   return {
