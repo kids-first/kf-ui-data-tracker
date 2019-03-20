78e25b1b7d754145f52d9c13b54ec420036ede2a
Dan Kolbman <dankolbman@gmail.com>
2019-03-20 19:06:32 -0400

    :recycle: Use query component in list view

--- src/views/StudyListView.js
+++ src/views/StudyListView.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import {graphql, Mutation} from 'react-apollo';
+import {Query} from 'react-apollo';
 import {Link} from 'react-router-dom';
-import {compose} from 'recompose';
 import {ALL_STUDIES} from '../state/queries';
 import StudyCard from '../components/StudyCard';
 import {renderWhileLoading, LoadingPlaceholder} from '../components/Loading';
 
-const StudyListView = ({
-  studies: {
-    loading,
-    allStudies: {edges},
-  },
-}) => {
-  return (
-    <ul className="study-list grid-container list-reset">
-      {edges.map(node => (
-        <li className="col-3 study-list--item" key={node.node.id}>
-          <Link to={`/study/${node.node.id}/files`}>
-            <StudyCard title={node.node.name}>{node.node.id}</StudyCard>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+const StudyListView = () => (
+  <Query query={ALL_STUDIES}>
+    {({loading, error, data}) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+      return (
+        <ul className="study-list grid-container list-reset">
+          {data.allStudies.edges.map(node => (
+            <li className="col-3 study-list--item" key={node.node.id}>
+              <Link to={`/study/${node.node.id}/files`}>
+                <StudyCard title={node.node.name}>{node.node.id}</StudyCard>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+);
 
-export default compose(
-  graphql(ALL_STUDIES, {name: 'studies'}),
-  renderWhileLoading(LoadingPlaceholder, 'studies'),
-)(StudyListView);
+export default StudyListView;
