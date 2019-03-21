8036b3015b8dadebf776683871b6524e8a3099a2
liberaliscomputing <liberalis.computing@gmail.com>
2019-03-21 15:15:14 -0400

    :sparkles: local cache user info

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import {GOOGLE_APP_ID, EGO_API} from '../../common/globals';
+import jwtDecode from 'jwt-decode';
+import {ApolloConsumer} from 'react-apollo';
 import {withRouter} from 'react-router';
 
-const onSuccess = (repsonse, history) => {
+const onSuccess = (repsonse, client, history) => {
   fetch(EGO_API + '/oauth/google/token', {
     method: 'GET',
     mode: 'cors',
@@ -17,7 +19,13 @@ const onSuccess = (repsonse, history) => {
     })
     .then(text => {
       localStorage.setItem('egoToken', text);
+
+      const jwtData = jwtDecode(text);
+      const user = {...jwtData.context};
+      client.writeData({data: {user}});
+
       history.push('/');
+
       return text;
     })
     .catch(err => {
@@ -31,12 +39,16 @@ const onFailure = repsonse => {
 };
 
 const LoginContainer = ({history}) => (
-  <GoogleLogin
-    clientId={GOOGLE_APP_ID}
-    buttonText="Sign in with Google"
-    onSuccess={response => onSuccess(response, history)}
-    onFailure={onFailure}
-  />
+  <ApolloConsumer>
+    {client => (
+      <GoogleLogin
+        clientId={GOOGLE_APP_ID}
+        buttonText="Sign in with Google"
+        onSuccess={response => onSuccess(response, client, history)}
+        onFailure={onFailure}
+      />
+    )}
+  </ApolloConsumer>
 );
 
 export default withRouter(LoginContainer);
