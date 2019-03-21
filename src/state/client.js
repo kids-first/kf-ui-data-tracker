151c7d59df7af8baf582b455426881e4a5b20574
Dan Kolbman <dankolbman@gmail.com>
2019-03-21 12:03:38 -0400

    :rotating_light: Fix linter errors

--- src/state/client.js
+++ src/state/client.js
@@ -1,7 +1,6 @@
 import {ApolloClient} from 'apollo-client';
 import {ApolloLink} from 'apollo-link';
 import {createUploadLink} from 'apollo-upload-client';
-import {createHttpLink} from 'apollo-link-http';
 import {setContext} from 'apollo-link-context';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {KF_STUDY_API} from '../common/globals';
