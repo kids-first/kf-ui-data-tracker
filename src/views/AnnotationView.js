89662f2f64a15badfa4a3bd5e34b99dd7797dfd8
Dan Kolbman <dankolbman@gmail.com>
2019-03-25 17:00:37 -0400

    :sparkles: Add file editor to annotation view

--- src/views/AnnotationView.js
+++ src/views/AnnotationView.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import FileEditorContainer from '../containers/FileEditorContainer';
 
 const AnnotationView = ({match}) => {
   // The kf_id of the file
   const fileId = match.params.fileId;
-  return 'annotate your file here'
+  return <FileEditorContainer kfId={fileId} />;
 };
 
 export default AnnotationView;
