d32dcc750dfd1086f339765502644b4493b79310
XuTheBunny <xzhu.fg@gmail.com>
2019-03-25 11:12:30 -0400

    ♻️ Refactor Header component routing to avoid reloading

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Routes from './Routes';
 import {ApolloProvider} from 'react-apollo';
 import {client} from './state/client';
-import {Header} from 'kf-uikit';
 
 const App = () => (
   <ApolloProvider client={client}>
     <main className="App">
-      <Header logo={{logoHref: '/'}} />
       <Routes />
     </main>
   </ApolloProvider>
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 
+import {Header} from 'kf-uikit';
 import {LoginView, StudyListView, FilesView, CallbackView} from '../views';
 
 const Routes = () => (
   <Router>
-    <Switch>
+    <div>
+      <Route
+        path="/"
+        render={({history}) => (
+          <Header
+            logo={{
+              onLogoClick: () => {
+                history.push(`/`);
+              },
+            }}
+          />
+        )}
+      />
       <Route path="/login" component={LoginView} />
       <Route path="/callback" component={CallbackView} />
       <PrivateRoute exact path="/" component={StudyListView} />
       <PrivateRoute path="/study/:kfId/files" component={FilesView} />
-    </Switch>
+    </div>
   </Router>
 );
 
