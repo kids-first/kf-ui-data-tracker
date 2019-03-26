ce36ceb2bbfc405defca6746eca231ed82b18938
XuTheBunny <xzhu.fg@gmail.com>
2019-03-26 16:43:40 -0400

    ♻️ Use flexbox on study banner

--- src/components/StudyHeader/StudyHeader.jsx
+++ src/components/StudyHeader/StudyHeader.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import propTypes from 'prop-types';
 import classes from 'classnames';
 import TimeAgo from 'react-timeago';
-import {GridContainer} from '../../components/Grid';
 import {Icon} from 'kf-uikit';
 
 const StudyHeader = ({
@@ -12,41 +11,34 @@ const StudyHeader = ({
   name: studyName,
   className,
 }) => {
-  const studyHeaderClasses = classes(
-    'StudyHeader',
-    'py-4',
-    'bg-lightGrey',
-    className,
-  );
+  const studyHeaderClasses = classes('StudyHeader', className);
   return (
     <header className={studyHeaderClasses}>
-      <GridContainer>
-        <div className="row-1 col-10">
-          <h3 className="study-id-tag w-full mt-1 text-base">
-            {kfId}
-            <Icon className="pt-3 ml-4" kind="copy" />
-            <span className="study-modified-date w-full pl-4 font-light">
-              <small>
-                last updated: <TimeAgo date={modifiedAt} />
-              </small>
-            </span>
-          </h3>
-          <h1 className="mt-4 col-12 font-light text-blue text-4xl">
-            {shortName || studyName}
-            <Icon className="pb-1 ml-6" kind="edit" />
-          </h1>
-        </div>
-        <div className="study-contacts row-1 col-2 self-start">
-          <h4 className="mt-5 font-bold">Contacts:</h4>
-          <p className="mt-1 font-title font-light">
-            Mary Marazita
-            <Icon className="pt-2 ml-4" kind="email" />
-          </p>
-          <button className="mt-0 font-title font-thin text-sm underline">
-            ADD CONTACT
-          </button>
-        </div>
-      </GridContainer>
+      <div className="flex-initial p-4 sm:ml-12">
+        <h3 className="study-id-tag mt-1 text-base">
+          {kfId}
+          <Icon className="pt-3 ml-4 text-white" kind="copy" />
+          <span className="study-modified-date pl-4 font-light">
+            <small>
+              last updated: <TimeAgo date={modifiedAt} />
+            </small>
+          </span>
+        </h3>
+        <h1 className="mt-4 font-light text-4xl">
+          {shortName || studyName}
+          <Icon className="pb-1 ml-6 text-white" kind="edit" />
+        </h1>
+      </div>
+      <div className="flex-initial p-4 sm:mr-12">
+        <h4 className="mt-5 font-bold">Contacts:</h4>
+        <p className="mt-1 font-title font-light">
+          Mary Marazita
+          <Icon className="pt-2 ml-4 text-white" kind="email" />
+        </p>
+        <button className="mt-0 font-title text-white font-thin text-sm underline">
+          ADD CONTACT
+        </button>
+      </div>
     </header>
   );
 };
