8ce872bc64222ccc8978d150f1309c6e0aead169
Ben Dolly <bromeodolly@gmail.com>
2019-04-25 11:53:20 -0400

    :construction: Study List view toggle
    Adds a toggle between list and grid view

---
+++ src/components/StudyList/StudyGrid.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from 'classnames';
+import {Icon} from 'kf-uikit';
+import {Link} from 'react-router-dom';
+import StudyCard from './StudyCard';
+
+const StudyGrid = ({studyList, loading, className}) => {
+  let studyListClass = classes('StudyList', className);
+  return (
+    <ul className={studyListClass}>
+      {loading
+        ? [0, 1, 2, 3].map(n => (
+            <li
+              className="p-8 cursor-not-allowed w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+              key={n}
+            >
+              <div className="StudyCard--loading">
+                <Icon width={28} height={28} kind="study" />
+              </div>
+            </li>
+          ))
+        : studyList.map(node => (
+            <li
+              className="p-8 cursor-pointer w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+              key={node.node.id}
+            >
+              <Link
+                className="no-underline"
+                to={`/study/${node.node.kfId}/files`}
+              >
+                <StudyCard
+                  title={node.node.kfId}
+                  body={node.node.name || node.node.shortName}
+                  lastUpdate={new Date(node.node.modifiedAt)}
+                />
+              </Link>
+            </li>
+          ))}
+    </ul>
+  );
+};
+
+export default StudyGrid;
--- src/components/StudyList/StudyList.jsx
+++ src/components/StudyList/StudyList.jsx
@@ -1,46 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classes from 'classnames';
 import {withRouter} from 'react-router-dom';
 import StudyCard from './StudyCard';
 import {Icon, GridContainer} from 'kf-uikit';
+import StudyGrid from './StudyGrid';
+import ToggleButtons from '../ToggleButtons/ToggleButtons';
+
 /**
  * Displays unordered studies in grid view (include empty stage message)
  */
-const StudyList = ({className, studyList, loading, history}) => {
-  let studyListClass = classes('StudyList', className);
+const StudyList = ({className, studyList, loading}) => {
+  const [view, setView] = useState('grid');
   const title = loading ? 'Loading studies ...' : 'Browse Studies';
-  const loadingCount = [1, 2, 3, 4];
+
   return (
     <div className="bg-lightGrey min-h-screen py-32">
       <GridContainer className={studyListClass}>
-        <h1 className="text-blue font-title row-1 cell-12">{title}</h1>
-        {loading
-          ? loadingCount.map(n => (
-              <div
-                className="cursor-not-allowed cell-12 sm:cell-6 md:cell-4 lg:cell-3"
-                key={n}
-              >
-                <div className="StudyCard--loading">
-                  <Icon width={28} height={28} kind="study" />
-                </div>
-              </div>
-            ))
-          : studyList.map(node => (
-              <div
-                className="cursor-pointer cell-12 sm:cell-6 md:cell-4 lg:cell-3"
-                key={node.node.id}
-                onClick={() => {
-                  history.push(`/study/${node.node.kfId}/files`);
-                }}
-              >
-                <StudyCard
-                  title={node.node.kfId}
-                  body={node.node.name || node.node.shortName}
-                  lastUpdate={new Date(node.node.modifiedAt)}
-                />
-              </div>
-            ))}
+        <h1 className="text-blue font-title row-1 cell-12">
+          {title}
+          {!loading ? (
+            <ToggleButtons
+              onToggle={({text}) => {
+                setView(text.toLowerCase());
+              }}
+              buttons={[
+                {text: 'Grid', icon: 'resources'},
+                {text: 'list', icon: 'study'},
+              ]}
+            />
+          ) : null}
+        </h1>
+        {view === 'grid' ? (
+          <StudyGrid {...{loading, studyList}} />
+        ) : (
+          <h2>list</h2>
+        )}
       </GridContainer>
     </div>
   );
@@ -60,4 +55,4 @@ StudyList.defaultProps = {
   studyList: [],
 };
 
-export default withRouter(StudyList);
+export default StudyList;
