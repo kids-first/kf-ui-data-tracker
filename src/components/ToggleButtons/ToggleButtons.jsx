46ec959cd2587b3e08c02cf99809d56004583968
Ben Dolly <bromeodolly@gmail.com>
2019-04-25 11:50:47 -0400

    :sparkles: Adds ToogleButtons component
    Simple button toggle using kf-uikit buttons

---
+++ src/components/ToggleButtons/ToggleButtons.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import propTypes from 'prop-types';
+import classnames from 'classnames';
+import {Button} from 'kf-uikit';
+
+const ToggleButtons = ({buttons, className, onToggle}) => {
+  const ToggleButtonsClass = classnames('ToggleButtons', className);
+  const [active, setActive] = useState(0);
+
+  return (
+    <div className={ToggleButtonsClass}>
+      {buttons.map(({text, icon}, idx) => (
+        <Button
+          {...{icon}}
+          color={active === idx ? 'primary' : null}
+          onClick={() => {
+            setActive(idx);
+            onToggle({active, text, icon});
+          }}
+          tabIndex="0"
+        >
+          {text}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+ToggleButtons.propTypes = {
+  buttons: propTypes.arrayOf(propTypes.shape({
+    /** Button text (optional) */
+    text: propTypes.string, 
+    /** kf-uikit icon name (optional) */
+    icon: propTypes.string,
+  })),
+  /** additional classes to add the wrapping div */
+  className: propTypes.string, 
+  /** onClick callback that rececives the button object as args */
+  onToggle: propTypes.func, 
+
+}
+
+export default ToggleButtons;
