bb20ba58b7b9a88ad5a0a1146f3cef15b67cf8b9
Ben Dolly <bromeodolly@gmail.com>
2019-04-30 13:58:19 -0400

    :art: adds activeView prop to StudyList
    :art: sets study list default view to grid

--- src/components/StudyList/StudyList.jsx
+++ src/components/StudyList/StudyList.jsx
@@ -11,33 +11,36 @@ import ToggleButtons from '../ToggleButtons/ToggleButtons';
 /**
  * Displays unordered studies in grid view (include empty stage message)
  */
-const StudyList = ({className, studyList, loading}) => {
-  const defaultView = 'grid';
-  const [view, setView] = useState(defaultView);
+const StudyList = ({className, studyList, loading, activeView = 'grid'}) => {
+  let studyListClass = classes('StudyList', className);
+  const title = loading ? 'Loading studies ...' : 'Browse Studies';
+  const [view, setView] = useState(activeView);
 
   return (
     <div className="bg-lightGrey min-h-screen py-32">
       <GridContainer className={studyListClass}>
         <h1 className="text-blue font-title row-1 cell-12">
           {title}
-          {!loading ? (
+          {!loading && (
             <ToggleButtons
               onToggle={({text}) => {
                 setView(text.toLowerCase());
               }}
               buttons={[
-                {text: 'Grid', icon: 'resources'},
+                {text: 'grid', icon: 'resources'},
                 {text: 'list', icon: 'study'},
               ]}
             />
-          ) : null}
-        </h1>
-        {view === 'grid' ? (
-          <StudyGrid {...{loading, studyList}} />
-        ) : (
-          <StudyTable {...{loading, studyList}} />
-        )}
-      </GridContainer>
+          )}
+        </header>
+        <main>
+          {view === 'grid' ? (
+            <StudyGrid loading={loading} studyList={studyList} />
+          ) : (
+            <StudyTable loading={loading} studyList={studyList} />
+          )}
+        </main>
+      </div>
     </div>
   );
 };
@@ -49,11 +52,14 @@ StudyList.propTypes = {
   studyList: PropTypes.array,
   /** Loading state of the studyList*/
   loading: PropTypes.bool,
+  /** view to show (grid | list) */
+  activeView: PropTypes.oneOf(['grid', 'list']),
 };
 
 StudyList.defaultProps = {
   className: null,
   studyList: [],
+  activeView: 'grid',
 };
 
 export default StudyList;
