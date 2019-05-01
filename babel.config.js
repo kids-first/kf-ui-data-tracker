ae87af7ff29e74ebc871b640494b1f785f7aecb3
Dan Kolbman <dankolbman@gmail.com>
2019-04-30 20:05:55 -0400

    :package: Add missing babel module

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
-{
-  "presets": ["@babel/preset-env", "@babel/preset-react"],
-  "plugins": ["@babel/plugin-proposal-export-default-from"]
-}
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-transform-modules-commonjs',
+  ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        '@babel/preset-react',
+      ],
+    },
+  },
+};
