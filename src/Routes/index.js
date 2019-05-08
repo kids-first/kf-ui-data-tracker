96f7eceb9fa605db41c32fca5d3272f03f32d40f
Dan Kolbman <dankolbman@gmail.com>
2019-05-08 11:44:04 -0400

    :truck: Moves the Router to App module
    Needed so that the Router may be initialized at the application level,
    rather than at the routes level. This is needed so that tests may swap
    out the router for their own and may also allow more complicated router
    definitions at the application level.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import AdminRoute from './AdminRoute';
 
@@ -16,43 +16,41 @@ import {
 } from '../views';
 
 const Routes = () => (
-  <Router>
-    <Fragment>
-      <Route
-        path="/"
-        render={({history}) => (
-          <Header
-            logo={{
-              onLogoClick: () => {
-                history.push(`/`);
-              },
-            }}
-          />
-        )}
-      />
-      <Route path="/login" component={LoginView} />
-      <Route path="/callback" component={CallbackView} />
-      <PrivateRoute path="/study/:kfId/" component={NavBarView} />
-      <PrivateRoute exact path="/" component={StudyListView} />
-      <PrivateRoute
-        exact
-        path="/study/:kfId/files"
-        component={StudyFilesListView}
-      />
-      <PrivateRoute
-        path="/study/:kfId/files/:fileId"
-        component={AnnotationView}
-      />
-      <PrivateRoute exact path="/study/:kfId/basicInfo" component={EmptyView} />
-      <PrivateRoute exact path="/study/:kfId/dashboard" component={EmptyView} />
-      <PrivateRoute
-        exact
-        path="/study/:kfId/collaborators"
-        component={EmptyView}
-      />
-      <AdminRoute exact path="/tokens" component={TokensListView} />
-    </Fragment>
-  </Router>
+  <Fragment>
+    <Route
+      path="/"
+      render={({history}) => (
+        <Header
+          logo={{
+            onLogoClick: () => {
+              history.push(`/`);
+            },
+          }}
+        />
+      )}
+    />
+    <Route path="/login" component={LoginView} />
+    <Route path="/callback" component={CallbackView} />
+    <PrivateRoute path="/study/:kfId/" component={NavBarView} />
+    <PrivateRoute exact path="/" component={StudyListView} />
+    <PrivateRoute
+      exact
+      path="/study/:kfId/files"
+      component={StudyFilesListView}
+    />
+    <PrivateRoute
+      path="/study/:kfId/files/:fileId"
+      component={AnnotationView}
+    />
+    <PrivateRoute exact path="/study/:kfId/basicInfo" component={EmptyView} />
+    <PrivateRoute exact path="/study/:kfId/dashboard" component={EmptyView} />
+    <PrivateRoute
+      exact
+      path="/study/:kfId/collaborators"
+      component={EmptyView}
+    />
+    <AdminRoute exact path="/tokens" component={TokensListView} />
+  </Fragment>
 );
 
 export default Routes;
