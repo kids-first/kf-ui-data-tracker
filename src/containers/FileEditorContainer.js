3950d0b66ac67a77d107493265b3b557ac26216e
Ben Dolly <bromeodolly@gmail.com>
2019-05-09 14:32:22 -0400

    :bug: make fileType not selected for staged files only
    This makes sure there is no pre-selected file type for staged files

--- src/containers/FileEditorContainer.js
+++ src/containers/FileEditorContainer.js
@@ -3,29 +3,36 @@ import {withRouter} from 'react-router-dom';
 import FileEditor from '../components/FileAnnotation/FileEditor';
 import {LoadingPlaceholder} from '../components/Loading';
 import {Query, Mutation} from 'react-apollo';
-import {UPDATE_FILE} from '../state/mutations';
+import {UPDATE_FILE, UPDATE_FILE_STATUS} from '../state/mutations';
 import {GET_FILE_BY_ID} from '../state/queries';
 import PropTypes from 'prop-types';
+import {directive} from '@babel/types';
 
 const FileEditorContainer = ({kfId, history, match}) => {
   const [fileType, setFileType] = useState();
   const [fileNameInput, setFileName] = useState();
+
   // Updates the file then routes to the study's files listing
-  const onSubmit = (e, updateFile) => {
+  const onSubmit = async (e, updateFile, updateFileStatus, id) => {
     e.preventDefault();
     const name = fileNameInput;
     const description = e.target.description.value;
-    updateFile({variables: {kfId, name, description, fileType}})
-      .then(() => {
-        history.push(`/study/${match.params.kfId}/files`);
-      })
-      .catch(err => console.log(err));
+    try {
+      const data = await updateFile({
+        variables: {name, description, kfId, fileType},
+      });
+      const status = await updateFileStatus({variables: {id, status: 'New'}});
+      history.push(`/study/${match.params.kfId}/files`);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   /**
    * Called from the onChange event on the file type radio buttons
    */
   const selectFileType = e => {
+    debugger;
     setFileType(e.target.value);
   };
 
@@ -36,29 +43,45 @@ const FileEditorContainer = ({kfId, history, match}) => {
         if (error) return `Error!: ${error}`;
         // Set the current file type to whatever the query returns, if it's
         // not yet been selected by the user
-        if (fileType === undefined) setFileType(data.fileByKfId.fileType);
+
+        if (data.fileByKfId.status != 'staged' && fileType == undefined) {
+          setFileType(data.fileByKfId.fileType);
+        }
         if (fileNameInput === undefined) setFileName(data.fileByKfId.name);
         return (
-          <Mutation
-            mutation={UPDATE_FILE}
-            refetchQueries={res => [
-              {
-                query: GET_FILE_BY_ID,
-                variables: {kfId},
-              },
-            ]}
-          >
-            {(updateFile, {_}) => {
+          <Mutation mutation={UPDATE_FILE_STATUS}>
+            {(updateFileStatus, {__}) => {
               return (
-                <FileEditor
-                  kfId={data.fileByKfId.kfId}
-                  name={fileNameInput}
-                  description={data.fileByKfId.description}
-                  fileType={fileType}
-                  selectFileType={selectFileType}
-                  onSubmit={e => onSubmit(e, updateFile)}
-                  onNameChange={e => setFileName(e.target.value)}
-                />
+                <Mutation
+                  mutation={UPDATE_FILE}
+                  refetchQueries={res => [
+                    {
+                      query: GET_FILE_BY_ID,
+                      variables: {kfId},
+                    },
+                  ]}
+                >
+                  {(updateFile, {_}) => {
+                    return (
+                      <FileEditor
+                        kfId={data.fileByKfId.kfId}
+                        name={fileNameInput}
+                        description={data.fileByKfId.description}
+                        fileType={fileType}
+                        selectFileType={selectFileType}
+                        onSubmit={e =>
+                          onSubmit(
+                            e,
+                            updateFile,
+                            updateFileStatus,
+                            data.fileByKfId.id,
+                          )
+                        }
+                        onNameChange={e => setFileName(e.target.value)}
+                      />
+                    );
+                  }}
+                </Mutation>
               );
             }}
           </Mutation>
