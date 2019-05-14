4139f573251b41b99807b912fef74b7c2a6f060f
XuTheBunny <xzhu.fg@gmail.com>
2019-05-13 23:42:44 -0400

    ✨ Add pagination to file list (5 per page)

--- src/components/FileList/FileList.js
+++ src/components/FileList/FileList.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import classes from 'classnames';
 import FileElement from './FileElement';
-
+import ReactPaginate from 'react-paginate';
 /**
  * Displays list of study files
  */
 const FileList = ({className, fileList, studyId}) => {
   let fileListClass = classes('FileList', className);
-
+  const perPage = 5;
+  const [page, setPage] = useState(0);
+  const handlePageClick = data => {
+    setPage(data.selected);
+  };
+  const pageCount = () => {
+    if (Math.ceil(fileList.length / perPage) < page + 1) {
+      setPage(page - 1);
+    }
+    return Math.ceil(fileList.length / perPage);
+  };
+  const offset = fileList.slice(perPage * page, perPage * page + perPage);
   return (
-    <ul className={fileListClass}>
-      {fileList.length ? (
-        fileList.map(({node}) => (
-          <FileElement key={node.kfId} fileListId={studyId} fileNode={node} />
-        ))
-      ) : (
-        <h3 className="FileList--Empty">You don't have any files yet.</h3>
+    <Fragment>
+      <ul className={fileListClass}>
+        {fileList.length ? (
+          offset.map(({node}) => (
+            <FileElement key={node.kfId} fileListId={studyId} fileNode={node} />
+          ))
+        ) : (
+          <h3 className="FileList--Empty">You don't have any files yet.</h3>
+        )}
+      </ul>
+      {pageCount() > 0 && (
+        <Fragment>
+          <p className="m-0 text-sm text-mediumGrey text-right">
+            {offset.length} of {fileList.length} files
+          </p>
+          <ReactPaginate
+            previousLabel={'<'}
+            nextLabel={'>'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={pageCount()}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            forcePage={page}
+            onPageChange={handlePageClick}
+            containerClassName={'Pagination'}
+            subContainerClassName={'Pagination'}
+            activeClassName={'Pagination--active'}
+          />
+        </Fragment>
       )}
-    </ul>
+    </Fragment>
   );
 };
 
