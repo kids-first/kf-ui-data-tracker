9d2a70e2b0bf0fcea259d2d985ae2d6f9732958f
XuTheBunny <xzhu.fg@gmail.com>
2019-05-13 23:42:44 -0400

    📷 Add test for pagination Having 8 files in files mock, 5 on page1, 3 on page 2

--- src/components/FileList/__tests__/FileList.test.js
+++ src/components/FileList/__tests__/FileList.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import wait from 'waait';
 import {MemoryRouter} from 'react-router-dom';
 import {MockedProvider} from 'react-apollo/test-utils';
 import {render} from 'react-testing-library';
 import FileList from '../FileList';
 import studyByKfId from './studyByKfId';
 
-it('renders with files', () => {
+it('renders with files', async () => {
   const files = studyByKfId.data.studyByKfId.files.edges;
 
   const tree = render(
@@ -17,8 +18,19 @@ it('renders with files', () => {
   );
   expect(tree.container).toMatchSnapshot();
 
-  const lis = tree.container.querySelectorAll('.FileList--Element');
-  expect(lis.length).toBe(4);
+  // 8 files in total, page 1 contains 5
+  const lisPage1 = tree.container.querySelectorAll('.FileList--Element');
+  expect(lisPage1.length).toBe(5);
+
+  // Click on the next button to go to next page
+  let button = tree.getByText(/>/);
+  button.click();
+
+  await wait();
+
+  // 8 files in total, 5 files per page, page 2 should have 3 files
+  const lisPage2 = tree.container.querySelectorAll('.FileList--Element');
+  expect(lisPage2.length).toBe(3);
 });
 
 it('renders without files', () => {
@@ -33,4 +45,8 @@ it('renders without files', () => {
 
   const lis = tree.container.querySelectorAll('.FileList--Element');
   expect(lis.length).toBe(0);
+
+  // Hide pagination when no files exist
+  const pagination = tree.container.querySelectorAll('.Pagination');
+  expect(pagination.length).toBe(0);
 });
