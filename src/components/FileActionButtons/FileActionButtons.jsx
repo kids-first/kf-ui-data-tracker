c55c2aa5bcb4fd9072d0ca7f2d7a75c71330a7c7
XuTheBunny <xzhu.fg@gmail.com>
2019-05-13 23:42:44 -0400

    🔥 Remove obsolete import statement

--- src/components/FileActionButtons/FileActionButtons.jsx
+++ src/components/FileActionButtons/FileActionButtons.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from 'classnames';
-import {KF_STUDY_API} from '../../common/globals';
 import {Icon} from 'kf-uikit';
 import {downloadFile} from '../../common/fileUtils';
 /**
