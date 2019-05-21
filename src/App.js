f2f612b5997f10cee6c5dc133c7cad7c9fac54f5
Dan Kolbman <dankolbman@gmail.com>
2019-05-21 14:37:33 -0400

    :sparkles: Add grommet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
 import {ApolloProvider} from 'react-apollo';
 import {client} from './state/client';
+import {Grommet} from 'grommet';
+import {theme} from './grommet';
 
 const App = () => (
   <ApolloProvider client={client}>
     <Router>
-      <main className="App">
-        <Routes />
-      </main>
+      <Grommet theme={theme}>
+        <main className="App">
+          <Routes />
+        </main>
+      </Grommet>
     </Router>
   </ApolloProvider>
 );
---
+++ src/grommet.js
@@ -0,0 +1,4 @@
+export const theme = {
+  global: {},
+  radioButtonGroup: {margin: {bottom: 'small'}},
+};
