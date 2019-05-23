159a3c9da5fd9500fcd9ea9847b06aa26dfdc7b2
XuTheBunny <xzhu.fg@gmail.com>
2019-05-23 15:38:51 -0400

    ✨ Modal markup with header and footer in its own components

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,56 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import classes from 'classnames';
+import ReactModal from 'react-modal';
+import ModalHeader from './ModalHeader';
+import ModalFooter from './ModalFooter';
+/**
+ * Displays modal view
+ */
+const Modal = ({
+  className,
+  onSubmit,
+  title,
+  children,
+  onCloseModal,
+  onBeforeClose,
+  disableSubmit,
+}) => {
+  let modalClass = classes('Modal--container', className);
+  return (
+    <ReactModal
+      data-testid="modal"
+      isOpen={true}
+      contentLabel="File Version Uploda/Annotation Modal"
+      onRequestClose={onBeforeClose}
+      className={modalClass}
+      overlayClassName="Modal--overlay"
+      ariaHideApp={false}
+    >
+      <ModalHeader title={title} onCloseModal={onCloseModal} />
+      <section className="Modal--content">
+        <Fragment>{children}</Fragment>
+      </section>
+      <ModalFooter
+        onCloseModal={onCloseModal}
+        onSubmit={onSubmit}
+        disableSubmit={disableSubmit}
+      />
+    </ReactModal>
+  );
+};
+
+Modal.propTypes = {
+  /** Any additional classes to be applied to the modal*/
+  className: PropTypes.string,
+  /** Action to submit content*/
+  onSubmit: PropTypes.func,
+  /** Modal title*/
+  title: PropTypes.string,
+};
+
+Modal.defaultProps = {
+  className: null,
+};
+
+export default Modal;
