1483bf4e044a5a12634bdc9aad248ccc8a254319
XuTheBunny <xzhu.fg@gmail.com>
2019-05-24 13:35:57 -0400

    ♻️ Refactor upload container to take children as prop

--- src/containers/UploadContainer.js
+++ src/containers/UploadContainer.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react';
 import {withRouter} from 'react-router-dom';
-import {GET_STUDY_BY_ID} from '../state/queries';
 import {Mutation} from 'react-apollo';
-import FileUploadTarget from '../components/FileUpload/FileUploadTarget';
 import {CREATE_FILE} from '../state/mutations';
 
-const UploadContainer = props => {
+const UploadContainer = ({
+  studyId,
+  fileId,
+  refetchAfterUpload = [],
+  history,
+  match,
+  children,
+}) => {
   const [dragging, setDragging] = useState(false);
   const [count, setCount] = useState(0);
 
@@ -33,16 +38,17 @@ const UploadContainer = props => {
 
   // General handler for uploading files
   // Will forward user to annotation page when a file has been uploaded
-  const uploadAndAnnotate = (createFile, file) => {
-    const studyId = props.match.params.kfId;
+  const uploadFile = (file, createFile, onUploadCallBack) => {
     createFile({
       variables: {file, studyId},
     })
       .then(resp => {
         if (resp.data.createFile.success) {
-          props.history.push(
-            `files/${resp.data.createFile.file.kfId}/annotation/`,
-          );
+          if (onUploadCallBack) {
+            onUploadCallBack();
+          } else {
+            history.push(`files/${resp.data.createFile.file.kfId}/annotation/`);
+          }
         }
       })
       .catch(err => {
@@ -51,44 +57,20 @@ const UploadContainer = props => {
       });
   };
 
-  // Upload a file when a document is selected from the browser window
-  const handleSelectedFile = (e, createFile) => {
-    e.preventDefault();
-    uploadAndAnnotate(createFile, e.target.files[0]);
-  };
-
-  // Upload a file when it is dropped on the container
-  const handleDrop = (e, createFile) => {
-    e.preventDefault();
-    setDragging(false);
-
-    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      uploadAndAnnotate(createFile, e.dataTransfer.files[0]);
-    }
-  };
-
   return (
-    <Mutation
-      mutation={CREATE_FILE}
-      refetchQueries={res => [
-        {
-          query: GET_STUDY_BY_ID,
-          variables: {kfId: props.match.params.kfId},
-        },
-      ]}
-    >
-      {(createFile, {data, error}) => (
-        <FileUploadTarget
-          error={error}
-          dragging={dragging}
-          handleDragOver={handleDragOver}
-          onDragEnter={handleDragEnter}
-          onDragLeave={handleDragLeave}
-          handleDrop={e => handleDrop(e, createFile)}
-          handleSelectedFile={e => handleSelectedFile(e, createFile)}
-          instructions="To upload files, drag and drop them here"
-        />
-      )}
+    <Mutation mutation={CREATE_FILE} refetchQueries={res => refetchAfterUpload}>
+      {(createFile, {data, error}) =>
+        children &&
+        children({
+          createFile,
+          error,
+          dragging,
+          handleDragOver,
+          handleDragEnter,
+          handleDragLeave,
+          uploadFile,
+        })
+      }
     </Mutation>
   );
 };
