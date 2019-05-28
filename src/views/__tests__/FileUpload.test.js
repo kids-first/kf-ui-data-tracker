893b861884f5bc078d1c8a93e3aa075a60af6761
XuTheBunny <xzhu.fg@gmail.com>
2019-05-28 13:54:13 -0400

    🐛 Fix test for file upload by targeting document to render

--- src/views/__tests__/FileUpload.test.js
+++ src/views/__tests__/FileUpload.test.js
@@ -8,6 +8,24 @@ import {mocks} from '../../../__mocks__/kf-api-study-creator/mocks';
 
 jest.mock('auth0-js');
 
+beforeAll(() => {
+  // Always confirm prompts
+  global.prompt = () => true;
+  // Print alerts to console
+  window.alert = () => true;
+
+  // Needed to allow react-copy-to-clipboard to work in test renderer
+  const getSelection = () => ({
+    rangeCount: 0,
+    addRange: () => {},
+    getRangeAt: () => {},
+    removeAllRanges: () => {},
+  });
+
+  window.getSelection = getSelection;
+  document.getSelection = getSelection;
+});
+
 it('edits a file correctly', async () => {
   const tree = render(
     <MockedProvider mocks={mocks}>
@@ -23,17 +41,14 @@ it('edits a file correctly', async () => {
   // Click on the first file's name to go to file detail page
   const fileName = tree.queryByTestId('edit-file');
   fireEvent.click(fileName);
-
   await wait();
 
   // Click on the file's edit button
   fireEvent.click(tree.getByText(/EDIT/));
-
   await wait();
 
-  // Should be on the annotation page, edit the file name
+  // Click edit button to edit the file name
   fireEvent.click(tree.queryByTestId('edit-name-button'));
-
   await wait();
 
   const nameInput = tree.getByTestId('name-input');
@@ -41,13 +56,11 @@ it('edits a file correctly', async () => {
 
   // Click save icon
   fireEvent.click(tree.getByTestId('save-name-button'));
-
   await wait();
 
   // Update the description
   const descInput = tree.getByTestId('description-input');
   fireEvent.change(descInput, {target: {value: 'Some description here'}});
-
   await wait();
 
   // Click 'Annotate File' button to return to file listing
