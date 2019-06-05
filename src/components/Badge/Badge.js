96eb1646a26841e8e8cdc21a294d0c0f4b4888f5
XuTheBunny <xzhu.fg@gmail.com>
2019-06-05 13:11:06 -0400

    ♻️ Refactor badge component to take state enum value prop

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from 'classnames';
+import {versionState} from '../../common/fileUtils';
 /**
  * Displays badge describing the file state
  */
-const badgeState = {
-  new: {name: 'New', color: 'bg-teal'},
-  pendingApproval: {name: 'Pending Approval', color: 'bg-orange'},
-  contentApproved: {name: 'Content Approved', color: 'bg-lightBlue'},
-  changesRequired: {name: 'Changes Required', color: 'bg-red'},
-};
-
 const Badge = ({className, state, loading}) => {
-  const color = state in badgeState ? badgeState[state].color : null;
-  const name = state in badgeState ? badgeState[state].name : 'invalid';
+  const color = versionState[state]
+    ? versionState[state].color
+    : 'bg-lightGrey';
+  const name =
+    state && versionState[state] ? versionState[state].title : 'Invalid';
   const badgeClass = classes(
     'Badge',
     {
       [color]: !loading,
       'Badge--loading': loading,
-      'Badge--invalid': !(state in badgeState),
+      'Badge--invalid': !versionState[state] && !loading,
     },
     className,
   );
