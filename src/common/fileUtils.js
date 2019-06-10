bf867e3fd818f271e4d1a67b5193a692c7576552
XuTheBunny <xzhu.fg@gmail.com>
2019-06-10 17:10:48 -0400

    💄 Adjust approval status color

--- src/common/fileUtils.js
+++ src/common/fileUtils.js
@@ -68,9 +68,9 @@ export const fileTypeDetail = {
 // Store version state title and color
 export const versionState = {
   PEN: {color: 'bg-orange', title: 'Pending Review'},
-  APP: {color: 'bg-lightBlue', title: 'Approved'},
+  APP: {color: 'bg-teal', title: 'Approved'},
   CHN: {color: 'bg-red', title: 'Changes Needed'},
-  PRC: {color: 'bg-teal', title: 'Processed'},
+  PRC: {color: 'bg-blue', title: 'Processed'},
 };
 
 // Sort file versions based on the version createdAt date (Latest first)
