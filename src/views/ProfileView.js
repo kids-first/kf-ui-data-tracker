4de0c090d27fdd62af8b5491cae3d563f046344b
Dan Kolbman <dankolbman@gmail.com>
2019-06-12 12:39:07 -0400

    :lipstick: Use semantic for profile view

--- src/views/ProfileView.js
+++ src/views/ProfileView.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
-import classes from 'classnames';
 import {graphql, compose} from 'react-apollo';
 import jwtDecode from 'jwt-decode';
 import {MY_PROFILE} from '../state/queries';
 import {UPDATE_PROFILE} from '../state/mutations';
-import {Avatar, GridContainer} from 'kf-uikit';
+import {
+  Container,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Label,
+  Segment,
+} from 'semantic-ui-react';
 import UpdateProfileForm from '../forms/UpdateProfileForm';
 import StudySubscriptionContanier from '../containers/StudySubscriptionContainer';
 
@@ -21,6 +28,7 @@ const ProfileView = ({
 
   const handleSave = (slackNotify, slackMemberId) => {
     setMessage();
+    setErrors();
     setSubmitting(true);
     // Call update mutation
     updateProfile({variables: {slackNotify, slackMemberId}})
@@ -37,6 +45,13 @@ const ProfileView = ({
   if (loading) return <span>Loading...</span>;
   if (error) return <span>{error.message}</span>;
 
+  const fields = {
+    username: 'Username',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    email: 'Email',
+  };
+
   const token =
     localStorage.getItem('egoToken') || localStorage.getItem('accessToken');
   const decoded = jwtDecode(token);
@@ -44,90 +59,70 @@ const ProfileView = ({
     decoded['https://kidsfirstdrc.org/groups'] || decoded.context.groups;
   const roles =
     decoded['https://kidsfirstdrc.org/roles'] || decoded.context.roles;
-
-  const sectionHeader = classes(
-    'cell-12',
-    'text-blue',
-    'mt-16',
-    'py-8',
-    'border-b',
-    'border-mediumGrey',
-  );
-
   return (
-    <>
-      <div className="Profile--Header">
-        <GridContainer>
-          <Avatar className="cell-1" size={150} imgUrl={profile.picture} />
-          <div className="cell-10">
-            <h2 className="text-white font-bold">
-              {profile.firstName} {profile.lastName}
-            </h2>
-            <ul className="mx-0 px-0 list-none">
-              {roles.length > 0 ? (
-                roles.map((role, i) => (
-                  <li
-                    className="inline-block rounded-sm bg-lightGrey text-xs px-8 mr-8"
-                    key={i}
-                  >
-                    {role}
-                  </li>
-                ))
-              ) : (
-                <li
-                  className="inline-block text-lightGrey italic text-sm"
-                  key={0}
-                >
-                  No roles assigned
-                </li>
-              )}
-            </ul>
-          </div>
-        </GridContainer>
-      </div>
-      <GridContainer>
-        <h3 className={sectionHeader}>Profile</h3>
-        <div className="cell-12">
-          <p>Username: {profile.username}</p>
-          <p>Email: {profile.email}</p>
-          <p>Groups:</p>
-          <ul>
-            {groups.length > 0 ? (
-              groups.map((group, i) => (
-                <li
-                  className="inline-block rounded-sm bg-lightGrey text-xs px-8 mr-8"
-                  key={i}
-                >
-                  {group}
-                </li>
-              ))
-            ) : (
-              <li className="inline-block text-grey italic text-sm" key={0}>
-                No groups assigned
-              </li>
-            )}
-          </ul>
-        </div>
-        <h3 className={sectionHeader}>Notifications</h3>
-        <UpdateProfileForm
-          handleSubmit={handleSave}
-          defaultState={{
-            slackNotify: profile.slackNotify,
-            slackMemberId: profile.slackMemberId,
-          }}
-          errors={errors}
-          loading={submitting}
-          message={message}
-        />
-        <h3 className={sectionHeader}>Subscriptions</h3>
-        <p className="cell-12 font-title">
-          We'll notify you of daily activity when you subscribe to studies below
-        </p>
-        <div className="cell-12">
-          <StudySubscriptionContanier />
-        </div>
-      </GridContainer>
-    </>
+    <Container style={{marginTop: '30px'}}>
+      <Header as="h3">Your Profile</Header>
+      <Segment basic>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width="2" verticalAlign="middle">
+              <Image centered circular size={100} src={profile.picture} />
+            </Grid.Column>
+            <Label attached="top left">{roles.join(', ')}</Label>
+            <Grid.Column width="8">
+              <Form size="mini">
+                <Form.Group widths={2}>
+                  <Form.Field
+                    label={fields['username']}
+                    control="input"
+                    disabled
+                    value={profile['username']}
+                  />
+                  <Form.Field
+                    label={fields['email']}
+                    control="input"
+                    disabled
+                    value={profile['email']}
+                  />
+                </Form.Group>
+                <Form.Group widths={2}>
+                  <Form.Field
+                    label={fields['firstName']}
+                    control="input"
+                    disabled
+                    value={profile['firstName']}
+                  />
+                  <Form.Field
+                    label={fields['lastName']}
+                    control="input"
+                    disabled
+                    value={profile['lastName']}
+                  />
+                </Form.Group>
+              </Form>
+            </Grid.Column>
+            <Grid.Column width="6" textAlign="right">
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as="h3">Notifications</Header>
+      <UpdateProfileForm
+        handleSubmit={handleSave}
+        defaultState={{
+          slackNotify: profile.slackNotify,
+          slackMemberId: profile.slackMemberId,
+        }}
+        errors={errors}
+        loading={submitting}
+        message={message}
+      />
+      <Header as="h3">Your Subscriptions</Header>
+      <p>
+        We'll notify you of daily activity when you subscribe to studies below
+      </p>
+      <StudySubscriptionContanier />
+    </Container>
   );
 };
 
