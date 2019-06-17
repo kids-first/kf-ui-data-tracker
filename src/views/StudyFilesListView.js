60ffb16cb59dd2a9dd2bc0b5a4a37a7b73e1dd1e
Dan Kolbman <dankolbman@gmail.com>
2019-06-17 16:52:59 -0400

    :lipstick: Refactor files list layout

--- src/views/StudyFilesListView.js
+++ src/views/StudyFilesListView.js
@@ -4,48 +4,49 @@ import {GET_STUDY_BY_ID} from '../state/queries';
 import {UploadContainer} from '../containers';
 import FileList from '../components/FileList/FileList';
 import FileElement from '../components/FileList/FileElement';
-import {GridContainer} from 'kf-uikit';
+import {Grid, Header, Message} from 'semantic-ui-react';
 /**
  * List and manage files in a study and allow a user to upload more
  */
 const StudyFilesListView = props => (
   <Query query={GET_STUDY_BY_ID} variables={{kfId: props.match.params.kfId}}>
     {({loading, error, data}) => {
-      // TODO: add styled error state
       if (error)
         return (
-          <div>
-            <h3 className="text-red text-center">Error! </h3>
-            <p className="text-center">{error.message}</p>
-          </div>
+          <Message
+            negative
+            icon="warning circle"
+            header="Error"
+            content={error.message}
+          />
         );
       const files = !loading ? data.studyByKfId.files.edges : [];
       return (
-        <GridContainer collapsed="cells" className="my-20 px-12">
-          <h3 className="text-blue font-normal m-0 cell-12 row-1">
-            Upload Study Documents for DRC Approval
-          </h3>
-          <section className="study-file-list cell-12 row-2">
-            {loading ? (
-              <ul className="FileList">
-                <FileElement
-                  loading={loading}
-                  fileNode={{}}
-                  fileListId={props.match.params.kfId}
-                />
-              </ul>
-            ) : (
-              <FileList fileList={files} studyId={props.match.params.kfId} />
-            )}
-          </section>
-          <div className="row-3 cell-3-8">
-            <UploadContainer
-              handleUpload={file =>
-                props.history.push('documents/new-document', {file})
-              }
-            />
-          </div>
-        </GridContainer>
+        <Grid container columns={1} style={{paddingTop: '40px'}}>
+          <Grid.Column width={16}>
+            <Header as="h3">Upload Study Documents for DRC Approval</Header>
+            <Grid.Row>
+              {loading ? (
+                <ul className="FileList">
+                  <FileElement
+                    loading={loading}
+                    fileNode={{}}
+                    fileListId={props.match.params.kfId}
+                  />
+                </ul>
+              ) : (
+                <FileList fileList={files} studyId={props.match.params.kfId} />
+              )}
+            </Grid.Row>
+            <Grid.Row>
+              <UploadContainer
+                handleUpload={file =>
+                  props.history.push('documents/new-document', {file})
+                }
+              />
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
       );
     }}
   </Query>
