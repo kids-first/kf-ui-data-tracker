e266f22a097a8fecfe19f9a6b13ab85c5e4bac41
Dan Kolbman <dankolbman@gmail.com>
2019-06-18 11:14:15 -0400

    :lipstick: Refactor file list using semantic

--- src/components/FileList/FileElement.js
+++ src/components/FileList/FileElement.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from 'classnames';
 import {withRouter, Link} from 'react-router-dom';
-import CopyButton from '../CopyButton/CopyButton';
 import TimeAgo from 'react-timeago';
 import Badge from '../Badge/Badge';
-import {GridContainer} from 'kf-uikit';
+import {Header, Table, Icon, List} from 'semantic-ui-react';
+import CopyButton from '../CopyButton/CopyButton';
 import FileActionsContainer from '../../containers/FileActionsContainer';
 import {
   fileTypeDetail,
@@ -16,35 +15,7 @@ import {
 /**
  * Displays unordered study files in list view
  */
-const FileElement = ({className, fileNode, loading, match, fileListId}) => {
-  const fileElementClass = classes(
-    'FileList--Element',
-    {'cursor-not-allowed': loading},
-    'group',
-    className,
-  );
-  const fileStatusClass = classes(
-    {
-      'FileStatus--loading': loading,
-      FileStatus: !loading,
-    },
-    'w-full',
-    'inline-block',
-    'sm:inline',
-    'text-grey',
-  );
-  const fileNameClass = classes(
-    {
-      'FileStatus--loading': loading,
-      'FileList--Element-name': !loading,
-      'text-sm': !loading,
-    },
-    'font-bold',
-    'mb-0',
-    'mt-4',
-    'group-hover:text-blue',
-  );
-  const buttonClass = classes({invisible: loading});
+const FileElement = ({fileNode, loading, match, fileListId}) => {
   const fileKfID = fileNode.kfId || 'unknown ID';
   const fileName = fileNode.name || 'unknown file name';
   const fileDescription = fileNode.description || null;
@@ -56,71 +27,76 @@ const FileElement = ({className, fileNode, loading, match, fileListId}) => {
       ? fileTypeDetail[fileNode.fileType].title
       : 'unknown';
   return (
-    <li className={fileElementClass} tabIndex="0">
-      <GridContainer
-        fullWidth
-        collapsed="rows"
-        className="ml-0 mr-0 px-16 sm:p-0"
-      >
-        <div className=" cell-12 row-1 sm:row-1 sm:cell-2  sm:h-full sm:text-center  sm:border-r border-grey-lightest">
-          <Badge
-            className="ml-0"
-            state={
-              sortedVersions.length > 0 ? sortedVersions[0].node.state : null
-            }
-            loading={loading}
-          />
-        </div>
-
-        <div className="row-2 sm:row-1 cell-12 sm:cell-9   h-full">
-          <Link
-            to={`/study/${match.params.kfId}/documents/${fileKfID}`}
-            className="w-full no-underline text-black inline-block"
-            data-testid="edit-file"
-          >
-            <p className={fileNameClass}>{fileName}</p>
-            {fileDescription ? (
-              <p className="FileList--Element-desc w-3/4 h-8 m-0 p-0 text-truncate text-black">
-                <small>{fileDescription}</small>
-              </p>
-            ) : null}
-          </Link>
-          <span
-            className={fileStatusClass}
-            title={`Study ID: ${match.params.kfId}`}
-          >
-            {fileKfID} <CopyButton text={fileKfID} className={buttonClass} />
-          </span>
-          <span className={fileStatusClass}>
-            Created:{' '}
-            {latestDate ? (
-              <TimeAgo date={latestDate} live={false} />
-            ) : (
-              <span>unknown</span>
-            )}
-          </span>
-          <span className={fileStatusClass}>Size: {fileSize}</span>
-          <span className={fileStatusClass}>{fileType}</span>
-        </div>
-        <FileActionsContainer
-          className="opacity-25 hidden sm:block"
-          node={fileNode}
-          studyId={fileListId}
+    <Table.Row>
+      <Table.Cell singleLine collapsing textAlign="center">
+        <Badge
+          state={
+            sortedVersions.length > 0 ? sortedVersions[0].node.state : null
+          }
+          loading={loading}
         />
-      </GridContainer>
-    </li>
+      </Table.Cell>
+      <Table.Cell>
+        <Link
+          style={{display: 'block', width: '100%'}}
+          to={`/study/${match.params.kfId}/documents/${fileKfID}`}
+        >
+          <Header size="medium" as="span">
+            {fileName}
+          </Header>
+          <p
+            style={{
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              overflow: 'hidden',
+            }}
+          >
+            {fileDescription ? (
+              <>
+                {fileDescription.length > 100
+                  ? fileDescription.substring(0, 100) + '...'
+                  : fileDescription}
+              </>
+            ) : null}
+          </p>
+        </Link>
+        <List bulleted horizontal>
+          <List.Item>
+            <CopyButton basic size="mini" text={fileKfID} />
+          </List.Item>
+          <List.Item>
+            <List.Description>
+              {latestDate ? (
+                <>
+                  Created <TimeAgo date={latestDate} live={false} />
+                </>
+              ) : (
+                'Unknown time'
+              )}
+            </List.Description>
+          </List.Item>
+          <List.Item>
+            <List.Description>{fileSize}</List.Description>
+          </List.Item>
+          <List.Item>
+            <Icon name="hospital" />
+            {fileType}
+          </List.Item>
+        </List>
+      </Table.Cell>
+      <Table.Cell collapsing textAlign="right">
+        <FileActionsContainer node={fileNode} studyId={fileListId} />
+      </Table.Cell>
+    </Table.Row>
   );
 };
 
 FileList.propTypes = {
-  /** Any additional classes to be applied to the study list*/
-  className: PropTypes.string,
   /** Array of study object*/
   fileNode: PropTypes.object,
 };
 
 FileList.defaultProps = {
-  className: null,
   fileNode: null,
 };
 
