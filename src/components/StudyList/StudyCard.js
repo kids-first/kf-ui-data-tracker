d9a8d95952e15c7bf3c0d6eec000a8503d99a66b
Dan Kolbman <dankolbman@gmail.com>
2019-06-19 14:56:05 -0400

    :lipstick: Refactor study cards to use semantic

--- src/components/StudyList/StudyCard.js
+++ src/components/StudyList/StudyCard.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from 'classnames';
+import {Link} from 'react-router-dom';
 import TimeAgo from 'react-timeago';
-import {Card} from 'kf-uikit';
+import {Card} from 'semantic-ui-react';
 
 /**
  * Displays each study with its kfId, name(shortName), and modifiedAt
  */
 
-const StudyCard = ({className, title, body, lastUpdate}) => {
-  let studyCardClass = classes('StudyCard', 'hover:shadow-lg', className);
-
+const StudyCard = ({title, body, lastUpdate}) => {
   return (
-    <Card {...{title}} className={studyCardClass}>
-      <div className="flex flex-col justify-between">
-        <div className="overflow-hidden h-32">
-          <p className="m-0 font-title text-darkGrey">{body}</p>
-        </div>
-        <small className="w-full text-grey font-title text-right whitespace-no-wrap">
-          Updated: <TimeAgo className="whitespace-no-wrap" date={lastUpdate} />
-        </small>
-      </div>
+    <Card as={Link} to={`/study/${title}/documents`}>
+      <Card.Content>
+        <Card.Header>{title}</Card.Header>
+
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        Updated: <TimeAgo date={lastUpdate} />
+      </Card.Content>
     </Card>
   );
 };
 
 StudyCard.propTypes = {
-  /** Any additional classes to be applied to the study card */
-  className: PropTypes.string,
   /** Name to display as the card header */
   title: PropTypes.string,
   /** Text to display as the card body */
@@ -37,7 +33,6 @@ StudyCard.propTypes = {
 };
 
 StudyCard.defaultProps = {
-  className: null,
   title: null,
   body: null,
   lastUpdate: null,
--- src/components/StudyList/StudyGrid.js
+++ src/components/StudyList/StudyGrid.js
@@ -1,42 +1,46 @@
 import React from 'react';
-import classes from 'classnames';
-import {Icon} from 'kf-uikit';
-import {Link} from 'react-router-dom';
 import StudyCard from './StudyCard';
+import {Card, Placeholder} from 'semantic-ui-react';
 
-const StudyGrid = ({studyList, loading, className}) => {
-  let studyListClass = classes('StudyList', className);
+const GridSkeleton = () => (
+  <Card.Group stackable itemsPerRow={4}>
+    {[1, 2, 3, 4].map(i => (
+      <Card key={i}>
+        <Card.Content>
+          <Placeholder>
+            <Placeholder.Header>
+              <Placeholder.Line />
+            </Placeholder.Header>
+            <Placeholder.Paragraph>
+              <Placeholder.Line length="long" />
+            </Placeholder.Paragraph>
+          </Placeholder>
+        </Card.Content>
+        <Card.Content extra>
+          <Placeholder>
+            <Placeholder.Paragraph>
+              <Placeholder.Line length="short" />
+            </Placeholder.Paragraph>
+          </Placeholder>
+        </Card.Content>
+      </Card>
+    ))}
+  </Card.Group>
+);
+
+const StudyGrid = ({studyList, loading}) => {
+  if (loading) return <GridSkeleton />;
   return (
-    <ul className={studyListClass}>
-      {loading
-        ? [0, 1, 2, 3].map(n => (
-            <li
-              className="p-8 cursor-not-allowed w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-              key={n}
-            >
-              <div className="StudyCard--loading">
-                <Icon width={28} height={28} kind="study" />
-              </div>
-            </li>
-          ))
-        : studyList.map(node => (
-            <li
-              className="p-8 cursor-pointer w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-              key={node.node.id}
-            >
-              <Link
-                className="no-underline"
-                to={`/study/${node.node.kfId}/documents`}
-              >
-                <StudyCard
-                  title={node.node.kfId}
-                  body={node.node.name || node.node.shortName}
-                  lastUpdate={new Date(node.node.modifiedAt)}
-                />
-              </Link>
-            </li>
-          ))}
-    </ul>
+    <Card.Group stackable itemsPerRow={4}>
+      {studyList.map((node, i) => (
+        <StudyCard
+          key={i}
+          title={node.node.kfId}
+          body={node.node.name || node.node.shortName}
+          lastUpdate={new Date(node.node.modifiedAt)}
+        />
+      ))}
+    </Card.Group>
   );
 };
 
