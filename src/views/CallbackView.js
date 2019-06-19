6215cbe965322f058ea195b588ee9dd0e1215825
Dan Kolbman <dankolbman@gmail.com>
2019-06-19 13:04:59 -0400

    :lipstick: Add skeleton to callback view

--- src/views/CallbackView.js
+++ src/views/CallbackView.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import {auth} from '../state/auth';
+import {Container, Placeholder} from 'semantic-ui-react';
 
 const CallbackView = ({history}) => {
   auth.handleAuthentication(history);
-  return <div>Redirecting</div>;
+  return (
+    <>
+      <Placeholder fluid style={{height: 49, marginBottom: 50}}>
+        <Placeholder.Image />
+      </Placeholder>
+      <Container>
+        <Placeholder fluid>
+          <Placeholder.Header>
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Header>
+          <Placeholder.Paragraph>
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Paragraph>
+        </Placeholder>
+      </Container>
+    </>
+  );
 };
 
 export default CallbackView;
