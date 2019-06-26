83f47025df6f9d8c187298fd873fe90f391de554
Dan Kolbman <dankolbman@gmail.com>
2019-06-26 13:52:15 -0400

    :white_check_mark: Update tests

--- src/components/StudyList/__tests__/StudyList.test.js
+++ src/components/StudyList/__tests__/StudyList.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {MemoryRouter} from 'react-router-dom';
-import {render} from 'react-testing-library';
-import allStudies from './allStudies.json';
+import {render, cleanup} from 'react-testing-library';
+import allStudies from '../../../../__mocks__/kf-api-study-creator/responses/allStudies.json';
 import StudyList from '../StudyList';
+
+afterEach(cleanup);
 
 it('renders correctly', () => {
   const studies = allStudies.data.allStudies.edges;
@@ -15,7 +17,7 @@ it('renders correctly', () => {
   expect(tree.container).toMatchSnapshot();
 
   const cards = tree.container.querySelectorAll('.ui .card');
-  expect(cards.length).toBe(3);
+  expect(cards.length).toBe(4);
 });
 
 it('renders loading state', () => {
