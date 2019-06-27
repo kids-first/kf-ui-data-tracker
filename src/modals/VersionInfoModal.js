a4116ce1f959e51ebf24e99203194cd9fe30ccd5
XuTheBunny <xzhu.fg@gmail.com>
2019-06-27 16:00:54 -0400

    💄 Use semantic components on version info modal

--- src/modals/VersionInfoModal.js
+++ src/modals/VersionInfoModal.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import TimeAgo from 'react-timeago';
-import Modal from '../components/Modal/Modal';
 import SvgIcon from '../components/Icon/Icon';
 import Badge from '../components/Badge/Badge';
-import {GridContainer, Avatar} from 'kf-uikit';
+import {
+  Button,
+  Header,
+  Modal,
+  Icon,
+  Grid,
+  Label,
+  Statistic,
+} from 'semantic-ui-react';
 import {
   downloadFile,
   fileTypeDetail,
@@ -19,93 +26,126 @@ const VersionInfoModal = ({
   downloadFileMutation,
 }) => {
   return (
-    <Modal
-      title={`Document Versions: ${fileNode.name}`}
-      submitText="DOWNLOAD"
-      onSubmit={() =>
-        downloadFile(
-          studyId,
-          fileNode.kfId,
-          openedVersion.version.kfId,
-          downloadFileMutation,
-        )
-      }
-      onCloseModal={onCloseDialog}
-      footerButton={
-        <button className="FileVersionList--Button" onClick={onUploadClick}>
-          <SvgIcon kind="Upload" height="15" width="15" className="mr-4" />
+    <Modal open={true} onClose={onCloseDialog} closeIcon>
+      <Header content={`Document Versions: ${fileNode.name}`} />
+      <Modal.Content scrolling>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Header size="small">{openedVersion.version.fileName}</Header>
+              {openedVersion.index === 0 && (
+                <Label tag size="mini" color="purple" float="right">
+                  LATEST
+                </Label>
+              )}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row stretched>
+            <Grid.Column mobile={8} tablet={3} computer={4}>
+              <Statistic size="mini">
+                <Statistic.Label>Status</Statistic.Label>
+                <Statistic.Value>
+                  <Badge state={openedVersion.version.state} />
+                </Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column mobile={8} tablet={5} computer={5}>
+              <Statistic size="mini">
+                <Statistic.Label>Document Type</Statistic.Label>
+                <Statistic.Value>
+                  <Label icon basic size="tiny">
+                    <SvgIcon
+                      kind={fileTypeDetail[fileNode.fileType].icon}
+                      width="12"
+                      height="12"
+                    />
+                    <Label.Detail>
+                      {fileTypeDetail[fileNode.fileType].title}
+                    </Label.Detail>
+                  </Label>
+                </Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column mobile={8} tablet={4} computer={4}>
+              <Statistic size="mini">
+                <Statistic.Label>Last Updated</Statistic.Label>
+                <Statistic.Value>
+                  <Label image size="tiny">
+                    <img
+                      alt={
+                        openedVersion.version.creator
+                          ? openedVersion.version.creator.username
+                          : 'unknown'
+                      }
+                      src={
+                        openedVersion.version.creator.picture
+                          ? openedVersion.version.creator.picture
+                          : 'https://www.w3schools.com/css/img_avatar.png'
+                      }
+                    />
+                    {openedVersion.version.creator.username}
+                    <Label.Detail>
+                      {openedVersion.version.createdAt ? (
+                        <TimeAgo
+                          date={openedVersion.version.createdAt}
+                          live={false}
+                        />
+                      ) : (
+                        'Unknow'
+                      )}
+                    </Label.Detail>
+                  </Label>
+                </Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column mobile={8} tablet={3} computer={3}>
+              <Statistic size="mini">
+                <Statistic.Label>Size</Statistic.Label>
+                <Statistic.Value>
+                  <Label basic size="tiny">
+                    {formatFileSize(openedVersion.version.size)}
+                  </Label>
+                </Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Header size="tiny">Change Summary</Header>
+              {openedVersion.version.description}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          primary
+          icon
+          labelPosition="left"
+          size="mini"
+          onClick={onUploadClick}
+        >
+          <Icon name="cloud upload" />
           UPLOAD VERSION
-        </button>
-      }
-    >
-      {openedVersion.index === 0 && (
-        <div className="FileVersionElement--Tag">Latest</div>
-      )}
-      <h3 className="FileTitle py-8">{openedVersion.version.fileName}</h3>
-      <GridContainer centered={false} className="px-0 mb-20">
-        <div className="cell-6 lg:cell-3">
-          <p className="FileInfo--Title">Status:</p>
-          <Badge state={openedVersion.version.state} />
-        </div>
-        <div className="cell-6 lg:cell-3">
-          <p className="FileInfo--Title">File Type:</p>
-          <div className="flex">
-            <div className="FileInfo--Icon">
-              <SvgIcon
-                kind={fileTypeDetail[fileNode.fileType].icon}
-                width="24"
-                height="24"
-              />
-            </div>
-            <p className="FileInfo--Text">
-              {fileTypeDetail[fileNode.fileType].title}
-            </p>
-          </div>
-        </div>
-        <div className="cell-6 lg:cell-3">
-          <p className="FileInfo--Title">Updated:</p>
-          {openedVersion.version.createdAt ? (
-            <div className="flex">
-              <p className="FileInfo--Text">
-                <TimeAgo
-                  date={openedVersion.version.createdAt}
-                  live={false}
-                  className="mr-4"
-                />
-                by
-              </p>
-              <Avatar
-                className="inline-block ml-4 mt-4"
-                size={20}
-                imgUrl={
-                  openedVersion.version.creator
-                    ? openedVersion.version.creator.picture
-                    : 'https://www.w3schools.com/css/img_avatar.png'
-                }
-                userName={
-                  openedVersion.version.creator &&
-                  openedVersion.version.creator.username
-                }
-                userEmail={
-                  openedVersion.version.creator &&
-                  openedVersion.version.creator.email
-                }
-              />
-            </div>
-          ) : (
-            <p className="FileInfo--Text">Unknown</p>
-          )}
-        </div>
-        <div className="cell-6 lg:cell-3">
-          <p className="FileInfo--Title">Size:</p>
-          <p className="FileInfo--Text">
-            {formatFileSize(openedVersion.version.size)}
-          </p>
-        </div>
-      </GridContainer>
-      <p className="FileInfo--Title">Change Summary:</p>
-      <p>{openedVersion.version.description}</p>
-      <div className="h-8 w-48" />
+        </Button>
+        <Button
+          primary
+          icon
+          labelPosition="left"
+          size="mini"
+          onClick={() =>
+            downloadFile(
+              studyId,
+              fileNode.kfId,
+              openedVersion.version.kfId,
+              downloadFileMutation,
+            )
+          }
+        >
+          <Icon name="download" />
+          DOWNLOAD
+        </Button>
+      </Modal.Actions>
     </Modal>
   );
 };
