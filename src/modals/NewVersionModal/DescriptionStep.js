aa1e43f89a8b317208b8f1241fa971bbf9e44867
XuTheBunny <xzhu.fg@gmail.com>
2019-06-27 16:00:59 -0400

    💄 Use semantic components on version upload modal

--- src/modals/NewVersionModal/DescriptionStep.js
+++ src/modals/NewVersionModal/DescriptionStep.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {formatFileSize} from '../../common/fileUtils';
-
+import {Form, Label, Message} from 'semantic-ui-react';
 /**
  * In this step, the user will describ the file that they have uploaded
  */
 const DescriptionStep = ({file, handleDescription}) => {
   return (
     <>
-      <h3 className="text-blue font-bold font-body">Summarize your changes</h3>
-      <p className="mb-32 font-title ml-8">
+      <Message compact info>
         Help keep track of your document history by telling us what may have
         changed in this version
-      </p>
-      <b>Uploaded File:</b>
-      <p className="mb-32 font-title ml-8">
-        {file.name}
-        <span className="text-mediumGrey text-xs pl-16">
-          {formatFileSize(file.size)}
-        </span>
-      </p>
-      <div>
-        <label className="font-bold Form--Label-required" htmlFor="description">
-          Summarize document changes (required):
-        </label>
-        <textarea
-          data-testid="description-input"
-          name="description"
-          type="text"
-          className="Form--TextArea--dialog"
-          onChange={ev => handleDescription(ev.target.value)}
-        />
-      </div>
+      </Message>
+      <Form>
+        <Form.Field>
+          <label>Uploaded File:</label>
+          <p>
+            {file.name}
+            <Label size="mini" basic pointing="left">
+              {formatFileSize(file.size)}
+            </Label>
+          </p>
+        </Form.Field>
+        <Form.Field required>
+          <label>Summarize document changes:</label>
+          <Form.TextArea
+            data-testid="description-input"
+            name="description"
+            type="text"
+            onChange={ev => handleDescription(ev.target.value)}
+          />
+        </Form.Field>
+      </Form>
     </>
   );
 };
