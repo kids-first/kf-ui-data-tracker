8348ebcf1ca2e0636572b81889265c8eac1c4376
Dan Kolbman <dankolbman@gmail.com>
2019-07-01 17:51:31 -0400

    :lipstick: Add loading skeleton for file list

--- src/views/StudyFilesListView.js
+++ src/views/StudyFilesListView.js
@@ -3,8 +3,38 @@ import {Query} from 'react-apollo';
 import {GET_STUDY_BY_ID} from '../state/queries';
 import {UploadContainer} from '../containers';
 import FileList from '../components/FileList/FileList';
-import FileElement from '../components/FileList/FileElement';
-import {Divider, Grid, Header, Message} from 'semantic-ui-react';
+import {Divider, Grid, Header, Message, Placeholder} from 'semantic-ui-react';
+
+/**
+ * A place holder skeleton for a list of files
+ */
+const StudyListSkeleton = () => (
+  <Grid divided="vertically">
+    {[1, 2, 3].map(i => (
+      <Grid.Row key={i} style={{height: '90px'}}>
+        <Grid.Column width="2">
+          <Placeholder style={{width: '100%', height: '40px'}}>
+            <Placeholder.Image />
+          </Placeholder>
+        </Grid.Column>
+        <Grid.Column width="8">
+          <Placeholder>
+            <Placeholder.Header>
+              <Placeholder.Line length="medium" />
+              <Placeholder.Line length="full" />
+            </Placeholder.Header>
+          </Placeholder>
+        </Grid.Column>
+        <Grid.Column width="6" textAlign="right">
+          <Placeholder style={{float: 'right', width: '100px', height: '40px'}}>
+            <Placeholder.Image />
+          </Placeholder>
+        </Grid.Column>
+      </Grid.Row>
+    ))}
+  </Grid>
+);
+
 /**
  * List and manage files in a study and allow a user to upload more
  */
@@ -26,7 +56,7 @@ const StudyFilesListView = props => (
           <Grid.Column width={16}>
             <Header as="h3">Upload Study Documents for DRC Approval</Header>
             {loading ? (
-              'Loading...'
+              <StudyListSkeleton />
             ) : (
               <FileList fileList={files} studyId={props.match.params.kfId} />
             )}
