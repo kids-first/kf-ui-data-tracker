915bf4e3ec4424cf2a0d58a3765ae84635ef4412
XuTheBunny <xzhu.fg@gmail.com>
2019-07-09 11:33:35 -0400

    💄 Use semantic components on file detail view base

--- src/views/FileDetailView.js
+++ src/views/FileDetailView.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import FileDetailContainer from '../containers/FileDetailContainer';
-import {GridContainer} from 'kf-uikit';
+import {Container, Segment} from 'semantic-ui-react';
 
 const FileDetailView = ({match}) => {
   const fileId = match.params.fileId;
   return (
-    <GridContainer collapsed="cells" className="my-20 px-12">
-      <section className="study-file-detail cell-12">
-        <FileDetailContainer kfId={fileId} />
-      </section>
-    </GridContainer>
+    <Container as={Segment} basic vertical>
+      <FileDetailContainer kfId={fileId} />
+    </Container>
   );
 };
 
