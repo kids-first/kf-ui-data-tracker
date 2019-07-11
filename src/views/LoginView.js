ccc6594a30ade4a388c81e92d3df6101fad1c8d2
bdolly <bromeodolly@gmail.com>
2019-07-11 14:23:23 -0400

    :art: use 12 column semantic grid
    Use proper grid rows and columns for the layout

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import {Header, Icon, Image, Grid, Message, Segment} from 'semantic-ui-react';
-import {LoginCard} from '../components/LoginCard';
+import { Header, Icon, Image, Grid, Message, Segment } from 'semantic-ui-react';
+import { LoginCard } from '../components/LoginCard';
 import logo from '../assets/logo.svg';
+
 
 const LoginView = () => {
   return (
-    <Grid
-      stretched
-      className="View--Login"
-      textAlign="center"
-      style={{height: '100vh'}}
-      verticalAlign="middle"
-    >
-      <Grid.Column computer="8" tablet="12" mobile="15">
-        <Segment>
-          <Header as="h1">
-            <Image src={logo} />
-            <Header.Content>Kids First Data Tracker</Header.Content>
-          </Header>
-          <LoginCard />
-          <Message>
-            New to Kids First Data Tracker?
-            <a href="/">
-              Join now <Icon name="chevron right" />
-            </a>
-          </Message>
-        </Segment>
-      </Grid.Column>
-    </Grid>
+    <section className="View--Login">
+      <Grid
+        centered
+        columns={12}
+        className="h-screen"
+      >
+        <Grid.Row verticalAlign="middle">
+          <Grid.Column width={4}>
+            <Segment textAlign="center" raised>
+              <Header as="h1">
+                <Image src={logo} />
+                <Header.Content>Kids First Data Tracker</Header.Content>
+              </Header>
+              <LoginCard />
+              <Message>
+                New to Kids First Data Tracker?
+                <a href="mailto:support@kidsfirstdrc.org?Subject=DRC%20Data%20Tracker%20Access%20Request">
+                  Join now <Icon name="chevron right" />
+                </a>
+              </Message>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </section>
   );
-};
+}
 
 export default LoginView;
