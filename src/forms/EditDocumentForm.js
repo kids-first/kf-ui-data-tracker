a33140a455cf9ab3fe09e1cea4ad03fe5b4a4a4e
XuTheBunny <xzhu.fg@gmail.com>
2019-07-11 16:25:48 -0400

    💄 Use semantic component on edit file form

--- src/forms/EditDocumentForm.js
+++ src/forms/EditDocumentForm.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import classes from 'classnames';
-import {GridContainer, Icon} from 'kf-uikit';
 import SelectElement from '../components/FileAnnotation/SelectElement';
 import Badge from '../components/Badge/Badge';
 import {fileTypeDetail, versionState} from '../common/fileUtils';
+import {Form, TextArea, Dropdown} from 'semantic-ui-react';
 /**
  * Form to edit document information or annotate new file for uploading
  */
@@ -21,97 +20,91 @@ const EditDocumentForm = ({
   errors,
   submitButtons,
 }) => {
-  const versionStatusList = Object.keys(versionState);
-  const dropdownClass = classes('Dropdown');
+  const [newFileType, setFileType] = useState();
+  const [newFileName, setFileName] = useState();
+  const [newFileDescription, setFileDescription] = useState();
+  const [onUploading, setUploading] = useState(false);
+  const options = Object.keys(versionState).map(state => ({
+    key: state,
+    value: state,
+    text: versionState[state].title,
+    content: <Badge state={state} />,
+  }));
   return (
-    <form className="pt-16">
-      <GridContainer>
-        <label
-          className="Form--Label row-1 cell-3 text-right Form--Label-required"
-          htmlFor="file_name"
-        >
-          Document Title:
-        </label>
+    <Form
+      onSubmit={ev => {
+        if (handleSubmit) {
+          setUploading(true);
+          ev.preventDefault();
+          handleSubmit(newFileName, newFileType, newFileDescription);
+        }
+      }}
+    >
+      <Form.Field required>
+        <label htmlFor="file_title">Document Title:</label>
         <input
           data-testid="name-input"
-          className="Form--Input border border-grey row-1 cell-9"
           type="text"
           name="file_name"
           placeholder="Phenotypic Data manifest for..."
           defaultValue={fileName}
-          onChange={e => onNameChange(e)}
+          onChange={e => {
+            onNameChange ? onNameChange(e) : setFileName(e.target.value);
+          }}
         />
-        <label
-          className="Form--Label row-2 cell-3 text-right"
-          htmlFor="file_name"
-        >
-          Approval Status:
-        </label>
-        <div className="row-2 cell-9">
-          <div className={dropdownClass}>
-            <button
-              type="button"
-              className="Dropdown--button justify-start pl-20"
-              tabIndex="0"
-              data-testid="status-dropdown"
-            >
-              <Badge state={versionStatus} />
-              <Icon
-                className="m-12"
-                width={20}
-                height={20}
-                kind="chevron-down"
-              />
-            </button>
-            <div className="Dropdown--container">
-              <ul className="Dropdown--list">
-                {versionStatusList.map(versionStatusValue => (
-                  <button
-                    onClick={() => onVersionStatusChange(versionStatusValue)}
-                    type="button"
-                    className="Dropdown--item"
-                    tabIndex="0"
-                    key={versionStatusValue}
-                  >
-                    <Badge state={versionStatusValue} />
-                  </button>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <label
-          className="Form--Label row-3 cell-3 text-right Form--Label-required"
-          htmlFor="file_type"
-        >
-          Document Type:
-        </label>
-        <fieldset name="file_type" className="row-3 cell-9">
-          {Object.keys(fileTypeDetail).map(item => (
+      </Form.Field>
+      {versionStatus && (
+        <Form.Field>
+          <label>Approval Status:</label>
+          <Dropdown
+            selection
+            fluid
+            options={options}
+            value={versionStatus}
+            placeholder="Choose an option"
+            onChange={(e, {value}) => {
+              onVersionStatusChange(value);
+            }}
+          />
+        </Form.Field>
+      )}
+      <Form.Field required>
+        <label htmlFor="file_type">Document Type:</label>
+
+        {Object.keys(fileTypeDetail).map(item => (
+          <Form.Field key={item}>
             <SelectElement
-              key={item}
               value={item}
-              select={e => onFileTypeChange(e)}
-              selected={fileType === item}
+              selected={
+                onFileTypeChange ? fileType === item : newFileType === item
+              }
+              select={value => {
+                onFileTypeChange ? onFileTypeChange(value) : setFileType(item);
+              }}
             />
-          ))}
-        </fieldset>
-        <label
-          className="Form--Label row-4 cell-3 text-right Form--Label-required"
-          htmlFor="description"
-        >
-          Describe document contents:
-        </label>
-        <textarea
+          </Form.Field>
+        ))}
+      </Form.Field>
+      <Form.Field required>
+        <label>Describe document contents:</label>
+        <TextArea
           data-testid="description-input"
-          className="Form--TextArea border border-grey w-full row-4 cell-9"
           type="text"
           name="description"
           defaultValue={fileDescription}
-          onChange={e => onDescriptionChange(e)}
+          onChange={e => {
+            onDescriptionChange
+              ? onDescriptionChange(e)
+              : setFileDescription(e.target.value);
+          }}
         />
-      </GridContainer>
-    </form>
+      </Form.Field>
+      {submitButtons &&
+        submitButtons(
+          !newFileName || !newFileType || !newFileDescription || onUploading,
+          onUploading,
+        )}
+    </Form>
   );
 };
 
@@ -120,18 +113,24 @@ EditDocumentForm.propTypes = {
   fileName: PropTypes.string,
   /** The description for the file */
   fileDescription: PropTypes.string,
-  /** The value of the currently selected fileType's enum*/
+  /** The value of the currently selected fileType's enum */
   fileType: PropTypes.string,
+  /** The file approval status */
+  versionStatus: PropTypes.string,
   /** Action to perform when name input is updated */
   onNameChange: PropTypes.func,
   /** Action to perform when description input is updated */
   onDescriptionChange: PropTypes.func,
   /** Action to perform when fileType input is updated */
   onFileTypeChange: PropTypes.func,
+  /** Action to perform when file approval status is updated */
+  onVersionStatusChange: PropTypes.func,
   /** (New file) Function to perform on form submission  */
   handleSubmit: PropTypes.func,
   /** (New file) Any errors that occured when submitting the form */
   errors: PropTypes.string,
+  /** (New file) Displays as buttons for form submitting */
+  submitButtons: PropTypes.func,
 };
 
 export default EditDocumentForm;
