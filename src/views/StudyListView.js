14daa129940518d597c47ada4501e5b2f46271ba
XuTheBunny <xzhu.fg@gmail.com>
2019-07-11 11:39:07 -0400

    💄 Semantic error and warning message on Study list view

--- src/views/StudyListView.js
+++ src/views/StudyListView.js
@@ -2,18 +2,33 @@ import React from 'react';
 import {Query} from 'react-apollo';
 import {ALL_STUDIES} from '../state/queries';
 import StudyList from '../components/StudyList/StudyList';
+import {Message, Container, Segment} from 'semantic-ui-react';
 
 const StudyListView = () => (
   <Query query={ALL_STUDIES}>
     {({loading, error, data}) => {
-      if (error) return `Error! ${error.message}`;
+      if (error)
+        return (
+          <Container as={Segment} basic>
+            <Message
+              negative
+              icon="warning circle"
+              header="Error"
+              content={error.message}
+            />
+          </Container>
+        );
       const studyList = !loading ? data.allStudies.edges : [];
       if (!loading && studyList.length === 0)
         return (
-          <div className="text-center text-mediumGrey px-8 pt-32">
-            <h1>You don't have access to any studies yet.</h1>
-            <h3>Your account is being reviewed for the proper permissions.</h3>
-          </div>
+          <Container as={Segment} basic>
+            <Message
+              warning
+              icon="warning circle"
+              header="You don't have access to any studies yet."
+              content="Your account is being reviewed for the proper permissions."
+            />
+          </Container>
         );
       return <StudyList studyList={studyList} loading={loading} />;
     }}
