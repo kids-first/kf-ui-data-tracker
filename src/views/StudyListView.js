4297623d6ae31e1131b25bdd6829faec507cb4c4
Dan Kolbman <dankolbman@gmail.com>
2019-07-16 11:06:00 -0400

    :recycle: Replace Query with HOC in study list
    Replaces the <Query /> component with a graphql HOC in the study view to
    be more consisntent with other views.

--- src/views/StudyListView.js
+++ src/views/StudyListView.js
@@ -1,38 +1,34 @@
 import React from 'react';
-import {Query} from 'react-apollo';
+import {graphql} from 'react-apollo';
 import {ALL_STUDIES} from '../state/queries';
 import StudyList from '../components/StudyList/StudyList';
 import {Message, Container, Segment} from 'semantic-ui-react';
 
-const StudyListView = () => (
-  <Query query={ALL_STUDIES}>
-    {({loading, error, data}) => {
-      if (error)
-        return (
-          <Container as={Segment} basic>
-            <Message
-              negative
-              icon="warning circle"
-              header="Error"
-              content={error.message}
-            />
-          </Container>
-        );
-      const studyList = !loading ? data.allStudies.edges : [];
-      if (!loading && studyList.length === 0)
-        return (
-          <Container as={Segment} basic>
-            <Message
-              warning
-              icon="warning circle"
-              header="You don't have access to any studies yet."
-              content="Your account is being reviewed for the proper permissions."
-            />
-          </Container>
-        );
-      return <StudyList studyList={studyList} loading={loading} />;
-    }}
-  </Query>
-);
+const StudyListView = ({studies: {loading, allStudies, error}}) => {
+  if (error)
+    return (
+      <Container as={Segment} basic>
+        <Message
+          negative
+          icon="warning circle"
+          header="Error"
+          content={error.message}
+        />
+      </Container>
+    );
+  const studyList = !loading ? allStudies.edges : [];
+  if (!loading && studyList.length === 0)
+    return (
+      <Container as={Segment} basic>
+        <Message
+          warning
+          icon="warning circle"
+          header="You don't have access to any studies yet."
+          content="Your account is being reviewed for the proper permissions."
+        />
+      </Container>
+    );
+  return <StudyList studyList={studyList} loading={loading} />;
+};
 
-export default StudyListView;
+export default graphql(ALL_STUDIES, {name: 'studies'})(StudyListView);
