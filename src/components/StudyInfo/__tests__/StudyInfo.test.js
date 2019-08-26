fa16827a8a66dcb6a9b15864ec1ebe1c5da19ef5
XuTheBunny <xzhu.fg@gmail.com>
2019-08-26 15:21:54 -0400

    📷 Add test for study info page

---
+++ src/components/StudyInfo/__tests__/StudyInfo.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {MemoryRouter} from 'react-router-dom';
+import {render} from 'react-testing-library';
+import {mocks} from '../../../../__mocks__/kf-api-study-creator/mocks';
+import studyByKfId from '../../../../__mocks__/kf-api-study-creator/responses/studyByKfId';
+import {MockedProvider} from 'react-apollo/test-utils';
+import StudyInfo from '../StudyInfo';
+
+it('Study basic info page renders correctly', () => {
+  const study = studyByKfId.data.studyByKfId;
+  const tree = render(
+    <MockedProvider mocks={mocks}>
+      <MemoryRouter>
+        <StudyInfo studyNode={study} />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  expect(tree.container).toMatchSnapshot();
+});
