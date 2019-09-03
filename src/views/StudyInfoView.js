66c923633d864e6346a7cab1aafdb00832e7c11c
XuTheBunny <xzhu.fg@gmail.com>
2019-09-03 13:10:22 -0400

    ✨ Execute GET_PROJECTS, LINK/UNLINK_PROJECT queries from StudyInfoView

--- src/views/StudyInfoView.js
+++ src/views/StudyInfoView.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {graphql, compose} from 'react-apollo';
-import {GET_STUDY_BY_ID, MY_PROFILE} from '../state/queries';
-import {UPDATE_STUDY} from '../state/mutations';
+import {GET_STUDY_BY_ID, MY_PROFILE, GET_PROJECTS} from '../state/queries';
+import {UPDATE_STUDY, LINK_PROJECT, UNLINK_PROJECT} from '../state/mutations';
 import StudyInfo from '../components/StudyInfo/StudyInfo';
 import {Container, Segment, Message, Placeholder} from 'semantic-ui-react';
 import EmptyView from './EmptyView';
@@ -10,8 +10,11 @@ import NewProjectModal from '../modals/NewProjectModal';
 
 const StudyInfoView = ({
   study: {loading, studyByKfId, error},
+  projects: {allProjects, projectLoading, projectError},
   user,
   updateStudy,
+  linkProject,
+  unlinkProject,
 }) => {
   const isBeta = !user.loading ? user.myProfile.roles.includes('BETA') : false;
   const [showModal, setShowModal] = useState(false);
@@ -86,5 +89,45 @@ export default compose(
   graphql(UPDATE_STUDY, {
     name: 'updateStudy',
   }),
+  graphql(LINK_PROJECT, {
+    name: 'linkProject',
+    options: props => ({
+      refetchQueries: [
+        {
+          query: GET_STUDY_BY_ID,
+          variables: {
+            kfId: props.match.params.kfId,
+          },
+        },
+      ],
+    }),
+  }),
+  graphql(UNLINK_PROJECT, {
+    name: 'unlinkProject',
+    options: props => ({
+      refetchQueries: [
+        {
+          query: GET_STUDY_BY_ID,
+          variables: {
+            kfId: props.match.params.kfId,
+          },
+        },
+        {
+          query: GET_PROJECTS,
+          variables: {
+            study: '',
+          },
+        },
+      ],
+    }),
+  }),
   graphql(MY_PROFILE, {name: 'user'}),
+  graphql(GET_PROJECTS, {
+    name: 'projects',
+    options: props => ({
+      variables: {
+        study: '',
+      },
+    }),
+  }),
 )(StudyInfoView);
