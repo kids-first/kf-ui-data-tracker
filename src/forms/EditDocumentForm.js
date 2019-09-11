ef33ce9c824e869df7de30993e64f693330cc101
bdolly <bromeodolly@gmail.com>
2019-09-11 15:37:05 -0400

    :recycle: use Formik for EditDocumentForm
    migrated EditDocumentForm to use Formik in order to more easily manage values and validation. This also makes the rendered form element a forwardRef for easy access

--- src/forms/EditDocumentForm.js
+++ src/forms/EditDocumentForm.js
@@ -1,120 +1,120 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+
+import {Formik, Field} from 'formik';
 import SelectElement from '../components/FileAnnotation/SelectElement';
 import Badge from '../components/Badge/Badge';
 import {fileTypeDetail, versionState} from '../common/fileUtils';
 import {Form, TextArea, Dropdown} from 'semantic-ui-react';
+
 /**
  * Form to edit document information or annotate new file for uploading
  */
-const EditDocumentForm = ({
-  fileType,
-  fileName,
-  fileDescription,
-  versionStatus,
-  onFileTypeChange,
-  onNameChange,
-  onDescriptionChange,
-  isAdmin,
-  onVersionStatusChange,
-  handleSubmit,
-  errors,
-  submitButtons,
-}) => {
-  const [newFileType, setFileType] = useState();
-  const [newFileName, setFileName] = useState();
-  const [newFileDescription, setFileDescription] = useState();
-  const [onUploading, setUploading] = useState(false);
-  const options = Object.keys(versionState).map(state => ({
-    key: state,
-    value: state,
-    text: versionState[state].title,
-    content: <Badge state={state} />,
-  }));
-  return (
-    <Form
-      onSubmit={ev => {
-        if (handleSubmit) {
+const EditDocumentForm = React.forwardRef(
+  (
+    {
+      fileType,
+      fileName,
+      fileDescription,
+      versionStatus,
+      isAdmin,
+      handleSubmit,
+      submitButtons,
+    },
+    ref,
+  ) => {
+    const [onUploading, setUploading] = useState(false);
+    const options = Object.keys(versionState).map(state => ({
+      key: state,
+      value: state,
+      text: versionState[state].title,
+      content: <Badge state={state} />,
+    }));
+    return (
+      <Formik
+        initialValues={{
+          file_name: fileName,
+          file_type: fileType,
+          file_desc: fileDescription,
+          file_status: versionStatus,
+        }}
+        onSubmit={values => {
           setUploading(true);
-          ev.preventDefault();
-          handleSubmit(newFileName, newFileType, newFileDescription);
-        }
-      }}
-    >
-      <Form.Field required>
-        <label htmlFor="file_title">Document Title:</label>
-        <input
-          data-testid="name-input"
-          type="text"
-          name="file_name"
-          placeholder="Phenotypic Data manifest for..."
-          defaultValue={fileName}
-          onChange={e => {
-            onNameChange ? onNameChange(e) : setFileName(e.target.value);
-          }}
-        />
-      </Form.Field>
-      {versionStatus && (
-        <Form.Field>
-          <label>Approval Status:</label>
-          {isAdmin ? (
-            <Dropdown
-              selection
-              fluid
-              options={options}
-              value={versionStatus}
-              placeholder="Choose an option"
-              onChange={(e, {value}) => {
-                onVersionStatusChange(value);
-              }}
-            />
-          ) : (
-            <Badge state={versionStatus} />
-          )}
-        </Form.Field>
-      )}
-      <Form.Field required>
-        <label htmlFor="file_type">Document Type:</label>
-        {Object.keys(fileTypeDetail).map(item => (
-          <Form.Field key={item}>
-            <SelectElement
-              value={item}
-              selected={
-                onFileTypeChange ? fileType === item : newFileType === item
-              }
-              select={value => {
-                if (isAdmin) {
-                  onFileTypeChange
-                    ? onFileTypeChange(value)
-                    : setFileType(item);
-                }
-              }}
-            />
-          </Form.Field>
-        ))}
-      </Form.Field>
-      <Form.Field required>
-        <label>Describe Document Contents:</label>
-        <TextArea
-          data-testid="description-input"
-          type="text"
-          name="description"
-          defaultValue={fileDescription}
-          onChange={e => {
-            onDescriptionChange
-              ? onDescriptionChange(e)
-              : setFileDescription(e.target.value);
-          }}
-        />
-      </Form.Field>
-      {submitButtons &&
-        submitButtons(
-          !newFileName || !newFileType || !newFileDescription || onUploading,
-          onUploading,
+          handleSubmit(...Object.values(values));
+        }}
+      >
+        {({values, handleChange, handleSubmit, setFieldValue}) => (
+          <Form onSubmit={handleSubmit} ref={ref}>
+            <Form.Field required>
+              <label htmlFor="file_name">Document Title:</label>
+              <input
+                data-testid="name-input"
+                type="text"
+                name="file_name"
+                placeholder="Phenotypic Data manifest for..."
+                value={values.file_name}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            {versionStatus && (
+              <Form.Field>
+                <label>Approval Status:</label>
+                {isAdmin ? (
+                  <Dropdown
+                    selection
+                    fluid
+                    name="file_status"
+                    options={options}
+                    value={values.file_status}
+                    placeholder="Choose an option"
+                    onChange={(e, {value}) => {
+                      setFieldValue('file_status', value);
+                    }}
+                  />
+                ) : (
+                  <Badge state={versionStatus} />
+                )}
+              </Form.Field>
+            )}
+
+            <Form.Field required>
+              <label htmlFor="file_type">Document Type:</label>
+              {Object.keys(fileTypeDetail).map(item => (
+                <Form.Field key={item}>
+                  <Field
+                    component={SelectElement}
+                    name="file_type"
+                    id={item}
+                    label={item}
+                  />
+                </Form.Field>
+              ))}
+            </Form.Field>
+
+            <Form.Field required>
+              <label>Describe Document Contents:</label>
+              <TextArea
+                data-testid="description-input"
+                type="text"
+                name="file_desc"
+                value={values.file_desc}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            {submitButtons &&
+              submitButtons(
+                !Object.values(values).every(x => {
+                  console.log(x);
+                  return Boolean(x !== undefined);
+                }),
+                onUploading,
+              )}
+          </Form>
         )}
-    </Form>
-  );
-};
+      </Formik>
+    );
+  },
+);
 
 EditDocumentForm.propTypes = {
   /** The title of the file */
@@ -127,14 +127,6 @@ EditDocumentForm.propTypes = {
   versionStatus: PropTypes.string,
   /** If the user has ADMIN role */
   isAdmin: PropTypes.bool.isRequired,
-  /** Action to perform when name input is updated */
-  onNameChange: PropTypes.func,
-  /** Action to perform when description input is updated */
-  onDescriptionChange: PropTypes.func,
-  /** Action to perform when fileType input is updated */
-  onFileTypeChange: PropTypes.func,
-  /** Action to perform when file approval status is updated */
-  onVersionStatusChange: PropTypes.func,
   /** (New file) Function to perform on form submission  */
   handleSubmit: PropTypes.func,
   /** (New file) Any errors that occured when submitting the form */
