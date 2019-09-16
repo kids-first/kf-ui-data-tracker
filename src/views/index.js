ebbe576095b54f132ce1ba372b8d3fc8c1978c94
XuTheBunny <xzhu.fg@gmail.com>
2019-09-16 16:20:13 -0400

    ✨ Add cavatica tab view for ADMIN user

--- src/views/index.js
+++ src/views/index.js
@@ -12,3 +12,4 @@ export {default as NewStudyView} from './NewStudyView';
 export {default as ProfileView} from './ProfileView';
 export {default as StudyInfoView} from './StudyInfoView';
 export {default as EventsView} from './EventsView';
+export {default as CavaticaBixView} from './CavaticaBixView';
