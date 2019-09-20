b9a36ddac28c36e6a2889268851eda4eaa4ea116
XuTheBunny <xzhu.fg@gmail.com>
2019-09-20 10:03:55 -0400

    ✨ Use FormField component on edit study form

--- src/forms/EditStudyForm.js
+++ src/forms/EditStudyForm.js
@@ -1,255 +1,155 @@
-import React from 'react';
-import {Form, Label, Message, Grid} from 'semantic-ui-react';
+import React, {useState, Fragment} from 'react';
+import {Form} from 'semantic-ui-react';
+import FormField from './FormField';
 
 const EditStudyForm = ({formikProps}) => {
   const {values, errors, touched, handleChange, handleBlur} = formikProps;
+  const [focused, setFocused] = useState('');
   return (
-    <Grid doubling stackable>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field required>
-            <label>Study name:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-              error={
-                touched.name !== undefined &&
-                errors.name !== undefined &&
-                errors.name.length > 0
-              }
-            />
-            {touched.name && errors.name && errors.name.length > 0 && (
-              <Label pointing basic color="red">
-                {errors.name}
-              </Label>
-            )}
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            Full name of the study, often the full title of the X01 grant
-            application.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Study short name:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="shortName"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.shortName}
-            />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The name that will appear under portal facets.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field required>
-            <label>External ID:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="externalId"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.externalId}
-              error={
-                touched.externalId !== undefined &&
-                errors.externalId !== undefined &&
-                errors.externalId.length > 0
-              }
-            />
-            {touched.externalId &&
-              errors.externalId &&
-              errors.externalId.length > 0 && (
-                <Label pointing basic color="red">
-                  {errors.externalId}
-                </Label>
-              )}
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            Identifier used by external systems, often the PHS ID if the study
-            is registered with dbGaP.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Version:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="version"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.version}
-              error={
-                touched.version && errors.version && errors.version.length > 0
-              }
-            />
-            {touched.version && errors.version && errors.version.length > 0 && (
-              <Label pointing basic color="red">
-                {errors.version}
-              </Label>
-            )}
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            Study version, often the provided by the data access authority.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Release date:</label>
-            <Form.Input
-              fluid
-              type="date"
-              name="releaseDate"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.releaseDate == null ? '' : values.releaseDate}
-            />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The anticipated date on which this study's data will be made public
-            in Kids First.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Number of anticipated samples:</label>
-            <Form.Input
-              fluid
-              type="number"
-              name="anticipatedSamples"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.anticipatedSamples}
-              error={
-                touched.anticipatedSamples !== undefined &&
-                errors.anticipatedSamples !== undefined &&
-                errors.anticipatedSamples.length > 0
-              }
-            />
-            {touched.anticipatedSamples &&
-              errors.anticipatedSamples &&
-              errors.anticipatedSamples.length > 0 && (
-                <Label pointing basic color="red">
-                  {errors.anticipatedSamples}
-                </Label>
-              )}
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The anticipated number of samples awarded for sequencing for the
-            study.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Awardee organization:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="awardeeOrganization"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.awardeeOrganization}
-            />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The organization responsible for this study.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>Attribution:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="attribution"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.attribution}
-            />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The URL providing more information about the study.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={8}>
-          <Form.Field>
-            <label>S3 Bucket:</label>
-            <Form.Input
-              fluid
-              type="text"
-              name="bucket"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.bucket}
-            />
-          </Form.Field>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Message size="small">
-            The s3 bucket where data for this study resides.
-          </Message>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row verticalAlign="middle">
-        <Grid.Column width={16}>
-          <Form.Field>
-            <label>Description:</label>
-            <Message size="small">
-              Study description in markdown, commonly the X01 abstract text.
-            </Message>
-            <Form.TextArea
-              rows="15"
-              type="text"
-              name="description"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.description}
-            />
-          </Form.Field>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <Fragment>
+      <FormField
+        required
+        id="name"
+        name="Study Name"
+        description=" Full name of the study, often the full title of the X01 grant application."
+        focused={focused === 'name'}
+        value={values.name}
+        touched={touched.name}
+        errors={errors.name}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="shortName"
+        name="Study Short Name"
+        description="The name that will appear under portal facets."
+        type="text"
+        focused={focused === 'shortName'}
+        value={values.shortName}
+        touched={touched.shortName}
+        errors={errors.shortName}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        required
+        id="externalId"
+        name="External ID"
+        description="Identifier used by external systems, often the PHS ID if the study is registered with dbGaP."
+        type="text"
+        focused={focused === 'externalId'}
+        value={values.externalId}
+        touched={touched.externalId}
+        errors={errors.externalId}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="version"
+        name="dbGaP Version"
+        placeholder="v1.p1"
+        description="Study version, often the provided by the data access authority."
+        type="text"
+        focused={focused === 'version'}
+        value={values.version}
+        touched={touched.version}
+        errors={errors.version}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="releaseDate"
+        name="Release Date"
+        description="The anticipated date on which this study's data will be made public in Kids First."
+        type="date"
+        focused={focused === 'releaseDate'}
+        value={values.releaseDate}
+        touched={touched.releaseDate}
+        errors={errors.releaseDate}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="anticipatedSamples"
+        name="Number of anticipated samples"
+        description="The anticipated number of samples awarded for sequencing for the study."
+        type="number"
+        focused={focused === 'anticipatedSamples'}
+        value={values.anticipatedSamples}
+        touched={touched.anticipatedSamples}
+        errors={errors.anticipatedSamples}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="awardeeOrganization"
+        name="Awardee organization"
+        description="The organization responsible for this study."
+        type="text"
+        focused={focused === 'awardeeOrganization'}
+        value={values.awardeeOrganization}
+        touched={touched.awardeeOrganization}
+        errors={errors.awardeeOrganization}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="attribution"
+        name="Attribution"
+        description="The URL providing more information about the study."
+        type="text"
+        focused={focused === 'attribution'}
+        value={values.attribution}
+        touched={touched.attribution}
+        errors={errors.attribution}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="bucket"
+        name="S3 Bucket"
+        description="The s3 bucket where data for this study resides."
+        type="text"
+        focused={focused === 'bucket'}
+        value={values.bucket}
+        touched={touched.bucket}
+        errors={errors.bucket}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      />
+      <FormField
+        id="description"
+        name="Description"
+        description="Study description in markdown, commonly the X01 abstract
+      text."
+        type="text"
+        focused={focused === 'description'}
+        value={values.description}
+        touched={touched.description}
+        errors={errors.description}
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleFocus={id => setFocused(id)}
+      >
+        <Form.TextArea
+          rows="15"
+          type="text"
+          name="description"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.description}
+        />
+      </FormField>
+    </Fragment>
   );
 };
 
