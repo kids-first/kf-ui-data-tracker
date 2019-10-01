8e767f8e84811b3a62ce4cfda0c8c0aa172b4c9d
bdolly <bromeodolly@gmail.com>
2019-10-01 14:33:11 -0400

    :construction: adds amplitude user tracker to auth.js
    assigns users a uninque ampltiude userId based on their Auth Provider sub field

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,11 +1,14 @@
 import auth0 from 'auth0-js';
+import jwtDecode from 'jwt-decode';
 import {
   auth0Domain,
   auth0ClientId,
   auth0RedirectUri,
   auth0Aud,
 } from '../common/globals';
+import amplitude from 'amplitude-js';
 
+const ampltd = amplitude.getInstance();
 class Auth {
   accessToken;
   idToken;
@@ -32,11 +35,38 @@ class Auth {
         if (authResult && authResult.accessToken && authResult.idToken) {
           localStorage.setItem('accessToken', authResult.accessToken);
           localStorage.setItem('idToken', authResult.idToken);
+          const user = jwtDecode(authResult.idToken);
+          const userSubArr = user.sub.split('|');
+          // use the auth providers "sub" field as the stable user id for amplitude
+          ampltd.setUserId(userSubArr[1]);
+          var identify = new ampltd.Identify();
+          [
+            ['email'],
+            ['email_verified'],
+            ['family_name', 'last_name'],
+            ['given_name', 'first_name'],
+            ['https://kidsfirstdrc.org/roles', 'roles'],
+            ['https://kidsfirstdrc.org/permissions', 'premissions'],
+            ['picture'],
+          ].forEach(prop => {
+            identify.set(prop[1] || prop[0], user[prop[0]]);
+          });
+          ampltd.identify(identify);
+
+          ampltd.logEvent('Sign In', {
+            status: 'SUCCESS',
+            auth_sub: userSubArr[0],
+          });
+
           history.push(
             new URLSearchParams(history.location.search).get('from'),
           );
         } else if (err) {
           console.log(err);
+          ampltd.logEvent('Sign In', {
+            status: 'FAILED',
+            err,
+          });
           alert(`Error: ${err.error}. Check the console for further details.`);
         }
       });
@@ -58,6 +88,9 @@ class Auth {
 
     localStorage.removeItem('accessToken');
     localStorage.removeItem('idToken');
+
+    amplitude.getInstance().setUserId(null); // not string 'null'
+    amplitude.getInstance().regenerateDeviceId();
   }
 
   isAuthenticated() {
