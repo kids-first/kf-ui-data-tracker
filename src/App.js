61f02dcc17d54be1d0d2fe273ddfe19b949d21bf
bdolly <bromeodolly@gmail.com>
2019-10-01 12:10:29 -0400

    :heavy_plus_sign: adds @amplitude/react-amplitude
    adds the amplitude react library and implements the top-level AmplitudeProvider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
+import {AMPLITUDE_KEY} from './common/globals';
 import {ApolloProvider} from 'react-apollo';
 import {client} from './state/client';
+import amplitude from 'amplitude-js';
+import {AmplitudeProvider} from '@amplitude/react-amplitude';
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <main className="App">
-        <Routes />
-      </main>
-    </Router>
-  </ApolloProvider>
+  <AmplitudeProvider
+    amplitudeInstance={amplitude.getInstance()}
+    apiKey={AMPLITUDE_KEY}
+  >
+    <ApolloProvider client={client}>
+      <Router>
+        <main className="App">
+          <Routes />
+        </main>
+      </Router>
+    </ApolloProvider>
+  </AmplitudeProvider>
 );
 
 export default App;
