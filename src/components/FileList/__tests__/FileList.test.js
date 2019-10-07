2206c8f7fe66c5d8a4f4e166a6423be9852316a7
XuTheBunny <xzhu.fg@gmail.com>
2019-10-07 16:56:08 -0400

    📷 Update snapshot test for FileList

--- src/components/FileList/__tests__/FileList.test.js
+++ src/components/FileList/__tests__/FileList.test.js
@@ -32,17 +32,40 @@ it('renders with files', async () => {
   // 12 files in total, page 1 contains 10
   const lisPage1 = tree.queryAllByTestId('file-item');
   expect(lisPage1.length).toBe(10);
+  expect(tree.queryAllByText(/4.jpg/).length).toBe(1);
+  expect(tree.queryAllByText(/6.jpg/).length).toBe(0);
 
   // Click on the next button to go to next page
-
-  let button = tree.getByLabelText('Next page');
-  button.click();
+  let button1 = tree.getByLabelText('Next page');
+  button1.click();
 
   await wait();
+
+  expect(tree.container).toMatchSnapshot();
 
   // 12 files in total, 10 files per page, page 2 should have 2 files
   const lisPage2 = tree.queryAllByTestId('file-item');
   expect(lisPage2.length).toBe(2);
+  expect(tree.queryAllByText(/4.jpg/).length).toBe(0);
+  expect(tree.queryAllByText(/6.jpg/).length).toBe(1);
+
+  // Click on the previous button to go to previous page
+  let button2 = tree.getByLabelText('Previous page');
+  button2.click();
+
+  await wait();
+
+  // Filter files by file type by clicking on the file type dropdown
+  const filterDropdown = tree.getByText(/File type/i);
+  fireEvent.click(filterDropdown);
+  await wait();
+  const filterByFileType = tree.getByText(/Other/i);
+  fireEvent.click(filterByFileType);
+  await wait();
+
+  expect(tree.container).toMatchSnapshot();
+  const lisPage3 = tree.queryAllByTestId('file-item');
+  expect(lisPage3.length).toBe(8);
 });
 
 it('renders without files', () => {
