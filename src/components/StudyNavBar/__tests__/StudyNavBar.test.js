fbc71b00174d6e635a6adc16358aaf1d9584f476
XuTheBunny <xzhu.fg@gmail.com>
2019-10-07 16:56:08 -0400

    📷 Update snapshot test for StudyNavBar

--- src/components/StudyNavBar/__tests__/StudyNavBar.test.js
+++ src/components/StudyNavBar/__tests__/StudyNavBar.test.js
@@ -3,10 +3,19 @@ import {render} from 'react-testing-library';
 import {MemoryRouter} from 'react-router-dom';
 import StudyNavBar from '../StudyNavBar';
 
-it('renders correctly', () => {
+it('renders StudyNavBar correctly -- default look', () => {
   const tree = render(
     <MemoryRouter>
       <StudyNavBar />
+    </MemoryRouter>,
+  );
+  expect(tree.container).toMatchSnapshot();
+});
+
+it('renders StudyNavBar correctly -- BETA user look', () => {
+  const tree = render(
+    <MemoryRouter>
+      <StudyNavBar isBeta={true} />
     </MemoryRouter>,
   );
   expect(tree.container).toMatchSnapshot();
