75ef398fde2030f721319209692ee22944c9602e
XuTheBunny <xzhu.fg@gmail.com>
2019-10-07 16:56:08 -0400

    📷 Update snapshot test for Header

--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import wait from 'waait';
+import {render, fireEvent, act} from 'react-testing-library';
 import {MockedProvider} from 'react-apollo/test-utils';
 import {MemoryRouter} from 'react-router-dom';
 import {mocks} from '../../../../__mocks__/kf-api-study-creator/mocks';
+import myProfile from '../../../../__mocks__/kf-api-study-creator/responses/myProfile.json';
 import Header from '../Header';
+import Routes from '../../../Routes';
 
-it('renders correctly', () => {
+jest.mock('auth0-js');
+
+it('renders correctly -- default stage (USER role)', async () => {
+  const tree = render(
+    <MockedProvider
+      mocks={mocks}
+      resolvers={{
+        Query: {
+          myProfile: _ => myProfile.data.myProfile,
+        },
+      }}
+    >
+      <MemoryRouter initialEntries={['/']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+
+  await wait(10);
+
+  expect(tree.container).toMatchSnapshot();
+
+  // Show admin dropwdown
+  act(() => {
+    fireEvent.click(tree.getByText(/Admin/));
+  });
+  await wait();
+
+  expect(tree.container).toMatchSnapshot();
+
+  // Show user dropwdown
+  act(() => {
+    fireEvent.click(tree.getByText(/bendolly/));
+  });
+  await wait();
+
+  expect(tree.container).toMatchSnapshot();
+
+  // Logout
+  act(() => {
+    fireEvent.click(tree.getByText(/Logout/));
+  });
+  await wait();
+
+  expect(tree.container).toMatchSnapshot();
+});
+
+it('renders correctly -- loading stage (USER role)', () => {
   const tree = render(
     <MockedProvider mocks={mocks}>
       <MemoryRouter>
-        <Header />
+        <Header data={{loading: true}} />
       </MemoryRouter>
     </MockedProvider>,
   );
