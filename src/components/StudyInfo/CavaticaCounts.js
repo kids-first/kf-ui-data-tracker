67392bc052930465826daddf9c4c67f1a5ecfb14
XuTheBunny <xzhu.fg@gmail.com>
2019-10-10 13:48:19 -0400

    💄 Remove the quick link button from Cavatica counts component

--- src/components/StudyInfo/CavaticaCounts.js
+++ src/components/StudyInfo/CavaticaCounts.js
@@ -1,79 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
-import {List, Icon, Popup, Label, Button} from 'semantic-ui-react';
+import {List, Popup, Label} from 'semantic-ui-react';
 import {projectOptions} from '../../common/enums';
+import CavaticaLogo from '../../assets/CavaticaLogo';
 /**
  * Displays project counts with total number and breaking down by each type
  * When no projects exist, show buttons guiding user to add/link projects
  */
-const CavaticaCounts = ({projects, title}) => {
-  if (projects && projects.length > 0) {
-    const types = projects.map(({node: {projectType}}) => projectType);
-    const typeCounts = types.reduce((count, type) => {
-      count[type] = (count[type] || 0) + 1;
-      return count;
-    }, {});
-    return (
-      <List horizontal>
-        <List.Item
-          as={Link}
-          to={`/study/${title}/cavatica`}
-          onClick={e => e.stopPropagation()}
-        >
-          <Icon name="code branch" />
-          {projects.length} projects
-        </List.Item>
-        {projectOptions.map(
-          type =>
-            type.value in typeCounts && (
-              <Popup
-                inverted
-                position="top center"
-                size="small"
-                content={type.text}
-                key={type.key}
-                trigger={
-                  <List.Item>
-                    <Label circular empty size="mini" color="olive" />{' '}
-                    {typeCounts[type.key]}
-                  </List.Item>
-                }
-              />
-            ),
+const CavaticaCounts = ({projects, title, hideIcon}) => {
+  const types = projects.map(({node: {projectType}}) => projectType);
+  const typeCounts = types.reduce((count, type) => {
+    count[type] = (count[type] || 0) + 1;
+    return count;
+  }, {});
+  return (
+    <List horizontal>
+      <List.Item
+        as={Link}
+        to={`/study/${title}/cavatica`}
+        onClick={e => e.stopPropagation()}
+        className={hideIcon && projects.length === 0 ? 'text-red' : null}
+      >
+        {!hideIcon && (
+          <CavaticaLogo
+            className="mr-5 vertical-middle"
+            fill={
+              projects && projects.length > 0 ? 'rgba(0,0,0,.6)' : '#db2828'
+            }
+          />
         )}
-      </List>
-    );
-  } else {
-    return (
-      <List horizontal>
-        <List.Item>
-          <Icon color="red" name="code branch" />
-          <Button.Group size="mini" compact basic>
-            <Button
-              primary
-              size="mini"
-              icon="linkify"
-              content="LINK"
-              as={Link}
-              to={`/study/${title}/cavatica#link-cavatica-project`}
-              onClick={e => e.stopPropagation()}
+        {projects.length > 0 ? projects.length : 'No'} projects
+      </List.Item>
+      {projectOptions.map(
+        type =>
+          type.value in typeCounts && (
+            <Popup
+              inverted
+              position="top center"
+              size="small"
+              content={type.text}
+              key={type.key}
+              trigger={
+                <List.Item>
+                  <Label circular empty size="mini" color="olive" />{' '}
+                  {typeCounts[type.key]}
+                </List.Item>
+              }
             />
-            <Button
-              basic
-              primary
-              size="mini"
-              icon="add"
-              content="NEW "
-              as={Link}
-              to={`/study/${title}/cavatica#add-cavatica-project`}
-              onClick={e => e.stopPropagation()}
-            />
-          </Button.Group>
-        </List.Item>
-      </List>
-    );
-  }
+          ),
+      )}
+    </List>
+  );
 };
 
 CavaticaCounts.propTypes = {
