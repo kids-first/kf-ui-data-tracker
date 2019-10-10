19e9de496588df3790b6f4ea9b81973cfed82f4e
bdolly <bromeodolly@gmail.com>
2019-10-10 10:46:33 -0400

    :white_check_mark: adds history prop mocks to StudyFilesListView.test.js

--- src/analyticsTracking/AnalyticsViewConsumer.js
+++ src/analyticsTracking/AnalyticsViewConsumer.js
@@ -29,4 +29,20 @@ const AnalyticsViewConsumer = ({
   );
 };
 
+AnalyticsViewConsumer.propTypes = {
+  /** view components */
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  /** view loading status */
+  status: PropTypes.string,
+  /** props to log on view component mount */
+  mountProperties: PropTypes.object,
+  /** props to log with every view event  */
+  viewProperties: PropTypes.object,
+  /** name of view to log in event scope as `VIEW_<view>` defautls to VIEW_<view_component_file_name> */
+  view: PropTypes.string,
+};
+
 export default AnalyticsViewConsumer;
