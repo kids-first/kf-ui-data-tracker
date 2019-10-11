3fdadcec3800b6edd528df4652289a1dada8da55
bdolly <bromeodolly@gmail.com>
2019-10-10 21:35:14 -0400

    :hankey: adds withAnalyticsTracking HOC
    this component extends the react-amplitdue Amplitude class and provides it's methods to wrapped components

--- src/analyticsTracking/index.js
+++ src/analyticsTracking/index.js
@@ -1,3 +1,6 @@
+export {default as useAnalyticsTracking} from './useAnalyticsTracking';
+export {default as withAnalyticsTracking} from './withAnalyticsTracking';
+
 export {
   default as AnalyticsTrackingProvider,
 } from './AnalyticsTrackingProvider';
