0cbac7ab3e9e893d1e1f2122b7909843b84300a0
bdolly <bromeodolly@gmail.com>
2019-10-22 13:06:52 -0400

    :art: moves mosue and popup events tracking to utils file

---
+++ src/analyticsTracking/utils.js
@@ -0,0 +1,56 @@
+import analyticsTrackingConstants from '../common/analyticsTrackingConstants';
+
+const EVENT_CONSTANTS = analyticsTrackingConstants;
+
+const mouseEvents = log => (eventProps = {}) => {
+  const mouseActions = {
+    onClick: e => {
+      if (eventProps.stopPropagation) e.stopPropagation();
+      try {
+        log(EVENT_CONSTANTS.MOUSE.CLICK, eventProps);
+      } catch (e) {
+        console.error(
+          `[analytic-tracking]  ERROR mouseEvents eventType: ${
+            EVENT_CONSTANTS.MOUSE.CLICK
+          }`,
+          e,
+        );
+      }
+    },
+    onMouseOver: e => {
+      console.log(eventProps);
+      console.log(log);
+      if (eventProps.stopPropagation) e.stopPropagation();
+      try {
+        log(EVENT_CONSTANTS.MOUSE.HOVER, eventProps);
+      } catch (e) {
+        console.error(
+          `[analytic-tracking] ERROR mouseEvents eventType: ${
+            EVENT_CONSTANTS.MOUSE.HOVER
+          }`,
+          e,
+        );
+      }
+    },
+  };
+
+  return mouseActions;
+};
+
+export const buttonTracking = log => {
+  return mouseEvents(log);
+};
+
+export const popupTracking = (log, inheritedProps) => ({
+  name,
+  content,
+  link,
+}) => ({
+  ...mouseEvents(log)({
+    tooltip_name: name,
+    tooltip_content: content,
+    link,
+    scope: [...inheritedProps.scope, `TOOLTIP_${name}`],
+    stopPropagation: true,
+  }),
+});
