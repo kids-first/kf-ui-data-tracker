d79ae50c36caf0a3aeb2a181536d4f29533c7047
bdolly <bromeodolly@gmail.com>
2019-10-22 13:06:52 -0400

    :art: moves mosue and popup events tracking to utils file

--- src/analyticsTracking/withAnalyticsTracking.js
+++ src/analyticsTracking/withAnalyticsTracking.js
@@ -1,43 +1,26 @@
 import React from 'react';
 import {Amplitude} from '@amplitude/react-amplitude';
 import analyticsTrackingConstants from '../common/analyticsTrackingConstants';
+import {popupTracking, buttonTracking} from './utils';
 
 const EVENT_CONSTANTS = analyticsTrackingConstants;
-
-const buttonTracking = log => (buttonName, buttonEventProps) => ({
-  onMouseOver: e =>
-    log(EVENT_CONSTANTS.MOUSE.HOVER, {
-      button_name: buttonName,
-      ...buttonEventProps,
-    }),
-  onClick: e =>
-    log(EVENT_CONSTANTS.MOUSE.CLICK, {
-      button_name: buttonName,
-      ...buttonEventProps,
-    }),
-});
 
 /** HOC to augment and extend Amplitude tracking methods  */
 const withAnalyticsTracking = Component => {
   return class extends Amplitude {
-    constructor(props) {
-      super(props);
-
-      // augment the Amplitude consumer with new props
-      this._renderPropParams = {
-        ...this._renderPropParams,
-        logEvent: this.logEvent,
-        instrument: this.instrument,
-        buttonTracking: buttonTracking(this.logEvent),
-      };
-    }
-
     render() {
       return (
         <Component
           {...this.props}
           tracking={{
             ...this._renderPropParams,
+
+            popupTracking: popupTracking(
+              this.logEvent,
+              this.getAmplitudeEventProperties(),
+            ),
+            buttonTracking: buttonTracking(this.logEvent),
+            inheritedEventProps: this.getAmplitudeEventProperties(),
             EVENT_CONSTANTS,
           }}
         />
