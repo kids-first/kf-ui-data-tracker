bff7535911a954b94e18a6670940a2837beb8a46
bdolly <bromeodolly@gmail.com>
2019-10-30 10:34:10 -0400

    :white_check_mark: wraps FileCounts tests in AnalyticsMockProvider

--- src/components/StudyInfo/__test__/FileCounts.test.js
+++ src/components/StudyInfo/__test__/FileCounts.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, cleanup} from 'react-testing-library';
 import {MemoryRouter} from 'react-router-dom';
 import {MockedProvider} from 'react-apollo/test-utils';
+import {AnalyticsMockProvider} from '../../../analyticsTracking';
 import FileCounts from '../FileCounts';
 import studyByKfId from '../../../../__mocks__/kf-api-study-creator/responses/studyByKfId';
 
@@ -11,11 +12,13 @@ it('renders FileCounts in default stage', () => {
   const files = studyByKfId.data.studyByKfId.files.edges;
   const studyId = studyByKfId.data.studyByKfId.kfId;
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <FileCounts files={files} title={studyId} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <FileCounts files={files} title={studyId} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 });
@@ -23,11 +26,13 @@ it('renders FileCounts in default stage', () => {
 it('renders FileFileCounts with empty file list', () => {
   const studyId = studyByKfId.data.studyByKfId.kfId;
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <FileCounts files={[]} title={studyId} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <FileCounts files={[]} title={studyId} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 });
