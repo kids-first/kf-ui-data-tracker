f29144254b2faf8955b3bad3c2363dc476e85179
bdolly <bromeodolly@gmail.com>
2019-10-30 10:12:47 -0400

    :white_check_mark: wraps CavaticaCounts tests in AnalyticsMockProvider

--- src/components/StudyInfo/__test__/CavaticaCounts.test.js
+++ src/components/StudyInfo/__test__/CavaticaCounts.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, cleanup} from 'react-testing-library';
 import {MemoryRouter} from 'react-router-dom';
 import {MockedProvider} from 'react-apollo/test-utils';
+import {AnalyticsMockProvider} from '../../../analyticsTracking';
 import CavaticaCounts from '../CavaticaCounts';
 import studyByKfId from '../../../../__mocks__/kf-api-study-creator/responses/studyByKfId';
 
@@ -11,11 +12,13 @@ it('renders CavaticaCounts in default stage', () => {
   const projects = studyByKfId.data.studyByKfId.projects.edges;
   const studyId = studyByKfId.data.studyByKfId.kfId;
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <CavaticaCounts projects={projects} title={studyId} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <CavaticaCounts projects={projects} title={studyId} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 });
@@ -23,11 +26,13 @@ it('renders CavaticaCounts in default stage', () => {
 it('renders CavaticaCounts with empty project list', () => {
   const studyId = studyByKfId.data.studyByKfId.kfId;
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <CavaticaCounts projects={[]} title={studyId} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <CavaticaCounts projects={[]} title={studyId} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 });
