d4e4283d8edf272874c90d348e3f92ce2307bcab
bdolly <bromeodolly@gmail.com>
2019-10-31 11:37:36 -0400

    :white_check_mark: wrap FileList and FileListFilterBar tests in AnalyticsMockProvider

--- src/documents/components/ListFilterBar/__test__/ListFilterBar.test.js
+++ src/documents/components/ListFilterBar/__test__/ListFilterBar.test.js
@@ -5,17 +5,20 @@ import {MockedProvider} from 'react-apollo/test-utils';
 import {render, cleanup, act, fireEvent} from 'react-testing-library';
 import ListFilterBar from '../ListFilterBar';
 import studyByKfId from '../../../../../__mocks__/kf-api-study-creator/responses/studyByKfId';
+import {AnalyticsMockProvider} from '../../../../analyticsTracking';
 
 afterEach(cleanup);
 
 it('renders ListFilterBar with files', async () => {
   const files = studyByKfId.data.studyByKfId.files.edges;
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <ListFilterBar fileList={files} filteredList={jest.fn()} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <ListFilterBar fileList={files} filteredList={jest.fn()} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 
@@ -50,11 +53,13 @@ it('renders ListFilterBar with files', async () => {
 
 it('renders ListFilterBar with no files', async () => {
   const tree = render(
-    <MemoryRouter>
-      <MockedProvider>
-        <ListFilterBar filteredList={jest.fn()} />
-      </MockedProvider>
-    </MemoryRouter>,
+    <AnalyticsMockProvider>
+      <MemoryRouter>
+        <MockedProvider>
+          <ListFilterBar filteredList={jest.fn()} />
+        </MockedProvider>
+      </MemoryRouter>
+    </AnalyticsMockProvider>,
   );
   expect(tree.container).toMatchSnapshot();
 });
