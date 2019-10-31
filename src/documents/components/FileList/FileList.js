001c9053c30f63985b822e168192084b1d012702
bdolly <bromeodolly@gmail.com>
2019-10-31 16:27:42 -0400

    :chart_with_upwards_trend: FileElement mouse click and hover tracking

--- src/documents/components/FileList/FileList.js
+++ src/documents/components/FileList/FileList.js
@@ -69,6 +69,9 @@ const FileList = ({fileList, studyId, isAdmin}) => {
                         fileListId={studyId}
                         fileNode={node}
                         isAdmin={isAdmin}
+                        eventProperties={inherit => ({
+                          scope: [...inherit.scope, 'FileElement'],
+                        })}
                       />
                     ))}
                   </Table.Body>
