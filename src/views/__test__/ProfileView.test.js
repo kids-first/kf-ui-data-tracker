8c6cad9c1ecc1383886904ab29fe661790a32494
XuTheBunny <xzhu.fg@gmail.com>
2019-10-31 16:06:55 -0400

    📷 Add test for Profile View

---
+++ src/views/__test__/ProfileView.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import wait from 'waait';
+import {render, cleanup} from 'react-testing-library';
+import {MockedProvider} from 'react-apollo/test-utils';
+import {MemoryRouter} from 'react-router-dom';
+import {mocks} from '../../../__mocks__/kf-api-study-creator/mocks';
+import myProfile from '../../../__mocks__/kf-api-study-creator/responses/myProfile.json';
+import Routes from '../../Routes';
+
+jest.mock('auth0-js');
+afterEach(cleanup);
+
+it('renders profile view correctly', async () => {
+  const tree = render(
+    <MockedProvider
+      resolvers={{
+        Query: {
+          myProfile: _ => myProfile.data.myProfile,
+        },
+      }}
+      mocks={mocks}
+    >
+      <MemoryRouter initialEntries={['/profile']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  expect(tree.container).toMatchSnapshot();
+
+  await wait(10);
+
+  expect(tree.container).toMatchSnapshot();
+});
+
+it('renders profile view -- shows an error', async () => {
+  const tree = render(
+    <MockedProvider mocks={[mocks[14]]}>
+      <MemoryRouter initialEntries={['/profile']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  await wait(0);
+
+  expect(tree.container).toMatchSnapshot();
+  expect(tree.queryByText('Error')).not.toBeNull();
+});
