21bf4fe26f8da98ffd534826405cf1c0f66d2e04
bdolly <bromeodolly@gmail.com>
2019-11-02 15:11:15 -0400

    :recycle: top-level event properties
    Removes APP_MOUNT event and adds the "path" event property to all events in the app.
    We do this because amplitude has no pageview tracking like Google Analytics

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
 import {ApolloProvider} from 'react-apollo';
 import {client} from './state/client';
-import {
-  AnalyticsTrackingProvider,
-  TRACKING_CONSTANTS,
-} from './analyticsTracking';
-
-import {Amplitude, LogOnMount} from '@amplitude/react-amplitude';
-
-const {APP} = TRACKING_CONSTANTS;
+import {AnalyticsTrackingProvider} from './analyticsTracking';
+import {Amplitude} from '@amplitude/react-amplitude';
 
 const App = () => {
   return (
     <AnalyticsTrackingProvider>
-      <ApolloProvider client={client}>
-        <Amplitude eventProperties={{scope: [APP.scope]}}>
-          <LogOnMount eventType={APP.MOUNT} />
+      <Amplitude eventProperties={{path: window.location.pathname}}>
+        <ApolloProvider client={client}>
           <Router>
             <main className="App">
               <Routes />
             </main>
           </Router>
-        </Amplitude>
-      </ApolloProvider>
+        </ApolloProvider>
+      </Amplitude>
     </AnalyticsTrackingProvider>
   );
 };
