abf1a6990b45274a76eb1003f86a60eb204e8744
bdolly <bromeodolly@gmail.com>
2019-11-02 15:19:46 -0400

    :chart_with_upwards_trend: wrap AdminRoute in Amplitude

--- src/Routes/AdminRoute.js
+++ src/Routes/AdminRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import jwtDecode from 'jwt-decode';
 import {Route, Redirect, withRouter} from 'react-router-dom';
+import {Amplitude} from '@amplitude/react-amplitude';
 
 export const isAdmin = () => {
   const token = localStorage.getItem('accessToken');
@@ -23,13 +24,27 @@ export const isAdmin = () => {
  * A route that only allows access to admin users, otherwise routes to the
  * index page
  */
-const AdminRoute = ({component: Component, ...rest}) => (
-  <Route
-    {...rest}
-    render={props =>
-      isAdmin() ? <Component {...props} /> : <Redirect to="/" />
-    }
-  />
-);
+const AdminRoute = ({component: Component, path, ...rest}) => {
+  let comp = <Component />;
+
+  return (
+    <Amplitude
+      eventProperties={{
+        view: comp.type.WrappedComponent
+          ? comp.type.WrappedComponent.name
+          : comp.type.name,
+        route: path,
+      }}
+    >
+      <Route
+        {...rest}
+        path={path}
+        render={props =>
+          isAdmin() ? <Component {...props} /> : <Redirect to="/" />
+        }
+      />
+    </Amplitude>
+  );
+};
 
 export default withRouter(AdminRoute);
