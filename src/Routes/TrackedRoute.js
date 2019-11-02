b0fc1c5b3666c2270d47b9364ff11a4f884206eb
bdolly <bromeodolly@gmail.com>
2019-11-02 15:58:04 -0400

    :sparkles: component to Track Routes
    by default the TrackedRoute fires a PAGEVIEW event on mount

---
+++ src/Routes/TrackedRoute.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Route} from 'react-router-dom';
+import {Amplitude, LogOnMount} from '@amplitude/react-amplitude';
+
+/** Wrap all of our routes to make them log the view and route they render  */
+const TrackedRoute = ({
+  component,
+  path,
+  render,
+  logMount = true,
+  eventProperties,
+  ...rest
+}) => {
+  return (
+    <Amplitude
+      eventProperties={{
+        view: component ? component.name : render().type.WrappedComponent.name,
+        route: path,
+        ...eventProperties,
+      }}
+    >
+      {logMount && <LogOnMount eventType="PAGEVIEW" />}
+      <Route {...{component, path, render}} {...rest} />
+    </Amplitude>
+  );
+};
+
+TrackedRoute.propTypes = {
+  /** component passed to Route */
+  component: PropTypes.func,
+  /** render function to pass to Route  */
+  render: PropTypes.func,
+  /** url regex path to pass to Route */
+  path: PropTypes.string.isRequired,
+  /** boolean to fire a PAGEVIEW event Route mount (default: True) */
+  logMount: PropTypes.bool,
+  /** additional event properties object for events wihin the route */
+  eventProperties: PropTypes.object,
+};
+
+export default TrackedRoute;
