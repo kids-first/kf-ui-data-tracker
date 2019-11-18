7fa142cea2d3e5fa714e7756d7dddbd1786817d5
bdolly <bromeodolly@gmail.com>
2019-11-18 11:59:27 -0500

    :art: better eventUtils for AmplitudeProxy methods

--- src/analyticsTracking/withAnalyticsTracking.js
+++ src/analyticsTracking/withAnalyticsTracking.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {EVENT_CONSTANTS, AmplitudeProxy} from '../analyticsTracking';
+import {
+  popupTracking,
+  buttonTracking,
+  dropdownTracking,
+  popupTracking,
+} from './eventUtils';
 
 /** HOC to augment and extend Amplitude tracking methods  */
-const withAnalyticsTracking = Component => {
+const withAnalyticsTracking = (Component, config) => {
   return class extends AmplitudeProxy {
+    logToConsole = config ? config.logToConsole : false;
+
     render() {
       return (
         <Component
           {...this.props}
           tracking={{
             ...this._renderPropParams,
+            popupTracking: popupTracking(
+              this.logEvent,
+              this.getAmplitudeEventProperties(),
+            ),
+            buttonTracking: buttonTracking(this.logEvent),
+            dropdownTracking: dropdownTracking(this.logEvent),
+            inheritedEventProps: this.getAmplitudeEventProperties(),
             EVENT_CONSTANTS,
           }}
         />
