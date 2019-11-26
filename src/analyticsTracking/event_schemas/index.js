a8d0f2f7990287258c80e11dea9dce4524fd2e3a
bdolly <bromeodolly@gmail.com>
2019-11-26 13:40:19 -0500

    :bulb: adds TOOLTIP__EVENT schema
    this is a base schema for all tooltip/popup events

--- src/analyticsTracking/event_schemas/index.js
+++ src/analyticsTracking/event_schemas/index.js
@@ -1,5 +1,6 @@
 import * as commonDefsSchema from './common_definitions.schema.json';
 import * as buttonEventSchema from './BUTTON/BUTTON__EVENT.schema.json';
+import * as tooltipEventSchema from './TOOLTIP/TOOLTIP__EVENT.schema.json';
 import * as studyListViewToggleSchema from './STUDY_LIST/STUDY_LIST__VIEW_TOGGLE.schema.json';
 import * as authLoginSchema from './AUTH/AUTH__LOGIN.schema.json';
 import * as studyCardToggleSchema from './STUDY_CARD/STUDY_CARD__TOGGLE_DETAIL.schema.json';
@@ -21,6 +22,7 @@ import * as dropdownChagneSchema from './DROPDOWN/DROPDOWN__CHANGE.schema.json';
  * Schema keys then get translated into event constants in src/analtyicsTracking/index.js
  */
 const schemas = {
+  LOG_TEST: {description: 'sent in test and ci environments'},
   common_definitions: commonDefsSchema.default,
   // auth
   AUTH__LOGIN: authLoginSchema.default,
@@ -32,16 +34,16 @@ const schemas = {
   MOUSE__HOVER: buttonEventSchema.default,
   // dropdown
   DROPDOWN__CHANGE: dropdownChagneSchema.default,
-  DROPDOWN__CLOSE: {},
-  DROPDOWN__OPEN: {},
+  // DROPDOWN__CLOSE: {},
+  // DROPDOWN__OPEN: {},
   // tooltip
-  TOOLTIP: {},
+  TOOLTIP: tooltipEventSchema.default,
   // study
   STUDY_LIST__VIEW_TOGGLE: studyListViewToggleSchema.default,
   STUDY_LIST__SEARCH: studyListViewToggleSchema.default,
   // study card
   STUDY_CARD__TOGGLE_DETAIL: studyCardToggleSchema.default,
-  STUDY_CARD__CLCK: {},
+  // STUDY_CARD__CLCK: {},
 };
 
 export default schemas;
