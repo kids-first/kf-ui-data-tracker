3cd32be1454abc851399604235d4ac9af42bc5af
bdolly <bromeodolly@gmail.com>
2019-12-05 15:27:35 -0500

    🐛 set viewName prop in withAnalyticsTracking HOC
    This sets a viewName prop on the withAnalyticsTracking HOC to be picked up by TrackedRoute for Amplitude Logging

---
+++ src/analyticsTracking/withAnalyticsTracking.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {EVENT_CONSTANTS, AmplitudeProxy} from '../analyticsTracking';
+
+/** HOC to augment and extend Amplitude tracking methods  */
+const withAnalyticsTracking = (Component, config) => {
+  class AnalyticsTrackingHOC extends AmplitudeProxy {
+    logToConsole = config ? config.logToConsole : false;
+    saveSchemas = config ? config.saveSchemas : false;
+    view = Component.name;
+    render() {
+      return (
+        <Component
+          {...this.props}
+          tracking={{
+            ...this._renderPropParams,
+            inheritedEventProps: this.getAmplitudeEventProperties(),
+            EVENT_CONSTANTS,
+          }}
+        />
+      );
+    }
+  }
+  AnalyticsTrackingHOC.displayName = `withAnalyticsTracking(${Component.name})`;
+  /** set the parent view name to use in eventProps.view */
+  AnalyticsTrackingHOC.viewName = Component.name;
+  return AnalyticsTrackingHOC;
+};
+
+export default withAnalyticsTracking;
