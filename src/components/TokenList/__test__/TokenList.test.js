508c0d207641698a5ed9a0af0e7e187f67fcb594
XuTheBunny <xzhu.fg@gmail.com>
2019-12-06 15:30:18 -0500

    📷 Update snapshote test for tokens view and token list component

--- src/components/TokenList/__test__/TokenList.test.js
+++ src/components/TokenList/__test__/TokenList.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {render} from 'react-testing-library';
+import wait from 'waait';
+import {render, fireEvent, act} from 'react-testing-library';
 import TokenList from '../TokenList';
+import devTokens from '../../../../__mocks__/kf-api-study-creator/responses/devTokens.json';
 
 it('renders token list correctly - no data', () => {
   const tree = render(<TokenList />);
   expect(tree.container).toMatchSnapshot();
+});
+
+it('renders token list correctly - with new token', async () => {
+  const tokens = devTokens.data.allDevTokens.edges;
+  const newToken = devTokens.data.allDevTokens.edges[0].node;
+  const tree = render(
+    <TokenList tokens={tokens} newToken={newToken} deleteToken={jest.fn()} />,
+  );
+
+  await wait();
+  expect(tree.container).toMatchSnapshot();
+
+  // Click on the delete token button
+  act(() => {
+    fireEvent.click(tree.getAllByTestId('delete-token-button')[0]);
+  });
 });
