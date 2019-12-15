fa633cb57e342771edaf5a32bb8329c7df70d518
bdolly <bromeodolly@gmail.com>
2019-12-15 15:35:55 -0500

    📈 enhance useLogEvent
    Adds logEventToConsole,  saveSchemaToLocalFile,
    saveEventToSessionStorage to useLogEvent

--- src/analyticsTracking/hooks/use-analytics-tracking.js
+++ src/analyticsTracking/hooks/use-analytics-tracking.js
@@ -2,58 +2,34 @@ import React from 'react';
 import {AmplitudeContext} from '../services/Amplitude/AmplitudeProvider';
 import {useLogEvent} from './use-log-event';
 import {useInstrument} from './use-instrument';
+
 const useAnalyticsTracking = (
   eventProperties = {},
   instanceName = '$default_instance',
+  opts = {},
 ) => {
   const {
     amplitudeInstance,
     eventProperties: inheritedProperties,
   } = React.useContext(AmplitudeContext);
 
-  // return React.useMemo(() => {
-  const logEvent = useLogEvent(instanceName, eventProperties, 0);
-  const instrument = useInstrument(instanceName, eventProperties, 0);
-  // function logEvent(
-  //   eventType,
-  //   eventPropertiesIn = {},
-  //   callback
-  // ) {
-  //   if (!amplitudeInstance) {
-  //     return;
-  //   }
-
-  //   let computed = inheritedProperties;
-  //   if (typeof eventProperties === "function") {
-  //     computed = eventProperties(computed);
-  //   } else {
-  //     computed = { ...computed, ...(eventProperties || {}) };
-  //   }
-  //   if (typeof eventPropertiesIn === "function") {
-  //     computed = eventPropertiesIn(computed);
-  //   } else {
-  //     computed = { ...computed, ...(eventPropertiesIn || {}) };
-  //   }
-
-  //   amplitudeInstance.logEvent(eventType, computed, callback);
-  // }
-
-  // function instrument<T extends Function>(eventType: string, func: T): T {
-  //   function fn(...params: any) {
-  //     const retVal = func ? func(...params) : undefined;
-  //     logEvent(eventType);
-  //     return retVal;
-  //   }
-  //   return fn as any;
-  // }
-
-  return {
-    logEvent: logEvent,
-    instrument: instrument,
-    eventProperties: inheritedProperties,
-    amplitudeInstance: amplitudeInstance,
-  };
-  // }, [eventProperties, amplitudeInstance, inheritedProperties, instanceName]);
+  const logEvent = useLogEvent(instanceName, eventProperties, 0, opts);
+  const instrument = useInstrument(instanceName, eventProperties, 0, opts);
+  return React.useMemo(() => {
+    return {
+      logEvent: logEvent,
+      instrument: instrument,
+      eventProperties: inheritedProperties,
+      amplitudeInstance: amplitudeInstance,
+    };
+  }, [
+    // eventProperties,
+    amplitudeInstance,
+    inheritedProperties,
+    // instanceName,
+    logEvent,
+    instrument,
+  ]);
 };
 
 export default useAnalyticsTracking;
--- src/analyticsTracking/hooks/use-log-event.js
+++ src/analyticsTracking/hooks/use-log-event.js
@@ -2,26 +2,77 @@ import React from 'react';
 import debounce from 'lodash.debounce';
 import {useEventProperties} from './use-event-properties';
 import {useInstance} from './use-instance';
+import {normalizeEventType} from '../eventUtils';
+import saveSchema from '../saveSchema';
+import validate from '../eventSchemaValidator';
+
+const logEventToConsole = (EventType, eventProps, opts) => {
+  if (opts.logToConsole) {
+    console.log(`logEvent: ${EventType}`, eventProps);
+  }
+};
+
+const saveSchemaToLocalFile = (EventType, EventProps, opts) => {
+  /** saves the event schema as a "<event_constant>.schema.json" file for download */
+  if (
+    ['dev', 'development'].includes(process.env.NODE_ENV) &&
+    opts.saveSchemas
+  ) {
+    saveSchema(EventType, EventProps);
+  }
+};
+
+const saveEventToSessionStorage = (EventType, EventProps) => {
+  /** save all events to sessionStorage for unit testing */
+  if (['test', 'CI'].includes(process.env.NODE_ENV)) {
+    /** log all events to sessionStorage */
+    sessionStorage.setItem(
+      'session_events',
+      JSON.stringify([
+        ...(JSON.parse(sessionStorage.getItem('session_events')) || []),
+        {
+          EventType,
+          eventProps: EventProps,
+          utc_time: Date.now(),
+        },
+      ]),
+    );
+  }
+};
 
 export const useLogEvent = (
   instanceName,
   eventProperties,
   debounceInterval,
+  opts = {},
 ) => {
   const instance = useInstance(instanceName);
   const inheritedEventProperties = useEventProperties(eventProperties);
 
   return React.useMemo(() => {
     const logEvent = (eventType, eventProperties, callback) => {
-      if (instance) {
-        instance.logEvent(
-          eventType,
-          {
-            ...inheritedEventProperties,
-            ...(eventProperties || {}),
-          },
-          callback,
-        );
+      if (!eventType || eventType === 'undefined') {
+        console.error(`EventTypeError: No eventType given`);
+        return false;
+      }
+      // make sure we're consistent with our naming
+      const EventType = normalizeEventType(eventType);
+      const EventProps = {
+        ...inheritedEventProperties,
+        ...(eventProperties || {}),
+      };
+
+      logEventToConsole(EventType, EventProps, opts);
+
+      saveSchemaToLocalFile(EventType, EventProps, opts);
+
+      saveEventToSessionStorage(EventType, EventProps);
+
+      /** Validate our events against their event props schemas (src/analyticsTracking/event_schemas) */
+      const eventIsValid = validate(EventType, EventProps);
+
+      if (instance && eventIsValid) {
+        instance.logEvent(EventType, EventProps, callback);
       }
     };
 
@@ -30,5 +81,5 @@ export const useLogEvent = (
     } else {
       return logEvent;
     }
-  }, [debounceInterval, inheritedEventProperties, instance]);
+  }, [debounceInterval, inheritedEventProperties, instance, opts]);
 };
