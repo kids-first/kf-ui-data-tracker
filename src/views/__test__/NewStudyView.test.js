9621b4e9bf572ea3d31deb21a4d7d605fbfd38e6
XuTheBunny <xzhu.fg@gmail.com>
2019-12-16 10:24:50 -0500

    📷 Update test for study creation flow

--- src/views/__test__/NewStudyView.test.js
+++ src/views/__test__/NewStudyView.test.js
@@ -10,68 +10,6 @@ import Routes from '../../Routes';
 jest.mock('auth0-js');
 afterEach(cleanup);
 
-it('renders new study view correctly', async () => {
-  const tree = render(
-    <MockedProvider
-      resolvers={{
-        Query: {
-          myProfile: _ => myProfile.data.myProfile,
-        },
-      }}
-      mocks={[mocks[1], mocks[8], mocks[21]]}
-    >
-      <MemoryRouter initialEntries={['/study/new-study/info']}>
-        <Routes />
-      </MemoryRouter>
-    </MockedProvider>,
-    {
-      container: document.body,
-    },
-  );
-  await wait();
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.change(tree.getByLabelText('name'), {
-      target: {value: 'benchmark extensible e-business'},
-    });
-  });
-  await wait();
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.click(tree.getByText(/NEXT/i));
-  });
-  await wait();
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.change(tree.getByLabelText('externalId'), {
-      target: {value: 'benchmark extensible e-business'},
-    });
-  });
-  await wait();
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.click(tree.getByText(/NEXT/i));
-  });
-  await wait();
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.click(tree.getByText(/SUBMIT/i));
-  });
-  await wait();
-
-  expect(tree.container).toMatchSnapshot();
-
-  act(() => {
-    fireEvent.click(tree.getAllByTestId('new-study-confirm')[0]);
-  });
-  expect(tree.container).toMatchSnapshot();
-});
-
 it('renders new study view correctly --  with error on submit', async () => {
   const tree = render(
     <MockedProvider
@@ -119,23 +57,102 @@ it('renders new study view correctly --  with error on submit', async () => {
   act(() => {
     fireEvent.click(tree.getByText(/SUBMIT/i));
   });
+  await wait(3000);
+
+  expect(tree.container).toMatchSnapshot();
+  expect(
+    tree.queryByText(/Network error: Failed to create the new study/i),
+  ).not.toBeNull();
+});
+
+it('renders new study view correctly', async () => {
+  // browser sessionStorage mocks
+  const sessionStorageMock = (function() {
+    let store = {newStudy: 'SD_00000000_SUC'};
+    return {
+      getItem: function(key) {
+        return store[key] || null;
+      },
+      setItem: function(key, value) {
+        store[key] = value.toString();
+      },
+      removeItem: function(key) {
+        delete store[key];
+      },
+      clear: function() {
+        store = {};
+      },
+    };
+  })();
+
+  Object.defineProperty(window, 'sessionStorage', {
+    value: sessionStorageMock,
+  });
+
+  const tree = render(
+    <MockedProvider
+      resolvers={{
+        Query: {
+          myProfile: _ => myProfile.data.myProfile,
+        },
+      }}
+      mocks={[mocks[1], mocks[8], mocks[21], mocks[40], mocks[1]]}
+    >
+      <MemoryRouter initialEntries={['/study/new-study/info']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+    {
+      container: document.body,
+    },
+  );
   await wait();
+  expect(tree.container).toMatchSnapshot();
 
   act(() => {
-    fireEvent.click(tree.getByText(/Cancel/i));
+    fireEvent.change(tree.getByLabelText('name'), {
+      target: {value: 'benchmark extensible e-business'},
+    });
   });
   await wait();
+  expect(tree.container).toMatchSnapshot();
+
+  act(() => {
+    fireEvent.click(tree.getByText(/NEXT/i));
+  });
+  await wait();
+  expect(tree.container).toMatchSnapshot();
+
+  act(() => {
+    fireEvent.change(tree.getByLabelText('externalId'), {
+      target: {value: 'benchmark extensible e-business'},
+    });
+  });
+  await wait();
+  expect(tree.container).toMatchSnapshot();
+
+  act(() => {
+    fireEvent.click(tree.getByText(/NEXT/i));
+  });
+  await wait();
+  expect(tree.container).toMatchSnapshot();
 
   act(() => {
     fireEvent.click(tree.getByText(/SUBMIT/i));
   });
   await wait();
 
+  expect(tree.container).toMatchSnapshot();
+
+  await wait(1500);
+
+  expect(tree.container).toMatchSnapshot();
+
   act(() => {
-    fireEvent.click(tree.getAllByTestId('new-study-confirm')[0]);
+    fireEvent.click(tree.getByText(/Skip ahead to the study/i));
   });
 
-  await wait(3000);
+  await wait(100);
 
   expect(tree.container).toMatchSnapshot();
 });
