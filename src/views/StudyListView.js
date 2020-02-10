9e408425b0d1ceb4d28955d10f6bc4591be7743c
XuTheBunny <xzhu.fg@gmail.com>
2020-02-10 16:13:02 -0500

    ✨ Add release status onto study table

--- src/views/StudyListView.js
+++ src/views/StudyListView.js
@@ -2,29 +2,51 @@ import React from 'react';
 import {Helmet} from 'react-helmet';
 import {useQuery} from '@apollo/react-hooks';
 import {Link} from 'react-router-dom';
-import {ALL_STUDIES, MY_PROFILE} from '../state/queries';
+import {ALL_STUDIES, MY_PROFILE, GET_RELEASED_STUDY} from '../state/queries';
 import StudyList from '../components/StudyList/StudyList';
-import {Button, Message, Container, Segment} from 'semantic-ui-react';
+import {Button, Message, Container, Segment, Icon} from 'semantic-ui-react';
 
 const StudyListView = () => {
   const {data: profileData} = useQuery(MY_PROFILE);
   const myProfile = profileData && profileData.myProfile;
   const {loading, error, data} = useQuery(ALL_STUDIES);
+  const {
+    loading: releasesLoading,
+    error: releasesError,
+    data: releasesData,
+  } = useQuery(GET_RELEASED_STUDY, {
+    context: {clientName: 'coordinator'},
+  });
   const allStudies = data && data.allStudies;
+  const allReleases = releasesData && releasesData.allStudies;
+  var studyList = !loading && allStudies ? allStudies.edges : [];
+  const releaseList = !releasesLoading && allReleases ? allReleases.edges : [];
+  if (releaseList.length > 0 && studyList.length > 0) {
+    studyList.forEach(function(study) {
+      const release =
+        releaseList.find(r => r.node.kfId === study.node.kfId) || {};
+      study.node.release =
+        release.node && release.node.releases.edges.length > 0
+          ? release.node.releases.edges[0]
+          : {};
+    });
+  }
 
   if (error)
     return (
       <Container as={Segment} basic>
-        <Message
-          negative
-          icon="warning circle"
-          header="Error"
-          content={error.message}
-        />
+        <Message negative icon>
+          <Icon name="warning circle" />
+          <Message.Content>
+            <Message.Header>Error</Message.Header>
+            {error && <p>{error.message}</p>}
+            {releasesError && <p>{releasesError.message}</p>}
+          </Message.Content>
+        </Message>
       </Container>
     );
-  const studyList = !loading && allStudies ? allStudies.edges : [];
-  if (!loading && studyList.length === 0)
+
+  if (!loading && !releasesLoading && studyList.length === 0)
     return (
       <Container as={Segment} basic>
         <Helmet>
@@ -67,9 +89,20 @@ const StudyListView = () => {
       </Helmet>
       <StudyList
         studyList={studyList}
-        loading={loading}
+        loading={loading || releasesLoading}
         roles={myProfile ? myProfile.roles : []}
       />
+      <Container as={Segment} basic vertical>
+        {releasesError && (
+          <Message negative icon>
+            <Icon name="warning circle" />
+            <Message.Content>
+              <Message.Header>Releases Info Error</Message.Header>
+              {releasesError.message}
+            </Message.Content>
+          </Message>
+        )}
+      </Container>
     </>
   );
 };
