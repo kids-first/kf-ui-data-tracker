49a07375171a9c572dd7eb24c4a6dbeca5201331
XuTheBunny <xzhu.fg@gmail.com>
2020-02-10 16:55:31 -0500

    🤡 Add mocks for queries against coordinator api

---
+++ __mocks__/kf-api-release-coordinator/mocks.js
@@ -0,0 +1,36 @@
+import {GET_STUDY_RELEASES, GET_RELEASED_STUDY} from '../../src/state/queries';
+import getStudyReleases from './responses/getStudyReleases.json';
+import allReleaseStudies from './responses/allReleaseStudies.json';
+
+export const coordMocks = {
+  getStudyReleases: {
+    request: {
+      query: GET_STUDY_RELEASES,
+      variables: {
+        id: 'U3R1ZHlOb2RlOlNEXzhXWDhRUTA2',
+      },
+    },
+    result: getStudyReleases,
+  },
+  getStudyReleasesError: {
+    request: {
+      query: GET_STUDY_RELEASES,
+      variables: {
+        id: 'U3R1ZHlOb2RlOlNEXzhXWDhRUTA2',
+      },
+    },
+    error: new Error('Failed to fetch releases information'),
+  },
+  allReleaseStudies: {
+    request: {
+      query: GET_RELEASED_STUDY,
+    },
+    result: allReleaseStudies,
+  },
+  allReleaseStudiesError: {
+    request: {
+      query: GET_RELEASED_STUDY,
+    },
+    error: new Error('Failed to fetch study releases information'),
+  },
+};
