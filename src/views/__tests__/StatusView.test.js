01a4bd04e7526d60ca044010af057d83339ced0d
XuTheBunny <xzhu.fg@gmail.com>
2020-02-11 11:27:27 -0500

    ✅ Add test for status view

---
+++ src/views/__tests__/StatusView.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import wait from 'waait';
+import {render, cleanup} from '@testing-library/react';
+import {MockedProvider} from '@apollo/react-testing';
+import {MemoryRouter} from 'react-router-dom';
+import {mocks} from '../../../__mocks__/kf-api-study-creator/mocks';
+import {coordMocks} from '../../../__mocks__/kf-api-release-coordinator/mocks';
+import myProfile from '../../../__mocks__/kf-api-study-creator/responses/myProfile.json';
+import Routes from '../../Routes';
+
+jest.mock('auth0-js');
+afterEach(cleanup);
+
+it('renders study status view correctly', async () => {
+  const tree = render(
+    <MockedProvider
+      mocks={[mocks[1], mocks[8], mocks[38], coordMocks.getStudyReleases]}
+      resolvers={{
+        Query: {
+          myProfile: _ => myProfile.data.myProfile,
+        },
+      }}
+    >
+      <MemoryRouter initialEntries={['/study/SD_8WX8QQ06/status']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  expect(tree.container).toMatchSnapshot();
+
+  await wait(10);
+
+  expect(tree.container).toMatchSnapshot();
+});
+
+it('renders study status view -- shows an error', async () => {
+  const tree = render(
+    <MockedProvider
+      mocks={[mocks[2], mocks[8], mocks[41], coordMocks.getStudyReleasesError]}
+      resolvers={{
+        Query: {
+          myProfile: _ => myProfile.data.myProfile,
+        },
+      }}
+    >
+      <MemoryRouter initialEntries={['/study/SD_8WX8QQ06/status']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  await wait(10);
+
+  expect(tree.container).toMatchSnapshot();
+});
+
+it('renders study status view correctly --  showing empty view for non-admin user', async () => {
+  var regularUser = myProfile.data.myProfile;
+  regularUser.roles = ['USER'];
+  const tree = render(
+    <MockedProvider
+      resolvers={{
+        Query: {
+          myProfile: _ => regularUser,
+        },
+      }}
+      mocks={mocks.concat([coordMocks.getStudyReleases])}
+    >
+      <MemoryRouter initialEntries={['/study/SD_8WX8QQ06/status']}>
+        <Routes />
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+  await wait(10);
+  expect(tree.container).toMatchSnapshot();
+  expect(
+    tree.queryByText('You don’t have access to this page.'),
+  ).not.toBeNull();
+});
