4d97f6173d2341cdcd1fa437ee1216ed3719c168
XuTheBunny <xzhu.fg@gmail.com>
2020-02-17 14:39:16 -0500

    ♻️ Renaming the home page study list as "nvestigator Studies"

--- src/components/StudyList/StudyList.js
+++ src/components/StudyList/StudyList.js
@@ -56,7 +56,7 @@ const StudyList = ({studyList, loading, activeView, roles}) => {
   return (
     <Grid as={Segment} basic container stackable>
       <Grid.Column width={8} textAlign="left">
-        <Header as="h1">Your Studies</Header>
+        <Header as="h1">Your Investigator Studies</Header>
       </Grid.Column>
       <Grid.Column width={8} textAlign="right">
         {isAdmin && (
