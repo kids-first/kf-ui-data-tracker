c40c8bfe5a69469d0e1377e53ee977f279374eaa
XuTheBunny <xzhu.fg@gmail.com>
2020-02-17 14:51:51 -0500

    ✨ Add new study selection view giving new delivery or research study

---
+++ src/views/NewStudySelectionView.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import {Link} from 'react-router-dom';
+import {Segment, Grid, Header, Icon, Card} from 'semantic-ui-react';
+
+const NewStudySelectionView = () => (
+  <>
+    <Helmet>
+      <title>KF Data Tracker - New study selection</title>
+    </Helmet>
+    <Grid as={Segment} basic container stackable>
+      <Grid.Column width={16} textAlign="left">
+        <Header as="h1">Choose a study type</Header>
+      </Grid.Column>
+      <Grid.Row>
+        <Grid.Column width={8} textAlign="center">
+          <Card
+            data-testid="delivery-card"
+            fluid
+            as={Link}
+            to={`/study/new-study/info`}
+            header={
+              <Header icon>
+                <Icon name="dna" />
+                Delivery Study
+              </Header>
+            }
+            description="Create a study to deliver data to investigators and end users on the Kids First Portal"
+          />
+        </Grid.Column>
+        <Grid.Column width={8} textAlign="center">
+          <Card
+            data-testid="research-card"
+            fluid
+            as={Link}
+            to={`/study/new-study/info`}
+            header={
+              <Header icon>
+                <Icon name="flask" />
+                Research Study
+              </Header>
+            }
+            description="Create a study to contain projects to analyze other studies in Kids First"
+          />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </>
+);
+
+export default NewStudySelectionView;
