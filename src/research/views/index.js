a8c4e39e1a5df8d54ef2bae60696ed8ab13f62ec
XuTheBunny <xzhu.fg@gmail.com>
2020-02-20 11:00:36 -0500

    ✨ Add research study basic info view reusing new research study form

--- src/research/views/index.js
+++ src/research/views/index.js
@@ -1,2 +1,3 @@
 export {default as ResearchStudyListView} from './ResearchStudyListView';
 export {default as NewResearchStudyView} from './NewResearchStudyView';
+export {default as ResearchStudyInfoView} from './ResearchStudyInfoView';
