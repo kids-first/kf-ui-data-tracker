dce3eeceed5d537f724ae16f8bba3287eb487c1f
Dan Kolbman <dankolbman@gmail.com>
2020-03-23 21:54:37 -0400

    :chart_with_upwards_trend: Add events to actions on document list

--- src/documents/components/FileActionButtons/FileActionButtons.js
+++ src/documents/components/FileActionButtons/FileActionButtons.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import {Button, Divider, Icon, Popup} from 'semantic-ui-react';
 import {downloadFile, createDateSort} from '../../utilities';
 import CopyButton from '../../../components/CopyButton/CopyButton';
+import {Amplitude} from '@amplitude/react-amplitude';
+
 /**
  * Simple Icon Buttons to download and delete a file passed in as the node prop
  */
@@ -15,73 +17,104 @@ const FileActionButtons = ({
   deleteFile,
   vertical = true,
   fluid = false,
-}) => {
-  return (
-    <Button.Group fluid={fluid} size="small">
-      <CopyButton
-        textToCopy={
-          node.downloadUrl +
-          `/version/${node.versions.edges.sort(createDateSort)[0].node.kfId}`
-        }
-        basic
-        compact
-        position="top right"
-        tooltip="Copy download link"
-      />
-      <Popup
-        inverted
-        position="top right"
-        icon="download"
-        content="Download latest version"
-        trigger={
-          <Button
-            basic
-            compact
-            icon="download"
-            data-testid="download-file"
-            onClick={e => {
-              e.stopPropagation();
-              downloadFile(studyId, node.kfId, null, downloadFileMutation);
-            }}
-          />
-        }
-      />
-      {deleteFile && vertical && (
+}) => (
+  <Button.Group fluid={fluid} size="small">
+    <Amplitude
+      eventProperties={inheritedProps => ({
+        ...inheritedProps,
+        scope: [...(inheritedProps.scope || []), 'copy button'],
+      })}
+    >
+      {({logEvent}) => (
+        <CopyButton
+          textToCopy={
+            node.downloadUrl +
+            `/version/${node.versions.edges.sort(createDateSort)[0].node.kfId}`
+          }
+          basic
+          compact
+          position="top right"
+          tooltip="Copy download link"
+          onClick={ev => {
+            ev.stopPropagation();
+            logEvent('download link copied');
+          }}
+        />
+      )}
+    </Amplitude>
+    <Amplitude
+      eventProperties={inheritedProps => ({
+        ...inheritedProps,
+        scope: [...(inheritedProps.scope || []), 'download button'],
+      })}
+    >
+      {({logEvent}) => (
         <Popup
+          inverted
+          position="top right"
+          icon="download"
+          content="Download latest version"
           trigger={
             <Button
               basic
               compact
-              data-testid="delete-button"
-              onClick={e => e.stopPropagation()}
-              icon={<Icon name="trash alternate" />}
+              icon="download"
+              data-testid="download-file"
+              onClick={e => {
+                e.stopPropagation();
+                logEvent('download file');
+                downloadFile(studyId, node.kfId, null, downloadFileMutation);
+              }}
             />
           }
-          header="Are you sure?"
-          content={
-            <>
-              This file and all of its versions and history will be deleted
-              <Divider />
-              <Button
-                data-testid="delete-confirm"
-                negative
-                fluid
-                icon={<Icon name="trash alternate" />}
-                content="Delete"
-                onClick={e => {
-                  e.stopPropagation();
-                  deleteFile({variables: {kfId: node.kfId}});
-                }}
-              />
-            </>
-          }
-          on="click"
-          position="top right"
         />
       )}
-    </Button.Group>
-  );
-};
+    </Amplitude>
+    {deleteFile && vertical && (
+      <Amplitude
+        eventProperties={inheritedProps => ({
+          ...inheritedProps,
+          scope: [...(inheritedProps.scope || []), 'delete button'],
+        })}
+      >
+        {({logEvent}) => (
+          <Popup
+            trigger={
+              <Button
+                basic
+                compact
+                data-testid="delete-button"
+                onClick={e => e.stopPropagation()}
+                icon={<Icon name="trash alternate" />}
+              />
+            }
+            header="Are you sure?"
+            content={
+              <>
+                This file and all of its versions and history will be deleted
+                <Divider />
+                <Button
+                  data-testid="delete-confirm"
+                  negative
+                  fluid
+                  icon={<Icon name="trash alternate" />}
+                  content="Delete"
+                  onClick={e => {
+                    e.stopPropagation();
+                    logEvent('delete file');
+                    deleteFile({variables: {kfId: node.kfId}});
+                  }}
+                />
+              </>
+            }
+            on="click"
+            position="top right"
+          />
+        )}
+      </Amplitude>
+    )}
+  </Button.Group>
+);
 
 FileActionButtons.propTypes = {
   /** study file object */
@@ -102,4 +135,15 @@ FileActionButtons.propTypes = {
   fluid: PropTypes.bool,
 };
 
-export default FileActionButtons;
+const AmplitudeActionButtons = props => (
+  <Amplitude
+    eventProperties={inheritedProps => ({
+      ...inheritedProps,
+      scope: [...(inheritedProps.scope || []), 'file actions'],
+    })}
+  >
+    <FileActionButtons {...props} />
+  </Amplitude>
+);
+
+export default AmplitudeActionButtons;
