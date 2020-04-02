9cafd4b05ae5bd2d306b88ff4310774b8925faca
XuTheBunny <xzhu.fg@gmail.com>
2020-04-01 23:16:03 -0400

    ✨ Add link access to the study/file/version kfId in event description

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {List, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 import TimeAgo from 'react-timeago';
 import {eventType} from '../../common/enums';
 import {longDate} from '../../common/dateUtils';
@@ -11,6 +12,41 @@ import {longDate} from '../../common/dateUtils';
  * - Text descrbing who did what
  * - Timeago text indication when the action happened
  */
+const linkDescription = node => {
+  const studyId = node.study ? node.study.kfId : 'NO_STUDY_ID';
+  const fileId = node.file ? node.file.kfId : 'NO_FILE_ID';
+  const versionId = node.version ? node.version.kfId : 'NO_VERSION_ID';
+  var entities = {};
+  entities[studyId] = () => (
+    <Link to={`/study/${studyId}/basic-info/info`} key={studyId}>
+      {studyId + ' '}
+    </Link>
+  );
+  entities[fileId] = () => (
+    <Link to={`/study/${studyId}/documents/${fileId}`} key={fileId}>
+      {fileId + ' '}
+    </Link>
+  );
+  entities[versionId] = () => (
+    <Link to={`/study/${studyId}/documents/${fileId}`} key={versionId}>
+      {versionId + ' '}
+    </Link>
+  );
+  const entityPattern = /(SD_\w{8}|FV_\w{8}|SF_\w{8})/g;
+  const matches = node.description.split(entityPattern);
+  var parts = [];
+  for (var i = 0; i < matches.length; i += 2) {
+    parts = [
+      ...parts,
+      matches[i],
+      Object.keys(entities).includes(matches[i + 1])
+        ? entities[matches[i + 1]]()
+        : matches[i + 1],
+    ];
+  }
+  return parts;
+};
+
 const EventList = ({events}) => (
   <List relaxed="very">
     {events &&
@@ -37,7 +73,7 @@ const EventList = ({events}) => (
                 title={longDate(node.createdAt)}
               />
             </List.Content>
-            {node.description}
+            {linkDescription(node)}
           </List.Content>
         </List.Item>
       ))}
