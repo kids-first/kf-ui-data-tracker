6f47557e72d4e470f1cdd69627554dbac1c1134c
Dan Kolbman <dankolbman@gmail.com>
2020-04-17 11:09:16 -0400

    :art: Restyle study list items

--- src/components/StudyList/StudyTable.js
+++ src/components/StudyList/StudyTable.js
@@ -1,124 +1,112 @@
 import React from 'react';
-import TimeAgo from 'react-timeago';
 import {Link, withRouter} from 'react-router-dom';
-import {Table, Icon, Popup} from 'semantic-ui-react';
-import FileCounts from '../StudyInfo/FileCounts';
-import CavaticaCounts from '../StudyInfo/CavaticaCounts';
-import CavaticaLogo from '../../assets/CavaticaLogo';
-import {longDate} from '../../common/dateUtils';
 import {
-  countStudyNotification,
-  countProjectNotification,
-  countFileNotification,
-  trackedStudyFields,
-  trackedResearchStudyFields,
-} from '../../common/notificationUtils';
-import {hasPermission} from '../../common/permissions';
+  Button,
+  Header,
+  Label,
+  List,
+  Table,
+  Icon,
+  Popup,
+  Progress,
+  Rating,
+} from 'semantic-ui-react';
+import CavaticaLogo from '../../assets/CavaticaLogo';
+
+const PopupButton = ({header, content, icon, label}) => (
+  <Popup
+    inverted
+    header={header}
+    content={content}
+    position="top right"
+    trigger={
+      <Button icon={icon} style={{position: 'relative'}} content={label} />
+    }
+  />
+);
 
 /**
- * Renders a single row in the table
+ * Formats a link to a release
  */
-const TableValue = ({row, col, title, isResearch, myProfile}) => {
-  const coordUrl = process.env.REACT_APP_COORD_UI + 'releases/';
-  switch (col) {
-    case 'files':
-      return (
-        <FileCounts
-          files={row[col].edges}
-          showWarning={myProfile && hasPermission(myProfile, 'add_file')}
-          title={title}
-          hideIcon
-          wrap
-        />
-      );
-    case 'projects':
-      return (
-        <CavaticaCounts
-          showWarning={myProfile && hasPermission(myProfile, 'add_project')}
-          projects={row[col].edges}
-          title={title}
-          hideIcon
-          wrap={!isResearch}
-          isResearch={isResearch}
-        />
-      );
-    case 'createdAt':
-    case 'modifiedAt':
-      return (
-        <TimeAgo
-          date={new Date(row[col])}
-          title={longDate(new Date(row[col]))}
-          live={false}
-        />
-      );
-    case 'description':
-      return (
-        <Link
-          to={
-            isResearch
-              ? `/research-study/${title}/basic-info`
-              : `/study/${title}/basic-info/info`
-          }
-          onClick={e => e.stopPropagation()}
-          className={
-            myProfile && hasPermission(myProfile, 'change_study')
-              ? 'text-red'
-              : null
-          }
-        >
-          {isResearch
-            ? trackedResearchStudyFields.length -
-              row[col].missingValue +
-              '/' +
-              trackedResearchStudyFields.length +
-              ' complete'
-            : trackedStudyFields.length -
-              row[col].missingValue +
-              '/' +
-              trackedStudyFields.length +
-              ' complete'}
-        </Link>
-      );
-    case 'release':
-      const verson = row[col].node ? row[col].node.version : 'Not Published';
-      const time = row[col].node
-        ? longDate(new Date(row[col].node.createdAt))
-        : null;
-      const kfId = row[col].node ? row[col].node.kfId : '';
-      return (
-        <Popup
-          inverted
-          position="top center"
-          content={'Published on ' + time}
-          disabled={!time}
-          trigger={
-            <>
-              {verson === 'Not Published' ? (
-                <span className="text-gray">{verson}</span>
-              ) : (
-                <a
-                  href={`${coordUrl + kfId}`}
-                  onClick={e => e.stopPropagation()}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {verson + ' '}
-                  <Icon link size="small" name="external" />
-                </a>
-              )}
-            </>
-          }
-        />
-      );
-    default:
-      return row[col];
-  }
-};
+const Release = () => (
+  <Link to="/">
+    0.1.0{' '}
+    <Icon.Group>
+      <Icon name="tag" />
+      <Icon corner="top right" name="external" />
+    </Icon.Group>
+  </Link>
+);
+
+const ActionButtons = study => (
+  <Button.Group icon basic>
+    <PopupButton
+      header="Study Information"
+      content={
+        <>
+          <p>Missing 4 Fields:</p>
+          <List bulleted>
+            <List.Item>Attribution</List.Item>
+            <List.Item>Organization</List.Item>
+            <List.Item>Release Date</List.Item>
+          </List>
+        </>
+      }
+      icon="info"
+      label={<Label empty corner circular color="orange" />}
+    />
+    <PopupButton header="Documents" content="hello" icon="file" />
+    <PopupButton
+      header="Cavatica Projects"
+      content="hello"
+      icon={<CavaticaLogo className="mr-5 vertical-middle" />}
+    />
+    <PopupButton header="Collaborators" content="users" icon="users" />
+  </Button.Group>
+);
+
+const renderRow = node => ({
+  key: node.kfId,
+  cells: [
+    {
+      key: 'name',
+      content: (
+        <Header size="medium">
+          <Link to={'/study/' + node.kfId + '/basic-info/info'}>
+            {node.name}
+          </Link>
+          <Header.Subheader>
+            {(node.collaborators.edges.length &&
+              node.collaborators.edges[0].node.username) ||
+              node.shortName}
+          </Header.Subheader>
+        </Header>
+      ),
+    },
+    {
+      key: 'kfId',
+      width: 1,
+      content: <code>{node.kfId}</code>,
+      textAlign: 'center',
+    },
+    {
+      key: 'version',
+      textAlign: 'center',
+      width: 1,
+      content: <Release study={node} />,
+    },
+    {
+      key: 'actions',
+      textAlign: 'right',
+      content: <ActionButtons study={node} />,
+      width: 1,
+    },
+  ],
+});
 
 const StudyTable = ({
   studyList,
   loading,
-  exclude = [],
   clickable = true,
   history,
   myProfile,
@@ -127,113 +115,27 @@ const StudyTable = ({
   if (loading) {
     return <h2>loading studies</h2>;
   }
-  const hidden = ['id', '__typename', ...exclude];
-  const cols = Object.keys(
-    studyList.length > 0
-      ? studyList[0].node
-      : {'No access to any studies yet': []},
-  ).filter(v => hidden.indexOf(v) < 0);
 
-  // Formats the study objects and computes file state counts
-  const studies = studyList.map(({node}) =>
-    cols.reduce((row, col) => {
-      if (col === 'description') {
-        row[col] = {
-          missingValue: countStudyNotification(node, isResearch),
-          missingProject: countProjectNotification(node),
-          requiredFileChanges: countFileNotification(node),
-        };
-      } else if (col === 'name') {
-        row[col] =
-          node.name && node.name.length > 0 ? node.name : node.shortName;
-      } else {
-        row[col] = node[col];
-      }
-      return row;
-    }, {}),
-  );
+  const studies = studyList.map(({node}) => node);
 
-  const tableHeaderCell = text => {
-    var headerText =
-      text
-        .replace(/([A-Z])/g, ' $1')
-        .charAt(0)
-        .toUpperCase() + text.replace(/([A-Z])/g, ' $1').slice(1);
+  const header = [
+    {key: 'name', content: 'Name'},
+    {key: 'kfId', content: 'Kids First ID', textAlign: 'center'},
+    {key: 'version', content: 'Version', textAlign: 'center'},
+    {key: 'actions', content: 'Actions', textAlign: 'center'},
+  ];
 
-    switch (text) {
-      case 'kfId':
-        return (
-          <Table.HeaderCell key={text}>Kids First Study ID</Table.HeaderCell>
-        );
-      case 'name':
-        return <Table.HeaderCell key={text}>Study Name</Table.HeaderCell>;
-      case 'description':
-        return (
-          <Table.HeaderCell key={text}>
-            <Icon name="clipboard list" color="grey" />
-            Study Info
-          </Table.HeaderCell>
-        );
-      case 'projects':
-        return (
-          <Table.HeaderCell key={text}>
-            <CavaticaLogo className="mr-5 vertical-middle" />
-            <span>Cavatica Projects</span>
-          </Table.HeaderCell>
-        );
-      case 'files':
-        return (
-          <Table.HeaderCell key={text}>
-            <Icon name="file" color="grey" />
-            Documents
-          </Table.HeaderCell>
-        );
-      case 'release':
-        return (
-          <Table.HeaderCell key={text}>
-            <Icon name="tag" color="grey" />
-            Latest Release
-          </Table.HeaderCell>
-        );
-      default:
-        return <Table.HeaderCell key={text}>{headerText}</Table.HeaderCell>;
-    }
-  };
   return (
-    <Table striped selectable columns={Object.keys(studies[0]).length}>
-      <Table.Header>
-        <Table.Row>{cols.map(v => tableHeaderCell(v))}</Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {studies.map((row, idx) => (
-          <Table.Row
-            data-testid="table-row"
-            tabIndex="0"
-            key={idx}
-            onClick={() => {
-              if (clickable)
-                history.push(
-                  isResearch
-                    ? `/research-study/${row.kfId}/basic-info`
-                    : `/study/${row.kfId}/documents`,
-                );
-            }}
-          >
-            {cols.map((col, idx) => (
-              <Table.Cell key={idx}>
-                <TableValue
-                  row={row}
-                  col={col}
-                  title={row.kfId}
-                  isResearch={isResearch}
-                  myProfile={myProfile}
-                />
-              </Table.Cell>
-            ))}
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+    <Table
+      singleLine
+      striped
+      selectable
+      sortable
+      celled
+      headerRow={header}
+      tableData={studies}
+      renderBodyRow={renderRow}
+    />
   );
 };
 
