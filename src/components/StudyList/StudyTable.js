341409b524c244c7671d204d75f001ae1d3c618e
Dan Kolbman <dankolbman@gmail.com>
2020-04-24 09:32:51 -0400

    :lipstick: Style investigators sub header

--- src/components/StudyList/StudyTable.js
+++ src/components/StudyList/StudyTable.js
@@ -1,75 +1,97 @@
 import React, {useState} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import {Amplitude} from '@amplitude/react-amplitude';
-import {Header, Table} from 'semantic-ui-react';
+import {Header, Image, Label, Table} from 'semantic-ui-react';
+import defaultAvatar from '../../assets/defaultAvatar.png';
 import ActionButtons from './ActionButtons';
 import Release from './Release';
 
-const renderRow = node => ({
-  key: node.kfId,
-  cells: [
-    {
-      key: 'name',
-      selectable: true,
-      className: 'overflow-cell-container',
-      content: (
-        <Amplitude
-          eventProperties={inheritedProps => ({
-            ...inheritedProps,
-            scope: inheritedProps.scope
-              ? [...inheritedProps.scope, 'study name']
-              : ['study name'],
-          })}
-        >
-          {({logEvent}) => (
-            <Link
-              to={'/study/' + node.kfId + '/basic-info/info'}
-              onClick={() => logEvent('click')}
-              className="overflow-cell"
-            >
-              <Header size="medium">
-                {node.name}
-                <Header.Subheader>
-                  {(node.collaborators.edges.length &&
-                    node.collaborators.edges[0].node.username) ||
-                    node.shortName}
-                </Header.Subheader>
-              </Header>
-            </Link>
-          )}
-        </Amplitude>
-      ),
-    },
-    {
-      key: 'kfId',
-      width: 1,
-      textAlign: 'center',
-      selectable: true,
-      content: (
-        <Link to={'/study/' + node.kfId + '/basic-info/info'}>
-          <code>{node.kfId}</code>
-        </Link>
-      ),
-    },
-    {
-      key: 'version',
-      textAlign: 'center',
-      width: 1,
-      selectable: true,
-      content: (
-        <Release
-          release={node.release && node.release.node && node.release.node}
-        />
-      ),
-    },
-    {
-      key: 'actions',
-      textAlign: 'right',
-      content: <ActionButtons study={node} />,
-      width: 1,
-    },
-  ],
-});
+const Investigators = ({investigators}) => {
+  if (investigators.length) {
+    return (
+      <Label.Group>
+        {investigators.map(user => (
+          <Label image key={user.id}>
+            <img alt={user.username} src={user.picture || defaultAvatar} />
+            {user.username}
+          </Label>
+        ))}
+      </Label.Group>
+    );
+  }
+  return <span>No investigators</span>;
+};
+
+const renderRow = node => {
+  // TODO: Filter out only users in the Investigators group
+  const investigators =
+    node.collaborators.edges.length &&
+    node.collaborators.edges.map(({node}) => node);
+
+  return {
+    key: node.kfId,
+    cells: [
+      {
+        key: 'name',
+        selectable: true,
+        className: 'overflow-cell-container',
+        content: (
+          <Amplitude
+            eventProperties={inheritedProps => ({
+              ...inheritedProps,
+              scope: inheritedProps.scope
+                ? [...inheritedProps.scope, 'study name']
+                : ['study name'],
+            })}
+          >
+            {({logEvent}) => (
+              <Link
+                to={'/study/' + node.kfId + '/basic-info/info'}
+                onClick={() => logEvent('click')}
+                className="overflow-cell"
+              >
+                <Header size="medium" alt={node.name}>
+                  {node.name}
+                  <Header.Subheader>
+                    <Investigators investigators={investigators} />
+                  </Header.Subheader>
+                </Header>
+              </Link>
+            )}
+          </Amplitude>
+        ),
+      },
+      {
+        key: 'kfId',
+        width: 1,
+        textAlign: 'center',
+        selectable: true,
+        content: (
+          <Link to={'/study/' + node.kfId + '/basic-info/info'}>
+            <code>{node.kfId}</code>
+          </Link>
+        ),
+      },
+      {
+        key: 'version',
+        textAlign: 'center',
+        width: 1,
+        selectable: true,
+        content: (
+          <Release
+            release={node.release && node.release.node && node.release.node}
+          />
+        ),
+      },
+      {
+        key: 'actions',
+        textAlign: 'right',
+        content: <ActionButtons study={node} />,
+        width: 1,
+      },
+    ],
+  };
+};
 
 const StudyTable = ({
   studyList,
