af2baf14f5c34877a002ccb928d95cffdac75e18
Dan Kolbman <dankolbman@gmail.com>
2020-04-23 21:49:34 -0400

    :sparkles: Add column sorting

--- src/components/StudyList/StudyTable.js
+++ src/components/StudyList/StudyTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import {Amplitude} from '@amplitude/react-amplitude';
 import {Header, Table} from 'semantic-ui-react';
@@ -77,16 +77,63 @@ const StudyTable = ({
   myProfile,
   isResearch,
 }) => {
+  const [sorting, setSorting] = useState({
+    column: 'name',
+    direction: 'descending',
+  });
+
   if (loading) {
     return <h2>loading studies</h2>;
   }
 
-  const studies = studyList.map(({node}) => node);
+  const handleSort = column => () => {
+    console.log('sort', column, sorting.column, sorting.direction);
+    if (sorting.column !== column) {
+      setSorting({
+        column,
+        direction: 'ascending',
+      });
+    } else {
+      setSorting({
+        column,
+        direction:
+          sorting.direction === 'ascending' ? 'descending' : 'ascending',
+      });
+    }
+  };
+
+  const studies = studyList
+    .map(({node}) => ({
+      ...node,
+      version: node.release.node && node.release.node.version,
+    }))
+    .sort((s1, s2) =>
+      sorting.direction === 'ascending'
+        ? 1
+        : -1 * s1[sorting.column] > s2[sorting.column],
+    );
 
   const header = [
-    {key: 'name', content: 'Name'},
-    {key: 'kfId', content: 'Kids First ID', textAlign: 'center'},
-    {key: 'version', content: 'Version', textAlign: 'center'},
+    <Table.HeaderCell
+      key="name"
+      content="Name"
+      sorted={sorting.column === 'name' ? sorting.direction : null}
+      onClick={handleSort('name')}
+    />,
+    <Table.HeaderCell
+      key="kfId"
+      content="Kids First ID"
+      textAlign="center"
+      sorted={sorting.column === 'kfId' ? sorting.direction : null}
+      onClick={handleSort('kfId')}
+    />,
+    <Table.HeaderCell
+      key="version"
+      content="Version"
+      textAlign="center"
+      sorted={sorting.column === 'version' ? sorting.direction : null}
+      onClick={handleSort('version')}
+    />,
     {key: 'actions', content: 'Actions', textAlign: 'center'},
   ];
 
