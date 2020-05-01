f739888a28f9ee61805a6ef609b26c55ef1ad6d3
XuTheBunny <xzhu.fg@gmail.com>
2020-04-30 22:24:24 -0400

    ✅ Add test for study collabrators tab

---
+++ cypress/integration/studyInfo/collaborators.spec.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+context('Admin Study Collaborators', () => {
+  before(() => {
+    cy.resetdb();
+    cy.as(['Administrators']);
+  });
+
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/study/SD_ODWXI1TE/collaborators');
+  });
+
+  it('Show collaborators information with add and remove access', () => {
+    // Show no collaborator message
+    cy.contains('h2', 'No Collaborators Yet').should('exist');
+    // Click add collaborator button to add a user
+    cy.contains('button', 'ADD COLLABORATOR').click();
+    cy.contains('div', 'Choose a user').click();
+    cy.contains('span', 'Andrew Aguilar').click();
+    cy.get('[data-testid="add-button"]').click();
+    // Show newly added collaborator
+    cy.contains('div', 'Andrew Aguilar').should('exist');
+    // Click remove collaborator button to remove the user
+    cy.contains('button', 'Remove').click();
+    cy.get('[data-testid="remove-confirm"]').click();
+    // Show no collaborator message
+    cy.contains('h2', 'No Collaborators Yet').should('exist');
+  });
+});
+
+context('Investigator Study Collaborators', () => {
+  before(() => {
+    cy.resetdb();
+    cy.as(['Investigators']);
+  });
+
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/study/SD_ME0WME0W/collaborators');
+  });
+
+  it('Show collaborators information without add or remove access', () => {
+    // Show test collaborator
+    cy.contains('div', 'testuser').should('exist');
+    // No add collaborators button
+    cy.contains('button', 'ADD COLLABORATOR').should('not.exist');
+    // No remove collaborator button
+    cy.contains('button', 'Remove').should('not.exist');
+  });
+});
