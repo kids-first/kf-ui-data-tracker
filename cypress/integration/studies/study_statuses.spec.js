35024251e8195315d92a869196a3e5fb5d051eba
Dan Kolbman <dankolbman@gmail.com>
2020-05-04 15:28:12 -0400

    :white_check_mark: Add tests for sequencing status

---
+++ cypress/integration/studies/study_statuses.spec.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress" />
+
+context('Admin Study Status', () => {
+  before(() => {
+    cy.resetdb();
+    cy.as(['Administrators']);
+  });
+
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/');
+    // Always show all studies by default
+    cy.contains('label', 'Show only my studies').click();
+    // Always enable the status columns
+    cy.contains('span.ui.dropdown', 'Change Columns').click();
+    cy.contains('label', 'Sequencing Status').click();
+    cy.contains('span.ui.dropdown', 'Change Columns').click();
+  });
+
+  it('changes sequencing status', () => {
+    // All documents should have unknown sequencing status
+    cy.get('[data-cy="sequencing status cell"]')
+      .its('length')
+      .should('eq', 4);
+
+    cy.get('[data-cy="sequencing status cell"]')
+      .find('div.ui.dropdown')
+      .should('exist');
+
+    // Change status of first Unknown study
+    cy.contains('[data-cy="sequencing status cell"]', 'Unknown').click();
+    cy.contains('span.text', 'Not Started').click();
+
+    cy.get('[data-cy="sequencing status cell"].error')
+      .its('length')
+      .should('eq', 3);
+  });
+});
+
+context('Investigator Study Status', () => {
+  before(() => {
+    cy.resetdb();
+    cy.as(['Investigators']);
+  });
+
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/');
+    cy.contains('span.ui.dropdown', 'Change Columns').click();
+    cy.contains('label', 'Sequencing Status').click();
+    cy.contains('span.ui.dropdown', 'Change Columns').click();
+  });
+
+  it('changes sequencing status', () => {
+    // Only one study should be available
+    cy.get('[data-cy="sequencing status cell"]')
+      .its('length')
+      .should('eq', 1);
+
+    cy.get('[data-cy="sequencing status cell"]')
+      .find('div.ui.dropdown')
+      .should('not.exist');
+  });
+});
