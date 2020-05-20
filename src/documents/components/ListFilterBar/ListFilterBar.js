40d376bd9330e2035d8ac4ce07f84a16ad3d3bf3
Dan Kolbman <dankolbman@gmail.com>
2020-05-20 19:21:43 -0400

    :art: Restyle the document filter list

--- src/documents/components/ListFilterBar/ListFilterBar.js
+++ src/documents/components/ListFilterBar/ListFilterBar.js
@@ -1,20 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Icon,
-  Button,
-  Dropdown,
-  Input,
-  Segment,
-  Responsive,
-} from 'semantic-ui-react';
+import {Icon, Form, Responsive} from 'semantic-ui-react';
 import {fileTypeDetail, defaultTagOptions} from '../../../common/enums';
+
 /**
  * Filter Bar for Study Files, returns filtered list in "filteredList" render prop
  */
 const ListFilterBar = ({fileList, filters, setFilters}) => {
-  const [showFilter, setShowFilter] = useState(false);
-
   var defaultTags = {};
   defaultTagOptions.map(tagObj => (defaultTags[tagObj.key] = tagObj.text));
   var tagList = [];
@@ -45,42 +37,54 @@ const ListFilterBar = ({fileList, filters, setFilters}) => {
 
   return (
     <>
-      <Responsive
-        as={Segment}
-        maxWidth={699}
-        basic
-        className="noHorizontalPadding"
-      >
-        <Button
-          data-testid="show-filter-button"
-          basic={!showFilter}
-          floated="right"
-          primary={
-            filters.typeFilterStatus !== '' || filters.tagFilterStatus !== ''
-          }
-          icon="filter"
-          onClick={() => setShowFilter(!showFilter)}
-        />
-        <Input
-          aria-label="file-search-input"
-          className="mr-80"
-          fluid
-          icon="search"
-          onChange={(e, {value}) =>
-            setFilters({...filters, searchString: value})
-          }
-          value={filters.searchString}
-        />
-        {showFilter && (
-          <Segment>
-            <p className="mb-5">
-              <Icon name="filter" />
-              Filter by:
-            </p>
-            <Dropdown
-              fluid
+      <Responsive maxWidth={999}>
+        <Form as="div">
+          <Form.Input
+            fluid
+            aria-label="file-search-input"
+            icon="search"
+            placeholder="Search by name"
+            onChange={(e, {value}) =>
+              setFilters({...filters, searchString: value})
+            }
+            value={filters.searchString}
+          />
+          <Form.Group widths="equal">
+            <Form.Dropdown
+              button
+              placeholder="Tags"
               selection
               clearable
+              disabled={tagOptions.length === 0}
+              selectOnBlur={false}
+              value={filters.tagFilterStatus}
+              options={tagOptions}
+              onChange={(e, {value}) =>
+                setFilters({...filters, tagFilterStatus: value})
+              }
+            />
+            <Form.Dropdown
+              button
+              selection
+              clearable
+              selectOnBlur={false}
+              value={filters.typeFilterStatus}
+              options={typeOptions}
+              placeholder="Document type"
+              onChange={(e, {value}) =>
+                setFilters({...filters, typeFilterStatus: value})
+              }
+            />
+          </Form.Group>
+        </Form>
+      </Responsive>
+      <Responsive minWidth={1000}>
+        <Form as="div">
+          <Form.Group inline>
+            <Form.Dropdown
+              selection
+              clearable
+              label="Filter by:"
               disabled={tagOptions.length === 0}
               selectOnBlur={false}
               value={filters.tagFilterStatus}
@@ -90,8 +94,7 @@ const ListFilterBar = ({fileList, filters, setFilters}) => {
                 setFilters({...filters, tagFilterStatus: value})
               }
             />
-            <Dropdown
-              fluid
+            <Form.Dropdown
               selection
               clearable
               selectOnBlur={false}
@@ -102,110 +105,17 @@ const ListFilterBar = ({fileList, filters, setFilters}) => {
                 setFilters({...filters, typeFilterStatus: value})
               }
             />
-          </Segment>
-        )}
-      </Responsive>
-      <Responsive
-        as={Segment}
-        minWidth={700}
-        maxWidth={999}
-        basic
-        className="noHorizontalPadding"
-      >
-        <Input
-          fluid
-          aria-label="file-search-input"
-          icon="search"
-          onChange={(e, {value}) =>
-            setFilters({...filters, searchString: value})
-          }
-          value={filters.searchString}
-        />
-        <Segment clearing basic className="noHorizontalPadding noMargin">
-          <Segment
-            className="noMargin noVerticalPadding noHorizontalPadding"
-            basic
-            compact
-            floated="left"
-          >
-            <Dropdown
-              labeled
-              button
-              className="icon noMargin"
-              placeholder="Tags"
-              icon="tag"
-              selection
-              clearable
-              disabled={tagOptions.length === 0}
-              selectOnBlur={false}
-              value={filters.tagFilterStatus}
-              options={tagOptions}
+            <Form.Input
+              aria-label="file-search-input"
+              icon="search"
+              placeholder="Search by name"
               onChange={(e, {value}) =>
-                setFilters({...filters, tagFilterStatus: value})
+                setFilters({...filters, searchString: value})
               }
+              value={filters.searchString}
             />
-            <Dropdown
-              button
-              selection
-              clearable
-              selectOnBlur={false}
-              value={filters.typeFilterStatus}
-              options={typeOptions}
-              placeholder="Document type"
-              onChange={(e, {value}) =>
-                setFilters({...filters, typeFilterStatus: value})
-              }
-            />
-          </Segment>
-        </Segment>
-      </Responsive>
-      <Responsive
-        as={Segment}
-        minWidth={1000}
-        clearing
-        basic
-        className="noHorizontalPadding"
-      >
-        <Segment
-          className="noMargin noVerticalPadding noHorizontalPadding"
-          basic
-          compact
-          floated="left"
-        >
-          <span className="smallLabel">Filter by:</span>
-          <Dropdown
-            selection
-            clearable
-            disabled={tagOptions.length === 0}
-            selectOnBlur={false}
-            value={filters.tagFilterStatus}
-            options={tagOptions}
-            placeholder="Tag"
-            onChange={(e, {value}) =>
-              setFilters({...filters, tagFilterStatus: value})
-            }
-          />
-          <Dropdown
-            selection
-            clearable
-            selectOnBlur={false}
-            value={filters.typeFilterStatus}
-            options={typeOptions}
-            placeholder="Document type"
-            onChange={(e, {value}) =>
-              setFilters({...filters, typeFilterStatus: value})
-            }
-          />
-        </Segment>
-        <Input
-          fluid
-          aria-label="file-search-input"
-          icon="search"
-          onChange={(e, {value}) =>
-            setFilters({...filters, searchString: value})
-          }
-          value={filters.searchString}
-        />
+          </Form.Group>
+        </Form>
       </Responsive>
     </>
   );
