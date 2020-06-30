0579d38f92dd658464e4798323cadc6780673895
XuTheBunny <xzhu.fg@gmail.com>
2020-06-30 00:39:04 -0400

    ✨ Add query to get all referral tokens

--- src/admin/queries.js
+++ src/admin/queries.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import {TOKEN_FIELDS} from './fragments';
-import {STUDY_BASIC_FIELDS} from '../state/fragments';
+import {STUDY_BASIC_FIELDS, CREATOR_FIELDS} from '../state/fragments';
 
 // Query to get developer tokens
 export const GET_DEV_TOKENS = gql`
@@ -33,5 +33,46 @@ export const GET_BUCKETS = gql`
       }
     }
   }
+  ${STUDY_BASIC_FIELDS}
+`;
+
+// All pending invites
+export const ALL_REFERRAL_TOEKNS = gql`
+  query allReferralTokens {
+    allReferralTokens {
+      edges {
+        node {
+          id
+          email
+          claimed
+          isValid
+          createdAt
+          createdBy {
+            ...CreatorFields
+          }
+          claimedBy {
+            dateJoined
+            ...CreatorFields
+          }
+          studies {
+            edges {
+              node {
+                ...StudyBasicFields
+              }
+            }
+          }
+          groups {
+            edges {
+              node {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  ${CREATOR_FIELDS}
   ${STUDY_BASIC_FIELDS}
 `;
