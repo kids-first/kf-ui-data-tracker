cf052f85436511fc99aa95035ec356a8a9cfac02
XuTheBunny <xzhu.fg@gmail.com>
2020-09-21 21:37:37 -0400

    ✨ Add timeline component taking event object with empty message

---
+++ src/documents/components/FileDetail/Timelines.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Feed, Message} from 'semantic-ui-react';
+import TimeAgo from 'react-timeago';
+import defaultAvatar from '../../../assets/defaultAvatar.png';
+import {longDate} from '../../../common/dateUtils';
+
+/**
+ * Displays study document timelines with empty message
+ */
+const Timelines = ({eventData}) => {
+  const events =
+    eventData && eventData.allEvents ? eventData.allEvents.edges : [];
+
+  return (
+    <Feed>
+      {events.length > 0 ? (
+        events.map(({node}) => (
+          <Feed.Event key={node.id}>
+            <Feed.Label image={node.user.picture || defaultAvatar} />
+            <Feed.Content>
+              <Feed.Date className="text-10 lh-inherit">
+                {node.createdAt ? (
+                  <TimeAgo
+                    date={node.createdAt}
+                    live={false}
+                    title={longDate(node.createdAt)}
+                  />
+                ) : (
+                  'Unknown'
+                )}
+              </Feed.Date>
+              <Feed.Summary className="text-normal">
+                {node.description}
+              </Feed.Summary>
+            </Feed.Content>
+          </Feed.Event>
+        ))
+      ) : (
+        <Message
+          warning
+          icon="warning circle"
+          header="No Data"
+          content="No events found for this document"
+        />
+      )}
+    </Feed>
+  );
+};
+
+export default Timelines;
