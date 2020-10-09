6a183548a1925ed8b95d02b0c37574b0e1f178b0
XuTheBunny <xzhu.fg@gmail.com>
2020-10-09 18:32:25 -0400

    ✨ Add services list view with register a new service button

---
+++ src/releases/views/ServicesListView.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import {useQuery} from '@apollo/react-hooks';
+import {useHistory} from 'react-router-dom';
+import {Segment, Container, Button, Header} from 'semantic-ui-react';
+import {ServiceList} from '../components/ServiceList';
+import {MY_PROFILE} from '../../state/queries';
+import {ALL_SERVICES} from '../queries';
+import {hasPermission} from '../../common/permissions';
+
+const ServicesListView = props => {
+  const history = useHistory();
+
+  const {
+    loading: servicesLoading,
+    error: servicesError,
+    data: services,
+  } = useQuery(ALL_SERVICES, {
+    context: {clientName: 'coordinator'},
+  });
+
+  const {data: profileData} = useQuery(MY_PROFILE);
+  const myProfile = profileData && profileData.myProfile;
+
+  return (
+    <Container as={Segment} basic vertical>
+      <Helmet>
+        <title>{`KF Data Tracker - Release Services`}</title>
+      </Helmet>
+      {myProfile && hasPermission(myProfile, 'view_settings') && (
+        <Button
+          color="purple"
+          labelPosition="left"
+          floated="right"
+          icon="tag"
+          content="Register a Service"
+          onClick={() => history.push('/releases/services/new-service')}
+        />
+      )}
+      <Header as="h1" className="noMargin">
+        Kids First Release Task Services
+      </Header>
+      <ServiceList
+        services={services && services.allTaskServices.edges}
+        loading={servicesLoading}
+        error={servicesError}
+      />
+    </Container>
+  );
+};
+
+export default ServicesListView;
--- src/releases/views/index.js
+++ src/releases/views/index.js
@@ -1,3 +1,4 @@
 export {default as HomeView} from './HomeView';
 export {default as NewReleaseView} from './NewReleaseView';
 export {default as NavigationView} from './NavigationView';
+export {default as ServicesListView} from './ServicesListView';
