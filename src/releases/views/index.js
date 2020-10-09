5f10df186e89b4097a6064a6f7ac62d839b41dae
XuTheBunny <xzhu.fg@gmail.com>
2020-10-09 18:33:12 -0400

    ✨ Add register a new service view with error handling

--- src/releases/views/index.js
+++ src/releases/views/index.js
@@ -2,3 +2,4 @@ export {default as HomeView} from './HomeView';
 export {default as NewReleaseView} from './NewReleaseView';
 export {default as NavigationView} from './NavigationView';
 export {default as ServicesListView} from './ServicesListView';
+export {default as NewServiceView} from './NewServiceView';
