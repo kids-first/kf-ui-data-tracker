3ad08311163b4eb793a14bc066dbf58c0f743c5f
XuTheBunny <xzhu.fg@gmail.com>
2020-10-09 18:37:54 -0400

    ✨ Add UPDATE_SERVICE and CREATE_SERVICE mutation

--- src/releases/mutations.js
+++ src/releases/mutations.js
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import {RELEASE_FIELDS} from './fragments';
+import {RELEASE_FIELDS, SERVICE_FIELDS} from './fragments';
 
 export const START_RELEASE = gql`
   mutation StartRelease($input: ReleaseInput!) {
@@ -11,3 +11,26 @@ export const START_RELEASE = gql`
   }
   ${RELEASE_FIELDS}
 `;
+
+export const UPDATE_SERVICE = gql`
+  mutation UpdateService($taskService: ID!, $input: TaskServiceInput!) {
+    updateTaskService(taskService: $taskService, input: $input) {
+      taskService {
+        ...ServiceFields
+      }
+    }
+  }
+  ${SERVICE_FIELDS}
+`;
+
+export const CREATE_SERVICE = gql`
+  mutation CreateService($input: TaskServiceInput!) {
+    createTaskService(input: $input) {
+      taskService {
+        ...ServiceFields
+      }
+    }
+  }
+  ${SERVICE_FIELDS}
+`;
+
