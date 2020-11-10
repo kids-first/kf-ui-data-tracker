cbd9874f1905df41114b6a8cebff9f0942b967cc
Dan Kolbman <dankolbman@gmail.com>
2020-11-10 09:02:55 -0500

    :recycle: Update study list to get releases from study creator

--- src/state/queries.js
+++ src/state/queries.js
@@ -31,6 +31,10 @@ export const ALL_STUDIES = gql`
                 kfId
                 name
                 version
+                createdAt
+                creator {
+                  ...UserFields
+                }
               }
             }
           }
