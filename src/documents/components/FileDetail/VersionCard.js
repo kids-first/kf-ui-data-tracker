af0857a582124948751ceac563ea121036603c58
XuTheBunny <xzhu.fg@gmail.com>
2020-11-17 18:28:03 -0500

    💄 Adjust version card buttons and description position

--- src/documents/components/FileDetail/VersionCard.js
+++ src/documents/components/FileDetail/VersionCard.js
@@ -7,7 +7,7 @@ import {
   Menu,
   Popup,
   Segment,
-  Grid,
+  Image,
   Table,
 } from 'semantic-ui-react';
 import Markdown from 'react-markdown';
@@ -23,213 +23,174 @@ const VersionCard = ({version, downloadFileMutation, studyId, fileId}) => {
   return (
     <Card fluid className="mt-6">
       <Card.Content className="py-14" floated="left">
-        <Grid verticalAlign="middle">
-          <Grid.Row>
-            <Grid.Column
-              mobile={16}
-              tablet={16}
-              computer={13}
-              className="noMargin"
-            >
-              <Header as="h5" floated="left" className="noMargin">
-                {lengthLimit(version.fileName, 70)}
-              </Header>
-              <Card.Meta className="noMargin ml-10">
-                {version.size
-                  ? formatFileSize(version.size, true)
-                  : 'Size Unknown'}
-              </Card.Meta>
-              <Amplitude
-                eventProperties={inheritedProps => ({
-                  ...inheritedProps,
-                  scope: inheritedProps.scope
-                    ? [
-                        ...inheritedProps.scope,
-                        'button',
-                        'copy version ID button',
-                      ]
-                    : ['button', 'copy version ID button'],
-                })}
-              >
-                {({logEvent}) => (
-                  <Popup
-                    inverted
-                    position="top left"
-                    content={
-                      copied === 'id' ? (
-                        <Icon name="checkmark" color="green" />
-                      ) : (
-                        'Copy version ID'
-                      )
-                    }
-                    trigger={
-                      <CopyToClipboard
-                        text={version.kfId}
-                        onCopy={() => {
-                          logEvent('click');
-                          setCopied('id');
-                          setTimeout(() => {
-                            setCopied('');
-                          }, 700);
-                        }}
-                      >
-                        <Card.Meta as="a" className="noMargin ml-10">
-                          <code>{version.kfId}</code>
-                        </Card.Meta>
-                      </CopyToClipboard>
-                    }
-                  />
-                )}
-              </Amplitude>
+        <Menu
+          as={Segment}
+          secondary
+          compact
+          floated="right"
+          className="noPadding min-h-22"
+        >
+          <Amplitude
+            eventProperties={inheritedProps => ({
+              ...inheritedProps,
+              scope: inheritedProps.scope
+                ? [...inheritedProps.scope, 'button', 'preview button']
+                : ['button', 'preview button'],
+            })}
+          >
+            {({logEvent}) => (
               <Popup
-                wide="very"
-                disabled={
-                  !version.description || version.description.length === 0
-                }
+                content="Preview this Version"
                 position="top left"
-                content={
-                  <Segment basic className="x-scroll noPadding">
-                    <Markdown
-                      source={version.description}
-                      renderers={{
-                        image: Image,
-                        table: props => <Table>{props.children}</Table>,
-                      }}
-                    />
-                  </Segment>
-                }
+                inverted
                 trigger={
-                  <Card.Description>
-                    {lengthLimit(version.description || '', 110)}
-                    {(!version.description ||
-                      version.description.length === 0) &&
-                      'No version summary available'}
-                  </Card.Description>
+                  <Menu.Item
+                    className="p-5 noMargin"
+                    onClick={e => {
+                      logEvent('click');
+                      e.stopPropagation();
+                      window.open(
+                        `/study/${studyId}/documents/${fileId}/versions/${version.kfId}`,
+                      );
+                    }}
+                  >
+                    <Icon name="eye" />
+                  </Menu.Item>
                 }
               />
-            </Grid.Column>
-            <Grid.Column
-              className="noMargin"
-              mobile={16}
-              tablet={16}
-              computer={3}
-            >
-              <Menu
-                as={Segment}
-                secondary
-                compact
-                floated="right"
-                className="noPadding"
-              >
-                <Amplitude
-                  eventProperties={inheritedProps => ({
-                    ...inheritedProps,
-                    scope: inheritedProps.scope
-                      ? [...inheritedProps.scope, 'button', 'preview button']
-                      : ['button', 'preview button'],
-                  })}
+            )}
+          </Amplitude>
+          <Amplitude
+            eventProperties={inheritedProps => ({
+              ...inheritedProps,
+              scope: inheritedProps.scope
+                ? [...inheritedProps.scope, 'button', 'download button']
+                : ['button', 'download button'],
+            })}
+          >
+            {({logEvent}) => (
+              <Popup
+                inverted
+                position="top left"
+                content="Download this version"
+                trigger={
+                  <Menu.Item
+                    className="p-5 noMargin"
+                    onClick={e => {
+                      logEvent('click');
+                      e.stopPropagation();
+                      downloadFile(
+                        studyId,
+                        fileId,
+                        version.kfId,
+                        downloadFileMutation,
+                      );
+                    }}
+                  >
+                    <Icon name="download" />
+                  </Menu.Item>
+                }
+              />
+            )}
+          </Amplitude>
+          <Amplitude
+            eventProperties={inheritedProps => ({
+              ...inheritedProps,
+              scope: inheritedProps.scope
+                ? [
+                    ...inheritedProps.scope,
+                    'button',
+                    'copy download url button',
+                  ]
+                : ['button', 'copy download url button'],
+            })}
+          >
+            {({logEvent}) => (
+              <Popup
+                inverted
+                position="top left"
+                content={
+                  copied === 'url' ? (
+                    <Icon name="checkmark" color="green" />
+                  ) : (
+                    'Copy download link'
+                  )
+                }
+                trigger={
+                  <CopyToClipboard
+                    text={version.downloadUrl}
+                    onCopy={() => {
+                      logEvent('click');
+                      setCopied('url');
+                      setTimeout(() => {
+                        setCopied('');
+                      }, 700);
+                    }}
+                  >
+                    <Menu.Item className="p-5 noMargin">
+                      <Icon name="copy" />
+                    </Menu.Item>
+                  </CopyToClipboard>
+                }
+              />
+            )}
+          </Amplitude>
+        </Menu>
+        <Header as="h5" floated="left" className="noMargin">
+          {lengthLimit(version.fileName, 70)}
+        </Header>
+        <Card.Meta className="noMargin ml-10">
+          {version.size ? formatFileSize(version.size, true) : 'Size Unknown'}
+        </Card.Meta>
+        <Amplitude
+          eventProperties={inheritedProps => ({
+            ...inheritedProps,
+            scope: inheritedProps.scope
+              ? [...inheritedProps.scope, 'button', 'copy version ID button']
+              : ['button', 'copy version ID button'],
+          })}
+        >
+          {({logEvent}) => (
+            <Popup
+              inverted
+              position="top left"
+              content={
+                copied === 'id' ? (
+                  <Icon name="checkmark" color="green" />
+                ) : (
+                  'Copy version ID'
+                )
+              }
+              trigger={
+                <CopyToClipboard
+                  text={version.kfId}
+                  onCopy={() => {
+                    logEvent('click');
+                    setCopied('id');
+                    setTimeout(() => {
+                      setCopied('');
+                    }, 700);
+                  }}
                 >
-                  {({logEvent}) => (
-                    <Popup
-                      content="Preview this Version"
-                      position="top left"
-                      inverted
-                      trigger={
-                        <Menu.Item
-                          className="p-10 noMargin"
-                          onClick={e => {
-                            logEvent('click');
-                            e.stopPropagation();
-                            window.open(
-                              `/study/${studyId}/documents/${fileId}/versions/${version.kfId}`,
-                            );
-                          }}
-                        >
-                          <Icon name="eye" />
-                        </Menu.Item>
-                      }
-                    />
-                  )}
-                </Amplitude>
-                <Amplitude
-                  eventProperties={inheritedProps => ({
-                    ...inheritedProps,
-                    scope: inheritedProps.scope
-                      ? [...inheritedProps.scope, 'button', 'download button']
-                      : ['button', 'download button'],
-                  })}
-                >
-                  {({logEvent}) => (
-                    <Popup
-                      inverted
-                      position="top left"
-                      content="Download this version"
-                      trigger={
-                        <Menu.Item
-                          className="p-10 noMargin"
-                          onClick={e => {
-                            logEvent('click');
-                            e.stopPropagation();
-                            downloadFile(
-                              studyId,
-                              fileId,
-                              version.kfId,
-                              downloadFileMutation,
-                            );
-                          }}
-                        >
-                          <Icon name="download" />
-                        </Menu.Item>
-                      }
-                    />
-                  )}
-                </Amplitude>
-                <Amplitude
-                  eventProperties={inheritedProps => ({
-                    ...inheritedProps,
-                    scope: inheritedProps.scope
-                      ? [
-                          ...inheritedProps.scope,
-                          'button',
-                          'copy download url button',
-                        ]
-                      : ['button', 'copy download url button'],
-                  })}
-                >
-                  {({logEvent}) => (
-                    <Popup
-                      inverted
-                      position="top left"
-                      content={
-                        copied === 'url' ? (
-                          <Icon name="checkmark" color="green" />
-                        ) : (
-                          'Copy download link'
-                        )
-                      }
-                      trigger={
-                        <CopyToClipboard
-                          text={version.downloadUrl}
-                          onCopy={() => {
-                            logEvent('click');
-                            setCopied('url');
-                            setTimeout(() => {
-                              setCopied('');
-                            }, 700);
-                          }}
-                        >
-                          <Menu.Item className="p-10 noMargin">
-                            <Icon name="copy" />
-                          </Menu.Item>
-                        </CopyToClipboard>
-                      }
-                    />
-                  )}
-                </Amplitude>
-              </Menu>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+                  <Card.Meta as="a" className="noMargin ml-10">
+                    <code>{version.kfId}</code>
+                  </Card.Meta>
+                </CopyToClipboard>
+              }
+            />
+          )}
+        </Amplitude>
+        <Card.Description>
+          <Segment basic secondary className="x-scroll">
+            <Markdown
+              source={version.description}
+              renderers={{
+                image: Image,
+                table: props => <Table>{props.children}</Table>,
+              }}
+            />
+          </Segment>
+        </Card.Description>
       </Card.Content>
     </Card>
   );
