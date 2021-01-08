157219bbff6edada39fdde0c0f729192323ce2bd
XuTheBunny <xzhu.fg@gmail.com>
2021-01-08 11:44:50 -0500

    🔥 Remove obsolete verson modal component

--- src/documents/modals/index.js
+++ src/documents/modals/index.js
@@ -1,4 +1,3 @@
 export {default as NewVersionModal} from './NewVersionModal/NewVersionFlow';
-export {default as VersionInfoModal} from './VersionInfoModal';
 export {default as EditDocumentModal} from './EditDocumentModal';
 export {default as ExtractConfigModal} from './ExtractConfigModal';
