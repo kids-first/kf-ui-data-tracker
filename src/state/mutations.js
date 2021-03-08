8ba86b10532f7948bc33fc4eaeb98cae0b96d866
XuTheBunny <xzhu.fg@gmail.com>
2021-03-08 17:22:23 -0500

    ✨ Add approveDataReview mutation

--- src/state/mutations.js
+++ src/state/mutations.js
@@ -432,3 +432,31 @@ export const AWAIT_DATA_REVIEW = gql`
   ${VERSION_FIELDS}
 `;
 
+export const APPROVE_DATA_REVIEW = gql`
+  mutation approveDataReview($id: ID!) {
+    approveDataReview(id: $id) {
+      dataReview {
+        id
+        uuid
+        kfId
+        name
+        description
+        state
+        createdAt
+        study {
+          ...StudyBasicFields
+        }
+        versions {
+          edges {
+            node {
+              ...VersionFields
+            }
+          }
+        }
+      }
+    }
+  }
+  ${STUDY_BASIC_FIELDS}
+  ${VERSION_FIELDS}
+`;
+
