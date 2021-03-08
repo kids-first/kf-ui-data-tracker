2a98ce781a0b95ab420546bb7a2ca57306f6c9c6
XuTheBunny <xzhu.fg@gmail.com>
2021-03-08 18:18:32 -0500

    ✨ Add data reviews list view with add review button and permission check

---
+++ src/documents/views/ReviewListView.js
@@ -0,0 +1,190 @@
+import {
+  ALL_DATA_REVIEWS,
+  GET_STUDY_BY_ID,
+  MY_PROFILE,
+} from '../../state/queries';
+import {
+  Button,
+  Container,
+  Dimmer,
+  Grid,
+  List,
+  Loader,
+  Message,
+  Popup,
+  Segment,
+} from 'semantic-ui-react';
+
+import {Helmet} from 'react-helmet';
+import NotFoundView from '../../views/NotFoundView';
+import React from 'react';
+import TimeAgo from 'react-timeago';
+import {hasPermission} from '../../common/permissions';
+import {longDate} from '../../common/dateUtils';
+import {reviewStatus} from '../../common/enums';
+import {useQuery} from '@apollo/client';
+
+/**
+ * Review list screen
+ */
+const ReviewListView = ({
+  match: {
+    params: {kfId},
+  },
+  history,
+}) => {
+  const {data: studyData} = useQuery(GET_STUDY_BY_ID, {
+    variables: {
+      id: Buffer.from('StudyNode:' + kfId).toString('base64'),
+    },
+  });
+  const study = studyData && studyData.study;
+
+  const {data: profileData} = useQuery(MY_PROFILE);
+  const myProfile = profileData && profileData.myProfile;
+  const allowAddReview =
+    myProfile && hasPermission(myProfile, 'add_datareview');
+  const allowViewReview =
+    myProfile && hasPermission(myProfile, 'list_all_datareview');
+
+  const {loading, data, error} = useQuery(ALL_DATA_REVIEWS, {
+    variables: {
+      studyKfId: kfId,
+    },
+  });
+
+  const reviews =
+    data && data.allDataReviews.edges ? data.allDataReviews.edges : [];
+
+  if (!loading && reviews === null) {
+    return (
+      <NotFoundView
+        title="Reviews not found"
+        message={`Cannot find the reviews`}
+      />
+    );
+  }
+
+  if (loading)
+    return (
+      <Dimmer active inverted>
+        <Loader size="large">Loading Data Review List ...</Loader>
+      </Dimmer>
+    );
+
+  if (error)
+    return (
+      <Container as={Segment} basic>
+        <Helmet>
+          <title>
+            {`KF Data Tracker - Study documents - Error ${
+              study ? 'for ' + study.kfId : null
+            }`}
+          </title>
+        </Helmet>
+        <Message
+          negative
+          icon="warning circle"
+          header="Error"
+          content={error.message}
+        />
+      </Container>
+    );
+
+  return (
+    <Grid as={Segment} basic container columns={1}>
+      <Helmet>
+        <title>
+          {`KF Data Tracker - Study documents ${
+            study ? 'for ' + study.name : null
+          }`}
+        </title>
+      </Helmet>
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <h2>Study Reviews</h2>
+        </Grid.Column>
+        <Grid.Column width={12} textAlign="right">
+          {allowAddReview && (
+            <Button
+              compact
+              color="teal"
+              floated="right"
+              size="large"
+              icon="check"
+              labelPosition="left"
+              content="Start New Review"
+              as="label"
+              onClick={() => history.push(`/study/${study.kfId}/start-review`)}
+            />
+          )}
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={16}>
+          {reviews.length > 0 ? (
+            <List relaxed="very">
+              {reviews.map(({node}) => (
+                <List.Item key={node.kfId}>
+                  <Popup
+                    inverted
+                    position="left center"
+                    content={reviewStatus[node.state].text}
+                    trigger={
+                      <List.Icon
+                        name={reviewStatus[node.state].icon}
+                        color={reviewStatus[node.state].color}
+                        size="big"
+                        verticalAlign="middle"
+                      />
+                    }
+                  />
+                  <List.Content>
+                    <List.Header
+                      as="a"
+                      onClick={() =>
+                        history.push(
+                          `/study/${study.kfId}/reviews/${node.kfId}`,
+                        )
+                      }
+                    >
+                      {node.name.substring(0, 100)}
+                      {node.name.length > 100 && '...'}
+                      <small className="text-normal text-grey ml-10">
+                        {node.kfId}
+                      </small>
+                      <small className="text-normal text-grey ml-10">
+                        <TimeAgo
+                          date={node.createdAt}
+                          live={false}
+                          title={longDate(node.createdAt)}
+                        />
+                      </small>
+                    </List.Header>
+                    <List.Description as="a">
+                      {node.description.substring(0, 130)}
+                      {node.description.length > 130 && '...'}
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+              ))}
+            </List>
+          ) : (
+            <Message
+              warning
+              icon="warning circle"
+              header="No data review found."
+              content={
+                allowViewReview
+                  ? 'To create a new data review for this study, click on the start new review button.'
+                  : "You don't have access to data reviews."
+              }
+            />
+          )}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+export default ReviewListView;
