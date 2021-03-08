ce8e32088a4b5cd704d5b34f4d3930f1cac4882f
XuTheBunny <xzhu.fg@gmail.com>
2021-03-08 17:26:34 -0500

    ✨ Export StartReviewView, ReviewListView, ReviewDetailView from index

--- src/documents/views/index.js
+++ src/documents/views/index.js
@@ -2,3 +2,6 @@ export {default as StudyFilesListView} from './StudyFilesListView';
 export {default as FileDetailView} from './FileDetailView';
 export {default as UploadView} from './UploadView';
 export {default as VersionPreviewView} from './VersionPreviewView';
+export {default as StartReviewView} from './StartReviewView';
+export {default as ReviewListView} from './ReviewListView';
+export {default as ReviewDetailView} from './ReviewDetailView';
