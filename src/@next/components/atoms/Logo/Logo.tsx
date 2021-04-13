309d7a7745f6e0ec0eb801941dd48cfccabc96ff
Dan Kolbman <dankolbman@gmail.com>
2021-04-13 14:53:56 -0400

    :art: Add logo component

---
+++ src/@next/components/atoms/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import logo from '../../../../assets/logo.svg';
+
+const Logo = () => {
+    return <img className="w-14 h-14" src={logo} />;
+};
+
+export default Logo;
--- src/@next/components/atoms/index.ts
+++ src/@next/components/atoms/index.ts
@@ -2,3 +2,4 @@ export * from "./Badge";
 export * from "./Button";
 export * from "./Avatar";
 export * from "./Input";
+export * from "./Logo";
