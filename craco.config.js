c6eea897c21c19bccbb5f9166238143498ee9d2b
Dan Kolbman <dankolbman@gmail.com>
2021-04-13 11:03:21 -0400

    :package: Install cosmos

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,26 @@
+const CracoAlias = require('craco-alias');
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
+  webpack: {
+    configure: {
+      module: {
+        rules: [{test: /\.tsx?$/, loader: 'ts-loader'}],
+      },
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
 };
