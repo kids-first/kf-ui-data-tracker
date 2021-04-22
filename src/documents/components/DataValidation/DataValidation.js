a7fbb845b00bac4ecb3346d343d31d3020bd6d5c
XuTheBunny <xzhu.fg@gmail.com>
2021-04-22 11:04:48 -0400

    ✨ Add data validation view with init, running, and completed stage

---
+++ src/documents/components/DataValidation/DataValidation.js
@@ -0,0 +1,244 @@
+import {
+  Button,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Message,
+  Segment,
+  Table,
+} from 'semantic-ui-react';
+import {Link, withRouter} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
+import Markdown from 'react-markdown';
+import defaultAvatar from '../../../assets/defaultAvatar.png';
+import {longDate} from '../../../common/dateUtils';
+
+/**
+ * Form to display data review details
+ */
+const DataValidation = ({
+  reviewNode,
+  history,
+  match,
+  downloadFileMutation,
+  allowViewReview,
+  allowStartValidation,
+  startValidationRun,
+  startPolling,
+  stopPolling,
+}) => {
+  const validationRun =
+    reviewNode &&
+    reviewNode.validationRuns.edges.length > 0 &&
+    reviewNode.validationRuns.edges[reviewNode.validationRuns.edges.length - 1]
+      .node;
+  const validationRunState = validationRun && validationRun.state;
+  const result = reviewNode.validationResultset;
+  const validationState = !allowViewReview
+    ? 'invalid'
+    : result
+    ? 'completed'
+    : validationRunState
+    ? validationRunState
+    : 'not_started';
+
+  const [hasReport, setHasReport] = useState(validationState);
+  const [report, setReport] = useState('');
+  const [validationError, setValidationError] = useState('');
+
+  const studyId = match.params.kfId;
+
+  useEffect(() => {
+    if (result) {
+      stopPolling();
+      setHasReport('completed');
+      const downloadURL = result.downloadReportUrl;
+
+      fetch(downloadURL, {
+        headers: new Headers({
+          Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+        }),
+      })
+        .then(function(res) {
+          if (!res.ok) throw new Error('Failed to fetch the document');
+          return res.text();
+        })
+        .then(function(res) {
+          setReport(res);
+        });
+    }
+  }, [result, stopPolling]);
+
+  console.log();
+  return (
+    <Grid className="mb-50 pr-0" style={{width: '100%'}}>
+      <Grid.Row>
+        <Grid.Column mobile={16} tablet={16} computer={14}>
+          <Link to={`/study/${studyId}/reviews/${reviewNode.kfId}`}>
+            <Button basic size="mini" labelPosition="left" floated="left">
+              <Icon name="arrow left" />
+              Back to Data Review
+            </Button>
+          </Link>
+          <Header as="h2" className="pt-10">
+            {reviewNode.name}
+          </Header>
+        </Grid.Column>
+      </Grid.Row>
+      {validationError && (
+        <Message
+          className="ml-15"
+          negative
+          icon="warning circle"
+          header="Error"
+          content={validationError}
+        />
+      )}
+      {hasReport === 'completed' ? (
+        <Grid.Row className="noVerticalPadding" reversed="computer">
+          <Grid.Column
+            tablet={3}
+            computer={2}
+            mobile={16}
+            className="noPadding"
+          >
+            <Menu vertical secondary fluid>
+              {reviewNode.creator && (
+                <>
+                  <Menu.Item header as="h4" className="text-grey noMargin">
+                    Creator
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Image
+                      className="small-avatar"
+                      src={reviewNode.creator.picture || defaultAvatar}
+                      avatar
+                    />
+                    {reviewNode.creator.displayName ||
+                      reviewNode.creator.username}
+                  </Menu.Item>
+                </>
+              )}
+              {result.createdAt && (
+                <>
+                  <Menu.Item header as="h4" className="text-grey noMargin">
+                    Timestamp
+                  </Menu.Item>
+                  <Menu.Item>{longDate(result.createdAt)}</Menu.Item>
+                </>
+              )}
+            </Menu>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={13} computer={14} className="pt-10">
+            <Header as="h4" color="grey" className="pb-5">
+              Summary
+            </Header>
+            <Grid stackable columns={3}>
+              <Grid.Column>
+                <Header as="h4" textAlign="center">
+                  <Icon name="check circle" color="green" />
+                  <Header.Content>{result.passed} Checks Passed</Header.Content>
+                </Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as="h4" textAlign="center">
+                  <Icon name="times circle" color="red" />
+                  <Header.Content>{result.failed} Checks Failed</Header.Content>
+                </Header>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as="h4" textAlign="center">
+                  <Icon name="warning circle" color="yellow" />
+                  <Header.Content>
+                    {result.didNotRun} Checks Did Not Run
+                  </Header.Content>
+                </Header>
+              </Grid.Column>
+            </Grid>
+            <Header as="h4" color="grey" className="pb-5">
+              Validation Report
+            </Header>
+            {hasReport ? (
+              <Segment basic secondary className="x-scroll h-500-container">
+                <Markdown
+                  source={report}
+                  renderers={{
+                    image: Image,
+                    table: props => <Table>{props.children}</Table>,
+                  }}
+                  linkTarget="_blank"
+                />
+              </Segment>
+            ) : (
+              <Segment basic secondary placeholder>
+                <Header icon as="h4" disabled>
+                  <Icon name="clipboard outline" />
+                  No data validation report available
+                </Header>
+              </Segment>
+            )}
+          </Grid.Column>
+        </Grid.Row>
+      ) : (
+        <Grid.Row className="noVerticalPadding">
+          <Grid.Column className="pr-0 pt-20">
+            {hasReport === 'not_started' && (
+              <Segment placeholder>
+                <Header icon>
+                  <Icon name="clipboard check" />
+                  No data validation report created
+                </Header>
+                {allowStartValidation && (
+                  <Button
+                    primary
+                    onClick={() => {
+                      setHasReport('running');
+                      startValidationRun({
+                        variables: {
+                          input: {
+                            dataReview: reviewNode.id,
+                          },
+                        },
+                      })
+                        .then(resp => {
+                          startPolling(10000);
+                        })
+                        .catch(err => {
+                          setHasReport('not_started');
+                          setValidationError(err.message);
+                        });
+                    }}
+                  >
+                    Run Validation
+                  </Button>
+                )}
+              </Segment>
+            )}
+            {hasReport === 'running' && (
+              <Segment placeholder>
+                <Header icon>
+                  <Icon loading name="spinner" />
+                  Running data validation, this may take several minutes
+                </Header>
+                <Button disabled>Running ...</Button>
+              </Segment>
+            )}
+            {hasReport === 'invalid' && (
+              <Segment placeholder>
+                <Header icon>
+                  <Icon name="dont" />
+                  No data validation report available for this data review
+                </Header>
+              </Segment>
+            )}
+          </Grid.Column>
+        </Grid.Row>
+      )}
+    </Grid>
+  );
+};
+
+export default withRouter(DataValidation);
---
+++ src/documents/views/DataValidationView.js
@@ -0,0 +1,146 @@
+import {
+  Container,
+  Dimmer,
+  Grid,
+  Loader,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+import {DATA_REVIEW, GET_STUDY_BY_ID, MY_PROFILE} from '../../state/queries';
+import {useMutation, useQuery} from '@apollo/client';
+
+import DataValidation from '../components/DataValidation/DataValidation';
+import {FILE_DOWNLOAD_URL} from '../mutations';
+import {Helmet} from 'react-helmet';
+import NotFoundView from '../../views/NotFoundView';
+import React from 'react';
+import {START_VALIDATION_RUN} from '../../state/mutations';
+import {hasPermission} from '../../common/permissions';
+
+/**
+ * Data validation screen for study document review process
+ */
+const DataValidationView = ({
+  match: {
+    params: {kfId, reviewId},
+  },
+  history,
+}) => {
+  // Study query, includes documents
+  const {data: studyData} = useQuery(GET_STUDY_BY_ID, {
+    variables: {
+      id: Buffer.from('StudyNode:' + kfId).toString('base64'),
+    },
+  });
+  const study = studyData && studyData.study;
+
+  // Query for user
+  const {data: profileData} = useQuery(MY_PROFILE);
+  const myProfile = profileData && profileData.myProfile;
+
+  const allowViewReview =
+    myProfile && hasPermission(myProfile, 'view_datareview');
+  const allowStartValidation =
+    myProfile && hasPermission(myProfile, 'add_validationrun');
+
+  const [downloadFileMutation] = useMutation(FILE_DOWNLOAD_URL);
+
+  const {loading, data, error, startPolling, stopPolling} = useQuery(
+    DATA_REVIEW,
+    {
+      variables: {
+        id: Buffer.from('DataReviewNode:' + reviewId).toString('base64'),
+      },
+    },
+  );
+  const review = data && data.dataReview;
+
+  const [startValidationRun] = useMutation(START_VALIDATION_RUN, {
+    refetchQueries: [
+      {
+        query: DATA_REVIEW,
+        variables: {
+          id: Buffer.from('DataReviewNode:' + reviewId).toString('base64'),
+        },
+      },
+    ],
+  });
+
+  if (!loading && review === null) {
+    return (
+      <NotFoundView
+        title="Study not found"
+        message={`Cannot find the study with ID ${kfId}`}
+      />
+    );
+  }
+
+  if (loading)
+    return (
+      <Dimmer active inverted>
+        <Loader size="large">Loading Data Review Details ...</Loader>
+      </Dimmer>
+    );
+
+  if (error)
+    return (
+      <Container as={Segment} basic>
+        <Helmet>
+          <title>
+            {`KF Data Tracker - Study review detail - Error ${
+              study ? 'for ' + study.kfId : null
+            }`}
+          </title>
+        </Helmet>
+        <Message
+          negative
+          icon="warning circle"
+          header="Error"
+          content={error.message}
+        />
+      </Container>
+    );
+
+  if (!allowViewReview)
+    return (
+      <Container as={Segment} basic>
+        <Helmet>
+          <title>
+            {`KF Data Tracker - Study review detail - Error ${
+              study ? 'for ' + study.kfId : null
+            }`}
+          </title>
+        </Helmet>
+        <Message
+          negative
+          icon="warning circle"
+          header="Not allowed"
+          content="You don't have access to view this data review."
+        />
+      </Container>
+    );
+  return (
+    <Grid as={Segment} basic container columns={1}>
+      <Helmet>
+        <title>
+          {`KF Data Tracker - Study review detail ${
+            study ? 'for ' + study.name : null
+          }`}
+        </title>
+      </Helmet>
+      {review && (
+        <DataValidation
+          reviewNode={review}
+          downloadFileMutation={downloadFileMutation}
+          startValidationRun={startValidationRun}
+          allowViewReview={allowViewReview}
+          allowStartValidation={allowStartValidation}
+          startPolling={startPolling}
+          stopPolling={stopPolling}
+        />
+      )}
+    </Grid>
+  );
+};
+
+export default DataValidationView;
