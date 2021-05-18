0eb1c5423dafba321c8a54b49e1525ce566b521e
Dan Kolbman <dankolbman@gmail.com>
2021-05-18 16:16:53 -0400

    :sparkles: Add organization switcher

--- src/state/queries.js
+++ src/state/queries.js
@@ -165,6 +165,15 @@ export const MY_PROFILE = gql`
           }
         }
       }
+      organizations {
+        edges {
+          node {
+            id
+            name
+            image
+          }
+        }
+      }
       ...UserFields
       ...GroupFields
     }
