6fd2e36408944bbb00bc43338d71d2ec2e2a68d2
XuTheBunny <xzhu.fg@gmail.com>
2021-05-25 10:33:16 -0400

    ✨ Add 'Additional' step to study info page

--- src/forms/StudyInfoForm/Steps/index.js
+++ src/forms/StudyInfoForm/Steps/index.js
@@ -1,3 +1,4 @@
 export {default as InfoStep} from './InfoStep';
 export {default as LogisticsStep} from './LogisticsStep';
 export {default as ExternalStep} from './ExternalStep';
+export {default as AdditionalFields} from './AdditionalFields';
