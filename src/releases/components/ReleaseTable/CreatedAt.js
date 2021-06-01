84c9d39b75bf267c3013e14228cac0036c1a8a1b
XuTheBunny <xzhu.fg@gmail.com>
2021-06-01 11:11:04 -0400

    💄 Add time to release started at column

--- src/releases/components/ReleaseTable/CreatedAt.js
+++ src/releases/components/ReleaseTable/CreatedAt.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {Table} from 'semantic-ui-react';
 
 const CreatedAt = ({date}) => (
-  <Table.Cell width="2">
+  <Table.Cell width="4">
     {date
-      ? new Date(date).toLocaleDateString('en-US', {
+      ? new Date(date).toLocaleString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+          weekday: 'long',
           year: 'numeric',
           month: 'short',
           day: 'numeric',
