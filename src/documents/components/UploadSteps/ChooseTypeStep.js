ff1d3e3a636a4b3a019fa7c690643baf8a877276
XuTheBunny <xzhu.fg@gmail.com>
2021-08-24 01:51:52 -0400

    ✨ Display file evaluation results using template evaluation card

--- src/documents/components/UploadSteps/ChooseTypeStep.js
+++ src/documents/components/UploadSteps/ChooseTypeStep.js
@@ -18,41 +18,62 @@ const ChooseTypeStep = ({
   expeditedTypes,
   nextStep,
   previousStep,
+  version,
+  studyId,
+  templates,
+  selectedTemplate,
+  setSelectedTemplate,
+  evaluateResult,
+  setFieldValue,
 }) => (
   <>
     <Grid.Row>
       <Grid.Column width={4}>
         <Image src={documentSelection} size="medium" centered rounded />
-        <Header>Choose File Type</Header>
+        <Header>Choose Document Type</Header>
         <p>Help the DRC by indicating what type of file this is.</p>
-        <Header size="small">Expedited Types</Header>
+        <Header size="small">Templated Types</Header>
         <p>
-          Expedited types follow a defined file structure and can help the DRC
-          process the file faster
+          Templated types reflect the data templates your organization has setup
+          for your studies. They follow a defined file structure and can help
+          the DRC process the file faster.
         </p>
         <Header size="small">General Types</Header>
         <p>
-          General types provide a classification for the DRC to help organize
-          and find files
+          Use a General type if none of the Templated types provide the
+          appropriate categorization for your file. General types are still used
+          by the DRC to help organize and find files.
         </p>
       </Grid.Column>
       <Grid.Column width={12}>
+        {evaluateResult.graphQLErrors && (
+          <Message
+            negative
+            icon="warning circle"
+            header="Error"
+            content={evaluateResult.graphQLErrors[0].message}
+          />
+        )}
         <Form.Field required>
-          <Header size="small">
-            <Icon name="shipping fast" />
-            Expedited Types
-          </Header>
+          <Header size="small">Templated Types</Header>
           <Dimmer.Dimmable>
-            {expeditedTypes.length ? (
+            {templates.length ? (
               <Card.Group itemsPerRow={3}>
-                {expeditedTypes.map(item => (
-                  <Field
-                    expedited
-                    component={TypeCard}
-                    name="file_type"
-                    id={item}
-                    label={item}
-                    key={item}
+                {templates.map(({node}) => (
+                  <TemplateCard
+                    node={node}
+                    key={node.id}
+                    studyId={studyId}
+                    selectedTemplate={selectedTemplate}
+                    setSelectedTemplate={setSelectedTemplate}
+                    setFieldValue={setFieldValue}
+                    result={
+                      evaluateResult.results
+                        ? evaluateResult.results.filter(
+                            r => r.templateVersion.id === node.id,
+                          )
+                        : []
+                    }
                   />
                 ))}
               </Card.Group>
@@ -63,20 +84,20 @@ const ChooseTypeStep = ({
                 <TypeCardPlaceholder />
               </Card.Group>
             )}
-            <Dimmer active={!expeditedTypes.length} inverted>
-              <Header size="small">No expedited types available</Header>
-              <p>
-                If you'd like to have this file processed faster, please
-                consider formatting the file according to one of the{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={urls.expeditedFiles}
-                >
-                  file types
-                </a>
-                .
-              </p>
+            <Dimmer active={templates.length === 0} inverted>
+              <Header size="small">
+                No data templates available
+                <Header.Subheader>
+                  <p className="mb-0">
+                    Please contact organization administrators to create data
+                    submission templates.
+                  </p>
+                  <p>
+                    Data templates help to standardize how data is data
+                    collected for this study.
+                  </p>
+                </Header.Subheader>
+              </Header>
             </Dimmer>
           </Dimmer.Dimmable>
           <Header size="small">General Types</Header>
@@ -88,6 +109,7 @@ const ChooseTypeStep = ({
                 id={item}
                 label={item}
                 key={item}
+                setSelectedTemplate={setSelectedTemplate}
               />
             ))}
           </Card.Group>
