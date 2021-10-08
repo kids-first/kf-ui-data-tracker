3efe3e7aca6ea33fc549037fd43d34073ea75c30
Natasha Singh <dukedesi22@gmail.com>
2021-10-07 20:04:35 -0400

    :bug: Only exchange ref token if logged in user is referred user

--- src/views/InviteView.js
+++ src/views/InviteView.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import queryString from 'query-string';
+import {ApolloConsumer} from '@apollo/client';
+import {Helmet} from 'react-helmet';
 import jwtDecode from 'jwt-decode';
-import {Link} from 'react-router-dom';
+import queryString from 'query-string';
 import {useMutation} from '@apollo/client';
 import {Redirect} from 'react-router-dom';
 import {
@@ -11,11 +12,16 @@ import {
   Image,
   Loader,
   Segment,
+  Button,
+  Icon,
 } from 'semantic-ui-react';
 import {NotFoundView} from '../views';
 import {EXCHANGE_REFERRAL_TOKEN} from '../state/mutations';
-import added from '../assets/added.svg';
+import {auth} from '../state/auth';
 import StudyTable from '../components/StudyList/StudyTable';
+import {ImageMessage} from '../components/ImageMessage';
+import welcome from '../assets/welcome.svg';
+import added from '../assets/added.svg';
 
 export const InviteView = ({history}) => {
   const [
@@ -26,7 +32,6 @@ export const InviteView = ({history}) => {
       data: exchangeTokenData,
     },
   ] = useMutation(EXCHANGE_REFERRAL_TOKEN);
-  const qs = queryString.parse(history.location.search);
 
   const [columns, setColumns] = useState({
     columns: [],
@@ -36,23 +41,37 @@ export const InviteView = ({history}) => {
     },
   });
 
+  // Check access token
+  const accessToken = localStorage.getItem('accessToken');
+  const accessTokenValid =
+    accessToken && new Date().getTime() < jwtDecode(accessToken).exp * 1000;
+
+  // Try to claim the referral token
+  const qs = queryString.parse(history.location.search);
+  const tokenRelayId =
+    qs.token && Buffer.from('ReferralTokenNode:' + qs.token).toString('base64');
+
   useEffect(() => {
-    const relayId =
-      qs.token &&
-      Buffer.from('ReferralTokenNode:' + qs.token).toString('base64');
-    exchangeToken({variables: {token: relayId}});
-  }, [exchangeToken, qs.token]);
+    exchangeToken({variables: {token: tokenRelayId}}).catch(err => {
+      console.log('Exchange token error');
+      console.log(err);
+    });
+  }, [exchangeToken, tokenRelayId]);
+
+  // Check if current logged in user is the intended recipient of the referral
+  // token
+  const tokenErrorMessage = exchangeTokenError
+    ? [...exchangeTokenError.graphQLErrors].map(err => err.message).join(' ')
+    : '';
+  const wrongUserError = tokenErrorMessage.includes('cannot use this referral');
 
   // If there's no token, return to the home screen
   if (!qs.token) {
     return <Redirect to="/" />;
   }
 
-  // If the user is not logged in or does not have an account yet, we need them
-  // to log in first before we can exchange an invite token for them.
-  const token = localStorage.getItem('accessToken');
-  const valid = token && new Date().getTime() < jwtDecode(token).exp * 1000;
-  if (!valid) {
+  // User is not logged in or doesn't exist
+  if (!accessTokenValid) {
     return (
       <Redirect
         to={{
@@ -62,30 +81,84 @@ export const InviteView = ({history}) => {
       />
     );
   }
+  if (!exchangeTokenError && (exchangeTokenLoading || !exchangeTokenData)) {
+    return (
+      <Dimmer active>
+        <Loader active inverted>
+          Verifying referral link...
+        </Loader>
+      </Dimmer>
+    );
+  }
+  if (accessTokenValid && exchangeTokenError && wrongUserError) {
+    return (
+      <>
+        <Container
+          as={Segment}
+          basic
+          vertical
+          padded="very"
+          textAlign="center"
+          text
+        >
+          <Helmet>
+            <title>KF Data Tracker - User Invitation </title>
+          </Helmet>
+          <ImageMessage
+            image={welcome}
+            title="User Invitation Error"
+            message={
+              <>
+                {tokenErrorMessage}. Please logout and signup or login with the
+                correct email. If you have any issues, please get in touch at{' '}
+                <a href="mailto:support@kidsfirstdrc.org">
+                  support@kidsfirstdrc.org
+                </a>
+              </>
+            }
+          />
+          <ApolloConsumer>
+            {client => (
+              <Button
+                className="mb-15"
+                size="big"
+                onClick={() => auth.logout(window.location.href)}
+                positive
+                icon
+                labelPosition="right"
+              >
+                Click here to logout
+                <Icon name="chevron right" />
+              </Button>
+            )}
+          </ApolloConsumer>
+        </Container>
+      </>
+    );
+  }
 
-  if (exchangeTokenError) {
+  if (exchangeTokenError && !wrongUserError) {
     return (
       <NotFoundView
-        title="Error"
-        message={[...exchangeTokenError.graphQLErrors]
+        title="Exchange Token Error"
+        message={[
+          ...exchangeTokenError.graphQLErrors,
+          {
+            message: 'You may have already used the referral link',
+          },
+        ]
           .map(err => err.message)
           .join(' ')}
       />
     );
   }
-
-  if (exchangeTokenLoading || !exchangeTokenData) {
-    return (
-      <Dimmer active>
-        <Loader active inverted>
-          Adding you to studies...
-        </Loader>
-      </Dimmer>
-    );
-  }
+  // Set to null so that the header will be forced to refetch the user's profile
+  localStorage.setItem('currentOrganization', 'null');
 
   const studies =
     exchangeTokenData.exchangeReferralToken.referralToken.studies.edges;
+  const org =
+    exchangeTokenData.exchangeReferralToken.referralToken.organization;
 
   const handleSort = column => () => {
     const direction =
@@ -108,11 +181,12 @@ export const InviteView = ({history}) => {
         </Header>
         <Image centered src={added} size="large" />
         <Header textAlign="center">
-          We added you to {studies && studies.length} studies in the Data
-          Tracker. Nice!
+          We added you to your new organization {org.name}
+          <br />
+          and {studies && studies.length} studies in the Data Tracker. Nice!
           <Header.Subheader>
-            Your new studies are listed below. See all your studies and details
-            on <Link to="/">your studies</Link> page.
+            Your new studies are listed below. Click a study to view its details
+            in Data Tracker!
           </Header.Subheader>
         </Header>
         <Header as="h1">Your new studies</Header>
