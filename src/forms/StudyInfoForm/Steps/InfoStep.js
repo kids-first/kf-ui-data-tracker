a36a3e3adfb39989dda4157bd98eacdf0265f2dd
gsantia <gs495@drexel.edu>
2021-11-15 15:57:47 -0500

    :sparkles: Add fields to InfoStep of StudyForm

--- src/forms/StudyInfoForm/Steps/InfoStep.js
+++ src/forms/StudyInfoForm/Steps/InfoStep.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header} from 'semantic-ui-react';
+import {Dropdown, Form, Header} from 'semantic-ui-react';
 import FormField from '../../FormField';
 
 const InfoStep = ({
@@ -12,8 +12,33 @@ const InfoStep = ({
   editing,
   allowEdit,
 }) => {
-  const {values, errors, touched, handleChange, handleBlur} = formikProps;
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    setFieldValue,
+  } = formikProps;
   const mapFields = [
+    {
+      required: true,
+      id: 'shortCode',
+      name: 'Study Short Code',
+      description: 'A short code that identifies the study.',
+    },
+    {
+      required: true,
+      id: 'domain',
+      name: 'Domain',
+      description: 'The category of disease being studied.',
+    },
+    {
+      required: true,
+      id: 'program',
+      name: 'Program',
+      description: 'The administrative organization or group of the study.',
+    },
     {
       required: true,
       id: 'name',
@@ -32,9 +57,41 @@ const InfoStep = ({
       id: 'investigatorName',
       name: 'Principal Investigator',
       description:
-        'The name of the principle investigator, used to categorize this study',
+        'The name of the principle investigator, used to categorize this study.',
     },
   ];
+  const dropdownOptions = [
+    {
+      key: 'unknown',
+      text: 'Unknown',
+      value: 'UNKNOWN',
+    },
+    {
+      key: 'cancer',
+      text: 'Cancer',
+      value: 'CANCER',
+    },
+    {
+      key: 'birthdefect',
+      text: 'Birth Defect',
+      value: 'BIRTHDEFECT',
+    },
+    {
+      key: 'cancerandbirthdefect',
+      text: 'Cancer and Birth Defect',
+      value: 'CANCERANDBIRTHDEFECT',
+    },
+    {
+      key: 'covid19',
+      text: 'Covid-19',
+      value: 'COVID19',
+    },
+    {
+      key: 'other',
+      text: 'Other',
+      value: 'OTHER',
+    }
+  ]
   return (
     <>
       <Header
@@ -42,25 +99,50 @@ const InfoStep = ({
         className="text-wrap-75"
         content="Please provide the study's full name and a shortened version that may be used for display purposes."
       />
-      {mapFields.map(item => (
-        <FormField
-          key={item.id}
-          allowEdit={allowEdit}
-          newStudy={newStudy}
-          required={item.required}
-          id={item.id}
-          name={item.name}
-          description={item.description}
-          focused={focused === item.id}
-          value={values[item.id]}
-          touched={touched[item.id]}
-          errors={errors[item.id]}
-          handleChange={handleChange}
-          handleBlur={handleBlur}
-          handleFocus={id => setFocused(id)}
-          readOnly={!editing && !newStudy}
-        />
-      ))}
+      {mapFields.map(item => {
+        if (item.id !== 'domain') {
+          return (
+            <FormField
+              key={item.id}
+              allowEdit={allowEdit}
+              newStudy={newStudy}
+              required={item.required}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              focused={focused === item.id}
+              value={values[item.id]}
+              touched={touched[item.id]}
+              errors={errors[item.id]}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              handleFocus={id => setFocused(id)}
+              readOnly={!editing && !newStudy}
+            />
+          )
+        }
+        else {
+          return (
+            <Form.Field required={true}>
+              <label className="noMargin">Domain:</label>
+              <p className="noMargin">
+                <small>{item.description}</small>
+              </p>
+              <Dropdown
+                key={item.id}
+                id={item.id}
+                selection
+                options={dropdownOptions}
+                fluid
+                onChange={(ev, data) => {
+                  setFieldValue('domain', data.value);
+                }}
+                value={values.domain}
+              />
+            </Form.Field>
+          )
+        }
+      })}
       {!newStudy && (
         <FormField
           allowEdit={allowEdit}
